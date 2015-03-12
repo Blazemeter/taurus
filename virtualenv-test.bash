0579f302f20bbd25592b0b9d136aa9681b578400
Andrey Pohilko <apc4@ya.ru>
2015-03-12 12:54:16 +0300

    try to generate the docs dump

--- virtualenv-test.bash
+++ virtualenv-test.bash
@@ -1,18 +1,24 @@
 #!/bin/bash -xe
+
+# setup env
 virtualenv --clear --system-site-packages build
 source build/bin/activate
 
-pip install colorlog pyyaml psutil lxml cssselect
+# install depends
+pip install colorlog pyyaml psutil lxml cssselect grip
 
+# run unit tests
 ./run-test.sh
 
+# run installation test
 pip -v install dist/bzt-*.tar.gz
 
+# run functional tests
 bzt -o execution.scenario.script=tests/jmx/dummy.jmx -d build/test -o modules.jmeter.path=build/jmeter/bin/jmeter
-
 bzt -d build/test tests/json/jmeter_minimal.json -o modules.jmeter.path=build/jmeter/bin/jmeter
-
 bzt -d build/test tests/json/get-post.json -o modules.jmeter.path=build/jmeter/bin/jmeter
 
+# generate docs
+./make-docs.sh
 
 deactivate
