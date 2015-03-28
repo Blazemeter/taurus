58f697f0325ecc1ce26a364636051bd67ae1e806
Andrey Pohilko <apc4@ya.ru>
2015-03-28 21:00:10 +0300

    Fix unit test

--- tests/modules/test_blazeMeterUploader.py
+++ tests/modules/test_blazeMeterUploader.py
@@ -10,17 +10,19 @@ from tests.mocks import EngineEmul
 class TestBlazeMeterUploader(BZTestCase):
     def test_check(self):
         client = BlazeMeterClientEmul(logging.getLogger(''))
-        client.results.append({'result': {}})
-        client.results.append({'result': {}})
-        client.results.append({'result': {'id': 'unittest1'}})
-        client.results.append({'result': {'session': {'id': 'sess1', 'userId': 1}, 'signature': ''}})  # start
-        client.results.append({'result': {'session': {}}})  # first check kpi push
+        client.results.append({"marker": "ping", 'result': {}})
+        #client.results.append({"marker": "tests", 'result': {}})
+        #client.results.append({"marker": "test-create", 'result': {'id': 'unittest1'}})
+        client.results.append(
+            {"marker": "sess-start", 'result': {'session': {'id': 'sess1', 'userId': 1}, 'signature': ''}})
+        client.results.append({"marker": "first push", 'result': {'session': {}}})
         # client.results.append(None)  # first check error stats
-        client.results.append({'result': {'session': {"statusCode": 140, 'status': 'ENDED'}}})  # second check kpi push
+        client.results.append(
+            {"marker": "second push", 'result': {'session': {"statusCode": 140, 'status': 'ENDED'}}})
         client.results.append(None)  # second check error stats
-        client.results.append({'result': {'session': {}}})  # post-proc kpi push
+        client.results.append({"marker": "post-proc push", 'result': {'session': {}}})
         client.results.append(None)  # post-proc error stats
-        client.results.append({'result': {'session': {}}})  # terminate
+        client.results.append({"marker": "terminate", 'result': {'session': {}}})
 
         obj = BlazeMeterUploader()
         obj.engine = EngineEmul()
