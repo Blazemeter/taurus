2f810f38d870a32a95fe104608414cf0dd49f975
Andrey Pohilko <apc4@ya.ru>
2015-04-28 12:06:54 +0300

    Test for script with spaces

--- tests/test_engine.py
+++ tests/test_engine.py
@@ -1,5 +1,7 @@
 """ unit test """
+import os
 import tempfile
+import shutil
 
 from tests import BZTestCase, __dir__, local_paths_config
 from tests.mocks import EngineEmul
@@ -66,6 +68,25 @@ class TestEngine(BZTestCase):
             self.paths
         ]
         self.obj.configure(configs)
+        self.obj.prepare()
+        self.obj.run()
+        self.obj.post_process()
+
+    def test_gatling_spaces(self):
+        configs = [
+            __dir__() + "/../bzt/10-base.json",
+            __dir__() + "/json/gatling.json",
+            self.paths
+        ]
+        self.obj.configure(configs)
+        space = os.path.join(self.obj.artifacts_dir, "with space")
+        os.makedirs(space)
+        self.obj.artifacts_dir = space
+
+        shutil.copy("tests/gatling/BasicSimulation.scala", space)
+
+        self.obj.config.merge({"execution": {"scenario": {"script": os.path.join(space, "BasicSimulation.scala")}}})
+
         self.obj.prepare()
         self.obj.run()
         self.obj.post_process()
