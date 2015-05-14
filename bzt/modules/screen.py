bf9f44f85acc0e979d98942267a465d3903fdd55
vasischev <vasishchev@blazemeter.com>
2015-05-14 18:09:14 +0300

    fixed py3 test

--- bzt/modules/screen.py
+++ bzt/modules/screen.py
@@ -140,7 +140,10 @@ class GUIScreen(BaseScreen):
             pos = 0
             for part in row:
                 txt = part[2]
-                strlen = len(txt.decode('utf8'))
+                if isinstance(txt, six.text_type):
+                    strlen = len(txt)
+                else:
+                    strlen = len(txt.decode('utf8'))
                 self.text.insert(Tkinter.END, txt)
                 if part[0] is not None:
                     self.text.tag_add(part[0], "%s.%s" % (idx + 1, pos), "%s.%s" % (idx + 1, pos + strlen))
