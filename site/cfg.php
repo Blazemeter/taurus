a0135679afd1eb4e088a714fa908ba4e76ad2389
Andrey Pohilko <apc4@ya.ru>
2015-07-01 15:30:53 +0300

    Site skeleton

--- site/cfg.php
+++ site/cfg.php
@@ -1,12 +1,9 @@
 <?php
-require_once __DIR__.'/vendor/autoload.php';
+require_once __DIR__ . '/vendor/autoload.php';
 
-use PWE\Core\PWEAutoloader;
-
-\PWE\Core\PWELogger::setStdErr(__DIR__."/dat/tmp/pwe.log");
-\PWE\Core\PWELogger::setStdOut(__DIR__."/dat/tmp/pwe.log");
-\PWE\Core\PWELogger::setLevel($_SERVER['SERVER_ADDR']==$_SERVER['REMOTE_ADDR']?\PWE\Core\PWELogger::DEBUG:\PWE\Core\PWELogger::WARN);
+\PWE\Core\PWELogger::setStdErr(__DIR__ . "/dat/tmp/pwe.log");
+\PWE\Core\PWELogger::setStdOut(__DIR__ . "/dat/tmp/pwe.log");
+\PWE\Core\PWELogger::setLevel($_SERVER['SERVER_ADDR'] == $_SERVER['REMOTE_ADDR'] ? \PWE\Core\PWELogger::DEBUG : \PWE\Core\PWELogger::WARNING);
 
 /** @var $PWECore PWE\Core\PWECore */
 $PWECore->setRootDirectory(__DIR__);
-?>
