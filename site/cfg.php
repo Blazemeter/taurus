e8677d63114028ef03e0631c1aad796be07e7475
Andrey Pohilko <apc4@ya.ru>
2015-07-02 11:33:59 +0300

    use index.php from vendor

--- site/cfg.php
+++ site/cfg.php
@@ -1,10 +1,22 @@
 <?php
 require_once __DIR__ . '/vendor/autoload.php';
 
-\PWE\Core\PWELogger::setStdErr(__DIR__ . "/dat/tmp/pwe.log");
-\PWE\Core\PWELogger::setStdOut(__DIR__ . "/dat/tmp/pwe.log");
-\PWE\Core\PWELogger::setLevel($_SERVER['SERVER_ADDR'] == $_SERVER['REMOTE_ADDR'] ? \PWE\Core\PWELogger::DEBUG : \PWE\Core\PWELogger::WARNING);
+if ($_SERVER['SERVER_ADDR'] == $_SERVER['REMOTE_ADDR']) {
+    $level = \PWE\Core\PWELogger::DEBUG;
+    $logfile = "/tmp/pwe.log";
+    $tempdir = "/tmp";
+} else {
+    $level = \PWE\Core\PWELogger::WARNING;
+    $logfile = "/home/gettauru/logs/pwe.log";
+    $tempdir = "/home/gettauru/tmp";
+}
+
+\PWE\Core\PWELogger::setStdErr($logfile);
+\PWE\Core\PWELogger::setStdOut($logfile);
+\PWE\Core\PWELogger::setLevel($level);
+
 
 /** @var $PWECore PWE\Core\PWECore */
 $PWECore->setRootDirectory(__DIR__);
 $PWECore->setXMLDirectory($PWECore->getDataDirectory());
+$PWECore->setTempDirectory($tempdir);
