77974d22af791434843085dc99ade00cd6a73cc3
Andrey Pohilko <apc4@ya.ru>
2015-07-09 23:54:21 +0300

    Fix idea suggests

--- bzt/moves.py
+++ bzt/moves.py
@@ -19,28 +19,28 @@ if PY2:
         return iter(dictionary.iteritems(**kw))
 
 
-    urlopen = __import__("urllib2", fromlist=("urlopen")).urlopen
-    urlencode = __import__("urllib", fromlist=("urlencode")).urlencode
-    urlsplit = __import__("urlparse", fromlist=("urlsplit")).urlsplit
-    urlparse = __import__("urlparse", fromlist=("urlparse")).urlparse
-    build_opener = __import__("urllib2", fromlist=("build_opener")).build_opener
-    install_opener = __import__("urllib2", fromlist=("install_opener")).install_opener
+    urlopen = __import__("urllib2", fromlist="urlopen").urlopen
+    urlencode = __import__("urllib", fromlist="urlencode").urlencode
+    urlsplit = __import__("urlparse", fromlist="urlsplit").urlsplit
+    urlparse = __import__("urlparse", fromlist="urlparse").urlparse
+    build_opener = __import__("urllib2", fromlist="build_opener").build_opener
+    install_opener = __import__("urllib2", fromlist="install_opener").install_opener
 
-    Request = __import__("urllib2", fromlist=("Request")).Request
-    ProxyHandler = __import__("urllib2", fromlist=("ProxyHandler")).ProxyHandler
-    HTTPError = __import__("urllib2", fromlist=("HTTPError")).HTTPError
-    FancyURLopener = __import__("urllib", fromlist=("FancyURLopener")).FancyURLopener
-    URLopener = __import__("urllib", fromlist=("URLopener")).URLopener
+    Request = __import__("urllib2", fromlist="Request").Request
+    ProxyHandler = __import__("urllib2", fromlist="ProxyHandler").ProxyHandler
+    HTTPError = __import__("urllib2", fromlist="HTTPError").HTTPError
+    FancyURLopener = __import__("urllib", fromlist="FancyURLopener").FancyURLopener
+    URLopener = __import__("urllib", fromlist="URLopener").URLopener
     ConfigParser = __import__("ConfigParser")
     Tkinter = __import__("Tkinter")
     TkMoved = Tkinter.Tk
     Text = Tkinter.Text
-    tkFont = __import__("tkFont", fromlist=("Font")).Font
+    tkFont = __import__("tkFont", fromlist="Font").Font
     UserDict = __import__("UserDict").UserDict
 
     viewvalues = operator.methodcaller("viewvalues")
 
-    StringIO = BytesIO = __import__("StringIO", fromlist=("StringIO")).StringIO
+    StringIO = BytesIO = __import__("StringIO", fromlist="StringIO").StringIO
 
 
     def to_bytes(string):
@@ -62,25 +62,25 @@ else:
         return iter(dictionary.items(**kw))
 
 
-    urlopen = __import__("urllib.request", fromlist=("urlopen")).urlopen
-    urlencode = __import__("urllib.parse", fromlist=("urlencode")).urlencode
-    urlsplit = __import__("urllib.parse", fromlist=("urlsplit")).urlsplit
-    urlparse = __import__("urllib.parse", fromlist=("urlparse")).urlparse
-    build_opener = __import__("urllib.request", fromlist=("urlopen")).build_opener
-    install_opener = __import__("urllib.request", fromlist=("install_opener")).install_opener
+    urlopen = __import__("urllib.request", fromlist="urlopen").urlopen
+    urlencode = __import__("urllib.parse", fromlist="urlencode").urlencode
+    urlsplit = __import__("urllib.parse", fromlist="urlsplit").urlsplit
+    urlparse = __import__("urllib.parse", fromlist="urlparse").urlparse
+    build_opener = __import__("urllib.request", fromlist="urlopen").build_opener
+    install_opener = __import__("urllib.request", fromlist="install_opener").install_opener
 
-    Request = __import__("urllib.request", fromlist=("Request")).Request
-    ProxyHandler = __import__("urllib.request", fromlist=("ProxyHandler")).ProxyHandler
-    HTTPError = __import__("urllib.error", fromlist=("HTTPError")).HTTPError
-    FancyURLopener = __import__("urllib.request", fromlist=("FancyURLopener")).FancyURLopener
-    URLopener = __import__("urllib.request", fromlist=("URLopener")).URLopener
+    Request = __import__("urllib.request", fromlist="Request").Request
+    ProxyHandler = __import__("urllib.request", fromlist="ProxyHandler").ProxyHandler
+    HTTPError = __import__("urllib.error", fromlist="HTTPError").HTTPError
+    FancyURLopener = __import__("urllib.request", fromlist="FancyURLopener").FancyURLopener
+    URLopener = __import__("urllib.request", fromlist="URLopener").URLopener
     Tkinter = __import__("tkinter")
     _Tkinter = __import__("tkinter", fromlist=("Tk", "Text"))
     TkMoved = _Tkinter.Tk
     Text = _Tkinter.Text
-    tkFont = __import__("tkinter.font", fromlist=("Font")).Font
+    tkFont = __import__("tkinter.font", fromlist="Font").Font
     ConfigParser = __import__("configparser")
-    UserDict = __import__("collections", fromlist=("UserDict")).UserDict
+    UserDict = __import__("collections", fromlist="UserDict").UserDict
 
     viewvalues = operator.methodcaller("values")
 
