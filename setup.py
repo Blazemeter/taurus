91c5b87a97a6fadb0e3d7cb9249787dc14af66a2
Andrey Pohilko <apc4@ya.ru>
2015-07-11 22:13:02 +0300

    Ship bzt.six within egg

--- setup.py
+++ setup.py
@@ -56,13 +56,13 @@ setup(
     install_requires=[
         'pyyaml', 'psutil', 'colorlog', 'colorama', 'lxml >= 3.4.2', 'cssselect', 'urwid', 'six', 'nose',
         'selenium', 'progressbar33'],
-    packages=['bzt', 'bzt.modules', 'bzt.modules.resources'],
+    packages=['bzt', 'bzt.six', 'bzt.modules', 'bzt.modules.resources'],
     entry_points={
         'console_scripts': [
             'bzt=bzt.cli:main',
         ],
     },
-    include_package_data = True,
+    include_package_data=True,
     package_data={
         "bzt": [],
     },
