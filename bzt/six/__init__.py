f743026a375e1eb90e1fa2529c2f7b5f26957db7
Andrey Pohilko <apc4@ya.ru>
2015-07-11 22:39:41 +0300

    Pylint ignore

--- bzt/six/__init__.py
+++ bzt/six/__init__.py
@@ -15,7 +15,7 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-# pylint1: skip-file
+# pylint: skip-file
 import sys
 
 PY2 = sys.version_info[0] == 2
