a94e4b13ba0823d41cac77bdcec2e35e493f0fa0
Andrey Pohilko <apc4@ya.ru>
2015-08-13 18:53:16 +0300

    Implement update done

--- site/updates.php
+++ site/updates.php
@@ -2,26 +2,39 @@
 
 namespace Taurus;
 
-use PWE\Core\PWELogger;
-use PWE\Lib\Smarty\SmartyWrapper;
-use PWE\Modules\Outputable;
-use PWE\Modules\PWEModule;
-
-class UpdateChecker extends PWEModule implements Outputable
+class UpdateChecker extends \PWE\Modules\PWEModule implements \PWE\Modules\Outputable
 {
     public function process()
     {
-        PWELogger::warn("Check update: %s/%s", $_REQUEST['version'], $_REQUEST['installID']);
-
+        $latest = $this->getLatestVersion();
         $resp = array(
-            "latest" => null,
-            "needsUpgrade" => false
+            "latest" => $latest,
+            "needsUpgrade" => version_compare($latest, $_REQUEST['version'], '>')
         );
+        \PWE\Core\PWELogger::warn("Check update: %s %s %s", $_REQUEST['version'], $resp['latest'], $resp['needsUpgrade'], $_REQUEST['installID']);
 
         $smarty = $this->PWE->getSmarty();
-        $smarty->setTemplateFile(__DIR__.'/dat/json.tpl');
+        $smarty->setTemplateFile(__DIR__ . '/dat/json.tpl');
         $smarty->assign("data", $resp);
 
         $this->PWE->addContent($smarty);
+    }
+
+    public function getLatestVersion()
+    {
+        $pypi = $this->getPypiInfo();
+        return $pypi['info']['version'];
+    }
+
+    public function getPypiInfo()
+    {
+        $fname = $this->PWE->getTempDirectory() . "/bzt-pypi.json";
+        if (!file_exists($fname) || (time() - filemtime($fname) > 60 * 60)) {
+            $url = "https://pypi.python.org/pypi/bzt/json";
+            \PWE\Core\PWELogger::info("Refresh PyPi cache: %s", $url);
+            file_put_contents($fname, fopen($url, 'r'));
+        }
+
+        return json_decode(file_get_contents($fname), true);
     }
 }
