c1ae269f99b03efae6704af468467654e26eabb8
vasischev <vasischev@blazemeter.com>
2015-08-18 15:23:17 +0300

    junit xml writer

--- bzt/modules/reporting.py
+++ bzt/modules/reporting.py
@@ -22,7 +22,7 @@ from datetime import datetime
 from bzt.modules.aggregator import DataPoint, KPISet
 from bzt.engine import Reporter, AggregatorListener
 from bzt.modules.passfail import PassFailStatus
-from bzt.six import parse, etree
+from bzt.six import parse, etree, StringIO
 from tempfile import NamedTemporaryFile
 
 
@@ -169,58 +169,61 @@ class JUnitXMLReporter(Reporter, AggregatorListener):
 
             _kpiset = self.last_second[DataPoint.CUMULATIVE]
             summary_kpiset = _kpiset[""]
-            self.write_summary_report(tmp_xml_file, summary_kpiset)
-            testcase_template = '  <testcase classname="{classname}" name="{name}" time="0">\n'
-            err_template = '    <error message="{message}" type="{type}">'
+            xml_writer = JUnitXMLWriter(tmp_xml_file)
+            self.write_summary_report(xml_writer, summary_kpiset)
+
+            # testcase_template = '  <testcase classname="{classname}" name="{name}" time="0">\n'
+            # err_template = '    <error message="{message}" type="{type}">'
 
             for key in sorted(_kpiset.keys()):
-                if key != "":   # if label is not blank
+                if key != "":  # if label is not blank
                     class_name, resource_name = JUnitXMLReporter.__convert_label_name(key)
-                    # generate <testcase> subelement
-
-                    test_case = testcase_template.format(classname=class_name, name=resource_name)
-                    # test_case = etree.SubElement(root_xml_element, "testcase", classname=class_name,
-                    #                              name=resource_name, time="0")
-                    # if any errors in label report, generate <error> subelement with error description
-
-                    tmp_xml_file.write(testcase_template)
+                    # test_case = testcase_template.format(classname=class_name, name=resource_name)
+                    xml_writer.add_testcase(close=False, classname=class_name, name=resource_name)
+                    # tmp_xml_file.write(test_case)
 
                     if _kpiset[key][KPISet.ERRORS]:
                         for er_dict in _kpiset[key][KPISet.ERRORS]:
                             err_message = str(er_dict["rc"])
                             err_type = str(er_dict["msg"])
                             err_desc = "total errors of this type:" + str(er_dict["cnt"])
-                            err = err_template.format(message=err_message, type=err_type)
-                            tmp_xml_file.write(err)
-                            tmp_xml_file.write(err_desc)
-                            tmp_xml_file.write("</error>\n")
+                            # err = err_template.format(message=err_message, type=err_type)
+                            xml_writer.add_error(close=False, message=err_message, type=err_type)
+                            # tmp_xml_file.write(err)
+                            xml_writer.raw_write(err_desc)
+                            xml_writer.close_element()
 
-                            # etree.SubElement(test_case, "error", type=err_type,
-                            #                  message=err_message).text = err_desc
-
-                    tmp_xml_file.write("  </testcase>\n")
-            tmp_xml_file.write("</testsuite>")
+                            # tmp_xml_file.write("</error>\n")
+                    # tmp_xml_file.write("  </testcase>\n")
+                    xml_writer.close_element()
+            # tmp_xml_file.write("</testsuite>")
+            xml_writer.close_element()
         return tmp_xml_file.name
 
-    def write_summary_report(self, tmp_xml_file, summary_kpiset):
+    def write_summary_report(self, xml_writer, summary_kpiset):
         """
         writes testcase class_name="summary"
         :return: tmp xml filename
         """
-        tmp_xml_file.write("<?xml version='1.0' encoding='UTF-8'?>\n")
 
         succ = str(summary_kpiset[KPISet.SUCCESSES])
         throughput = str(summary_kpiset[KPISet.SAMPLE_COUNT])
         fail = str(summary_kpiset[KPISet.FAILURES])
 
-        testsuite_template = "<testsuite failures='{failures}' name='{test_name}' skip='0' tests='{tests}'>\n"
-        tmp_xml_file.write(testsuite_template.format(failures=fail, test_name="sample_labels", tests=throughput))
+        # testsuite_template = "<testsuite failures='{failures}' name='{test_name}' skip='0' tests='{tests}'>\n"
+        # tmp_xml_file.write(testsuite_template.format(failures=fail, test_name="sample_labels", tests=throughput))
 
-        summary_test_case_template = '  <testcase class_name="summary" name="summary_report">\n'
-        tmp_xml_file.write(summary_test_case_template)
+        xml_writer.add_testsuite(close=False, failures=fail, name='sample_labels', skip='0', tests=throughput)
 
-        errors_report_template = '    <error message="error statistics:" type="http error">\n'
-        tmp_xml_file.write(errors_report_template)
+        # summary_test_case_template = '  <testcase classname="bzt" name="summary_report">\n'
+        # tmp_xml_file.write(summary_test_case_template)
+
+        xml_writer.add_testcase(close=False, classname="bzt", name="summary_report")
+
+        # errors_report_template = '    <error message="error statistics:" type="http error">\n'
+        # tmp_xml_file.write(errors_report_template)
+
+        xml_writer.add_error(close=False, message="error statistics:", type="http error")
 
         errors_count = str(self.count_errors(summary_kpiset))
 
@@ -228,30 +231,11 @@ class JUnitXMLReporter(Reporter, AggregatorListener):
                                   "Failures: {fail}, Errors: {errors}\n"
         summary_report = summary_report_template.format(success=succ, throughput=throughput, fail=fail,
                                                         errors=errors_count)
-        tmp_xml_file.write(summary_report)
+        xml_writer.raw_write(summary_report)
 
-        self.write_errors(tmp_xml_file, summary_kpiset)
-        # summary_report += error_report
-
-        # generate xml root element <testsuite>
-
-
-        # root_xml_element = etree.Element("testsuite", name="taurus_sample-labels", tests=throughput,
-        #                                  failures=fail, skip="0")
-
-
-        # summary_test_case = etree.SubElement(root_xml_element, "testcase", class_name="summary",
-        #                                      name="summary_report")
-
-
-        # etree.SubElement(summary_test_case, "error", type="http error",
-        #                  message="error statistics:").text = summary_report
-
-
-        tmp_xml_file.write("    </error>\n")
-        tmp_xml_file.write("  </testcase>\n")
-
-        return tmp_xml_file.name
+        self.write_errors(xml_writer, summary_kpiset)
+        xml_writer.close_element()
+        xml_writer.close_element()
 
     def count_errors(self, summary_kpi_set):
         """
@@ -267,7 +251,7 @@ class JUnitXMLReporter(Reporter, AggregatorListener):
 
         return err_counter
 
-    def write_errors(self, tmp_xml_file, summary_kpi_set):
+    def write_errors(self, xml_writer, summary_kpi_set):
         """
         Writes error descriptions in summary_report
         :return:
@@ -276,11 +260,10 @@ class JUnitXMLReporter(Reporter, AggregatorListener):
         url_err_template = "URL: {url}, Error count {cnt}\n"
         urls_err_string = ""
         for error in summary_kpi_set[KPISet.ERRORS]:
-            tmp_xml_file.write(err_template.format(rc=error['rc'], msg=error['msg'], cnt=error['cnt']))
+            xml_writer.raw_write(err_template.format(rc=error['rc'], msg=error['msg'], cnt=error['cnt']))
             for _url, _err_count in error["urls"].items():
                 urls_err_string += url_err_template.format(url=_url, cnt=str(_err_count))
-                tmp_xml_file.write(urls_err_string)
-
+                xml_writer.raw_write(urls_err_string)
 
     @staticmethod
     def __convert_label_name(url):
@@ -302,7 +285,7 @@ class JUnitXMLReporter(Reporter, AggregatorListener):
                                       parsed_url.fragment.replace(".", "_")])
         else:
             class_name = url
-            resource_name = ""
+            resource_name = url
         return class_name, resource_name
 
     # def __save_report(self, root_node):
@@ -335,82 +318,6 @@ class JUnitXMLReporter(Reporter, AggregatorListener):
         :return:
         """
         os.rename(tmp_name, orig_name)
-
-    @staticmethod
-    def __make_summary_error_report(summary_kpi_set):
-        """
-        Makes summary error report
-        :return: str
-        """
-        err_template = "Error code: {rc}, Message: {msg}, count: {cnt}\n"
-        url_err_template = "URL: {url}, Error count {cnt}\n"
-        error_report = ""  # errors with descriptions and urls (summary report)
-        err_counter = 0  # used in summary report (summary report)
-
-        for error in summary_kpi_set[KPISet.ERRORS]:
-            error_report += err_template.format(rc=error['rc'], msg=error['msg'], cnt=error['cnt'])
-            urls_err_string = ""
-            # enumerate urls and count errors (from Counter object)
-            for _url, _err_count in error["urls"].items():
-                err_counter += _err_count
-                urls_err_string += url_err_template.format(url=_url, cnt=str(_err_count))
-                error_report += urls_err_string
-
-        return str(err_counter), error_report
-
-    # def __make_xml_header(self, summary_kpi_set):
-    #     """
-    #     get summary KPI, generate root_xml_element and summary report for erros.
-    #     used in __process_sample_labels
-    #     :return:
-    #     etree xml root node
-    #     """
-    #     summary_report_template = "Success: {success}, Sample count: {throughput}, " \
-    #                               "Failures: {fail}, Errors: {errors}\n"
-    #     succ = str(summary_kpi_set[KPISet.SUCCESSES])
-    #     throughput = str(summary_kpi_set[KPISet.SAMPLE_COUNT])
-    #     fail = str(summary_kpi_set[KPISet.FAILURES])
-    #     errors, error_report = JUnitXMLReporter.__make_summary_error_report(summary_kpi_set)
-    #     summary_report = summary_report_template.format(success=succ, throughput=throughput, fail=fail,
-    #                                                     errors=errors)
-    #     summary_report += error_report
-    #
-    #     # generate xml root element <testsuite>
-    #     root_xml_element = etree.Element("testsuite", name="taurus_sample-labels", tests=throughput,
-    #                                      failures=fail, skip="0")
-    #     summary_test_case = etree.SubElement(root_xml_element, "testcase", class_name="summary",
-    #                                          name="summary_report")
-    #     etree.SubElement(summary_test_case, "error", type="http error",
-    #                      message="error statistics:").text = summary_report
-    #
-    #     return root_xml_element
-    #
-    # def __process_sample_labels(self):
-    #     """
-    #     :return: etree xml root element
-    #     """
-    #     root_xml_element = None
-    #     # _kpiset - cumulative test data, type: KPISet
-    #     _kpiset = self.last_second[DataPoint.CUMULATIVE]
-    #     # enumerate all sample-labels, blank url is a summary data
-    #     for key in sorted(_kpiset.keys()):
-    #         if key == "":
-    #             summary_kpiset = _kpiset[key]
-    #             root_xml_element = self.__make_xml_header(summary_kpiset)
-    #         else:  # if label is not blank
-    #             class_name, resource_name = JUnitXMLReporter.__convert_label_name(key)
-    #             # generate <testcase> subelement
-    #             test_case = etree.SubElement(root_xml_element, "testcase", classname=class_name,
-    #                                          name=resource_name, time="0")
-    #             # if any errors in label report, generate <error> subelement with error description
-    #             if _kpiset[key][KPISet.ERRORS]:
-    #                 for er_dict in _kpiset[key][KPISet.ERRORS]:
-    #                     err_message = str(er_dict["rc"])
-    #                     err_type = str(er_dict["msg"])
-    #                     err_desc = "total errors of this type:" + str(er_dict["cnt"])
-    #                     etree.SubElement(test_case, "error", type=err_type,
-    #                                      message=err_message).text = err_desc
-    #     return root_xml_element
 
     def __process_pass_fail(self):
         """
@@ -450,3 +357,52 @@ class JUnitXMLReporter(Reporter, AggregatorListener):
 
         # FIXME: minor fix criteria representation in report
         return root_xml_element
+
+
+class JUnitXMLWriter(object):
+    """
+    Writes report in JUnitXML format in file
+    """
+
+    def __init__(self, fds):
+        self.fds = fds
+        self.endings = []
+        self.write_header()
+
+    def add_element(self, element_name, text="", close=True, **kwargs):
+        """
+        adds element
+        :param element_name:
+        :param kwargs:
+        :return:
+        """
+        self.fds.write("<%s" % element_name)
+        for k, v in kwargs.items():
+            self.fds.write(" %s='%s'" % (k, v))
+        self.fds.write(">\n")
+        if text:
+            self.fds.write(text)
+            self.fds.write("\n")
+        if close:
+            self.fds.write("</%s>\n" % element_name)
+        else:
+            self.endings.append("</%s>\n" % element_name)
+
+    def add_testsuite(self, close=True, **kwargs):
+        self.add_element("testsuite", close=close, **kwargs)
+
+    def add_testcase(self, close=True, **kwargs):
+        self.add_element("testcase", close=close, **kwargs)
+
+    def add_error(self, close=True, **kwargs):
+        self.add_element("error", close=close, **kwargs)
+
+    def close_element(self):
+        if self.endings:
+            self.fds.write(self.endings.pop())
+
+    def write_header(self):
+        self.fds.write("<?xml version='1.0' encoding='UTF-8'?>\n")
+
+    def raw_write(self, data):
+        self.fds.write(data)
