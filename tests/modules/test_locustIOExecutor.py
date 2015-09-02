7accdb05b287eb2d1f9dd7c21342fd210016e4a8
vasischev <vasischev@blazemeter.com>
2015-09-02 17:48:13 +0300

    draft

--- tests/modules/test_locustIOExecutor.py
+++ tests/modules/test_locustIOExecutor.py
@@ -59,3 +59,19 @@ class TestLocustIOExecutor(BZTestCase):
         self.assertEqual(obj.widget.duration, 30)
         self.assertTrue(obj.widget.widgets[0].text.endswith("simple.py"))
         obj.shutdown()
+
+    def test_locust_resource_files(self):
+        obj = LocustIOExecutor()
+        obj.engine = EngineEmul()
+        obj.engine.config['provisioning'] = 'local'
+        obj.execution.merge({
+            "concurrency": 1,
+            "iterations": 10,
+            "hold-for": 30,
+            "scenario": {
+                "default-address": "http://blazedemo.com",
+                "script": __dir__() + "/../locust/simple.py"
+            }
+        })
+        resource_files = obj.resource_files()
+        self.assertEqual(1, len(resource_files))
