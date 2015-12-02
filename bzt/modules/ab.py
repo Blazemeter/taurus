26efaa13839c8ae92ff0a5afc70a94317747b8c2
fenrir <fenrir@gecko>
2015-12-02 14:46:01 +0300

    intermittent a

---
+++ bzt/modules/ab.py
@@ -0,0 +1,153 @@
+"""
+Module holds all stuff regarding Apache Benchmark tool usage
+
+Copyright 2015 BlazeMeter Inc.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+   http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+"""
+
+import platform
+import time
+import traceback
+import subprocess
+from subprocess import CalledProcessError
+from bzt.engine import ScenarioExecutor
+from bzt.modules.aggregator import ResultsReader, ConsolidatingAggregator
+from bzt.utils import shutdown_process, shell_exec, RequiredTool
+
+EXE_SUFFIX = ".exe" if platform.system() == "Windows" else ""
+
+
+class ABExecutor(ScenarioExecutor):
+    """
+    AB executor module
+    """
+
+    def __init__(self):
+        super(ABExecutor, self).__init__()
+        self.process = None
+        self.start_time = None
+        self.end_time = None
+        self.reader = None
+        self.log.warning("___ABExecutor.__init__!")
+
+    def prepare(self):
+        self._check_installed()
+        self.log.warning("___ABExecutor.prepare!")
+        self.reader = DataLogReader(self.engine.artifacts_dir, self.log)
+        if isinstance(self.engine.aggregator, ConsolidatingAggregator):
+            self.engine.aggregator.add_underling(self.reader)
+
+    def _check_installed(self):
+        ab_path = "ab" + EXE_SUFFIX
+        required_tools = [AB(ab_path, self.log)]
+        for tool in required_tools:
+            if not tool.check_if_installed():
+                self.log.info("Installing %s", tool.tool_name)
+                tool.install()
+
+    def startup(self):
+        self.log.warning("___ABExecutor.startup!")
+
+        self.start_time = time.time()
+
+        concurrency = 10
+        iteration = 10
+        requests = self.get_scenario().get("requests", ["ya.ru"])
+
+        request = requests[0] + '/'  # TODO: process many requests
+
+        cmd_line = "ab -c %s -n %s %s" % (concurrency, iteration, request)
+        ab = shell_exec(cmd_line, stderr=subprocess.STDOUT)
+        ab_out, ab_err = ab.communicate()
+        ab_out = ab_out.split()
+        self.log.warning("%s" % ab_out)
+        self.reader.output = [int(time.time()), request, concurrency,
+                              float(self._val(ab_out, 'tests:')),
+                              float(self._val(ab_out, 'tests:')),
+                              float(self._val(ab_out, 'tests:')),
+                              0, None, '']
+
+    def _val(self, outlist, key):
+        return outlist[outlist.index(key) + 1]
+
+    def check(self):
+        self.log.warning("___ABExecutor.check!")
+        return True
+
+    def shutdown(self):
+        """
+        If tool is still running - let's stop it.
+        """
+        self.log.warning("___ABExecutor.shutdown!")
+        shutdown_process(self.process, self.log)
+
+        if self.start_time:
+            self.end_time = time.time()
+            self.log.debug("AB worked for %s seconds", self.end_time - self.start_time)
+
+
+class DataLogReader(ResultsReader):
+    """ Class to read ___ """
+
+    def __init__(self, basedir, parent_logger):
+        super(DataLogReader, self).__init__()
+        self.log = parent_logger.getChild(self.__class__.__name__)
+        self.output = None
+
+    def _read(self, last_pass=False):
+        """
+        Generator method that returns next portion of data
+
+        :param last_pass:
+        :return: timestamp, label, concurrency, rt, latency, rc, error
+        """
+        # while not last_pass:
+        #     yield None
+        #
+        # a = iter(((1, 'abc', 2, 3, 4, 5, 6, None, ''),
+        #           (2, 'abd', 12, 13, 14, 15, 16, None, ''),
+        #           (4, 'abe', 22, 23, 24, 25, 26, None, ''),))
+        #
+        # self.log.warning("%s" % self.output.split())
+        #
+        # while a:
+        #     yield a.next()
+        return self.output, None
+
+
+class AB(RequiredTool):
+    """
+    AB tool
+    """
+
+    def __init__(self, tool_path, parent_logger):
+        super(AB, self).__init__("AB", tool_path)
+        self.log = parent_logger.getChild(self.__class__.__name__)
+
+    def check_if_installed(self):
+        self.log.debug("Trying AB: %s", self.tool_path)
+        try:
+            ab = shell_exec([self.tool_path], stderr=subprocess.STDOUT)
+            ab_out, ab_err = ab.communicate()
+            self.log.debug("AB check: %s", ab_out)
+            if ab_err:
+                self.log.warning("AB check stderr: %s", ab_err)
+            return True
+        except (CalledProcessError, OSError):
+            self.log.debug("Check failed: %s", traceback.format_exc())
+            self.log.error("Apache Benchmark check failed. Consider installing it")
+            return False
+
+    def install(self):
+        raise RuntimeError("Please install Apache Benchmark tool manually")
