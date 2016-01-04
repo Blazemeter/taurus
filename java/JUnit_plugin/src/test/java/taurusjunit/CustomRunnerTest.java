031e75385183370438f9efc21624250cb0004885
Andrey Pohilko <apc4@ya.ru>
2016-01-04 17:05:24 +0300

    cover iterations+hold case

--- java/JUnit_plugin/src/test/java/taurusjunit/CustomRunnerTest.java
+++ java/JUnit_plugin/src/test/java/taurusjunit/CustomRunnerTest.java
@@ -97,4 +97,33 @@ public class CustomRunnerTest extends TestCase {
         assertTrue(3 < getLinesCount(log));
         assertTrue(8 < getLinesCount(err));
     }
+
+    public void testHoldIterations() throws Exception {
+        CustomRunner obj = new CustomRunner();
+        File log = File.createTempFile("log", ".jtl");
+        log.deleteOnExit();
+
+        File err = File.createTempFile("err", ".jtl");
+        err.deleteOnExit();
+
+        URL res = Thread.currentThread().getContextClassLoader().getResource("dummy.jar");
+        assert res != null;
+
+        Properties props = new Properties();
+        props.setProperty(CustomRunner.KPI_LOG, log.getAbsolutePath());
+        props.setProperty(CustomRunner.ERROR_LOG, err.getAbsolutePath());
+        props.setProperty(CustomRunner.TARGET_PREFIX + "jar", res.getPath());
+        props.setProperty(CustomRunner.HOLD, String.valueOf(60));
+        props.setProperty(CustomRunner.ITERATIONS, String.valueOf(1));
+
+        File propsFile = File.createTempFile("runner", ".properties");
+        propsFile.deleteOnExit();
+        props.store(new FileWriter(propsFile), "test");
+
+        String[] args = {propsFile.getAbsolutePath()};
+        obj.main(args);
+
+        assertEquals(3, getLinesCount(log));
+        assertEquals(8, getLinesCount(err));
+    }
 }
