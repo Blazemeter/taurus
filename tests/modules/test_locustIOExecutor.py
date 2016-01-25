40758758b189b49d5284e04396b9e39c96615f34
greyfenrir <grey.fenrir@gmail.com>
2016-01-25 12:51:37 +0300

    as master

--- tests/modules/test_locustIOExecutor.py
+++ tests/modules/test_locustIOExecutor.py
@@ -11,4 +11,134 @@ from tests.mocks import EngineEmul
 
 class TestLocustIOExecutor(BZTestCase):
     def setUp(self):
-        pass
+        sys.path.append(__dir__() + "/../locust/")
+
+    def test_simple(self):
+        if six.PY3:
+            logging.warning("No locust available for python 3")
+
+        obj = LocustIOExecutor()
+        obj.engine = EngineEmul()
+        obj.engine.config['provisioning'] = 'local'
+        obj.execution.merge({
+            "concurrency": 1,
+            "iterations": 10,
+            "scenario": {
+                "default-address": "http://blazedemo.com",
+                "script": __dir__() + "/../locust/simple.py"
+            }
+        })
+
+        obj.prepare()
+        obj.startup()
+        try:
+            while not obj.check():
+                time.sleep(obj.engine.check_interval)
+        except RuntimeError:  # FIXME: not good, but what to do?
+            pass
+        obj.shutdown()
+        self.assertRaises(RuntimeWarning, obj.post_process)
+
+    def test_locust_widget(self):
+        if six.PY3:
+            logging.warning("No locust available for python 3")
+
+        obj = LocustIOExecutor()
+        obj.engine = EngineEmul()
+        obj.engine.config['provisioning'] = 'local'
+        obj.execution.merge({
+            "concurrency": 1,
+            "iterations": 10,
+            "hold-for": 30,
+            "scenario": {
+                "default-address": "http://blazedemo.com",
+                "script": __dir__() + "/../locust/simple.py"
+            }
+        })
+
+        obj.prepare()
+        obj.startup()
+        obj.get_widget()
+        obj.check()
+        self.assertEqual(obj.widget.duration, 30)
+        self.assertTrue(obj.widget.widgets[0].text.endswith("simple.py"))
+        obj.shutdown()
+
+    def test_locust_master(self):
+        if six.PY3:
+            logging.warning("No locust available for python 3")
+
+        obj = LocustIOExecutor()
+        obj.engine = EngineEmul()
+        obj.engine.config['provisioning'] = 'local'
+        obj.execution.merge({
+            "concurrency": 1,
+            "iterations": 10,
+            "hold-for": 30,
+            "master": True,
+            "slaves": 1,
+            "scenario": {
+                "default-address": "http://blazedemo.com",
+                "script": __dir__() + "/../locust/simple.py"
+            }
+        })
+
+        obj.prepare()
+        obj.startup()
+        obj.get_widget()
+        try:
+            obj.check()
+            time.sleep(2)
+            obj.check()
+        except RuntimeError:
+            logging.warning("Do you use patched locust for non-GUI master?")
+        obj.shutdown()
+        self.assertRaises(RuntimeWarning, obj.post_process)
+
+    def test_locust_slave_results(self):
+        if six.PY3:
+            logging.warning("No locust available for python 3")
+
+        obj = SlavesReader(__dir__() + "/../locust/locust-slaves.ldjson", 2, logging.getLogger(""))
+        points = [x for x in obj.datapoints(True)]
+        self.assertEquals(107, len(points))
+        for point in points:
+            self.assertGreater(point[DataPoint.CURRENT][''][KPISet.AVG_RESP_TIME], 0)
+
+    def test_locust_resource_files(self):
+        if six.PY3:
+            logging.warning("No locust available for python 3")
+
+        obj = LocustIOExecutor()
+        obj.engine = EngineEmul()
+        obj.engine.config['provisioning'] = 'local'
+        obj.execution.merge({
+            "concurrency": 1,
+            "iterations": 10,
+            "hold-for": 30,
+            "scenario": {
+                "default-address": "http://blazedemo.com",
+                "script": __dir__() + "/../locust/simple.py"
+            }
+        })
+        resource_files = obj.resource_files()
+        self.assertEqual(1, len(resource_files))
+
+    def test_fail_on_zero_results(self):
+        if six.PY3:
+            logging.warning("No locust available for python 3")
+
+        obj = LocustIOExecutor()
+        obj.engine = EngineEmul()
+        obj.engine.config['provisioning'] = 'local'
+        obj.execution.merge({
+            "concurrency": 1,
+            "iterations": 10,
+            "hold-for": 30,
+            "scenario": {
+                "default-address": "http://blazedemo.com",
+                "script": __dir__() + "/../locust/simple.py"
+            }
+        })
+        obj.prepare()
+        self.assertRaises(RuntimeWarning, obj.post_process)
--- tests/modules/test_pbench.py
+++ tests/modules/test_pbench.py
@@ -17,4 +17,221 @@ from tests.mocks import EngineEmul
 if not is_windows():
     class TestPBench(BZTestCase):
         def test_simple(self):
-            pass
+            obj = PBenchExecutor()
+            obj.engine = EngineEmul()
+            obj.engine.aggregator = ConsolidatingAggregator()
+            obj.engine.aggregator.add_listener(DataPointLogger())
+            obj.engine.config.merge({"provisioning": "test"})
+
+            if os.path.exists("/home/undera/Sources/phantom"):  # FIXME: not good, get rid of it
+                obj.settings.merge({
+                    "path": "/home/undera/Sources/phantom/bin/phantom",
+                    "modules-path": "/home/undera/Sources/phantom/lib/phantom",
+                })
+            else:
+                obj.settings.merge({
+                    "path": os.path.join(os.path.dirname(__file__), '..', "phantom.sh"),
+                })
+
+            obj.execution.merge({
+                "log-responses": "proto_error",
+                # "iterations": 5000000,
+                "concurrency": 10,
+                "throughput": 1000,
+                "ramp-up": "1m",
+                # "steps": 5,
+                "hold-for": "15",
+                "scenario": {
+                    "timeout": 1,
+                    "default-address": "http://localhost:33",
+                    "headers": {
+                        "Connection": "close"
+                    },
+                    "requests": [
+                        # "/",
+                        {
+                            "url": "/api",
+                            "method": "POST",
+                            "headers": {
+                                "Content-Length": 0
+                            },
+                            "body": {
+                                "param": "value"
+                            }
+                        }
+
+                    ]
+                }
+            })
+            obj.engine.aggregator.prepare()
+            obj.prepare()
+
+            obj.engine.aggregator.startup()
+            obj.startup()
+
+            while not obj.check():
+                logging.debug("Running...")
+                obj.engine.aggregator.check()
+                time.sleep(1)
+
+            obj.shutdown()
+            obj.engine.aggregator.shutdown()
+
+            obj.post_process()
+            obj.engine.aggregator.post_process()
+
+        def test_schedule_rps(self):
+            executor = PBenchExecutor()
+            executor.engine = EngineEmul()
+            executor.engine.config.merge({"provisioning": "test"})
+            rps = 9
+            rampup = 12
+            executor.execution.merge({"throughput": rps, "ramp-up": rampup, "steps": 3, "hold-for1": 0})
+            obj = Scheduler(executor.get_load(), StringIO("4 test\ntest\n"), logging.getLogger(""))
+
+            cnt = 0
+            cur = 0
+            currps = 0
+            for item in obj.generate():
+                # logging.debug("Item: %s", item)
+                if int(math.ceil(item[0])) != cur:
+                    # self.assertLessEqual(currps, rps)
+                    cur = int(math.ceil(item[0]))
+                    logging.debug("RPS: %s", currps)
+                    currps = 0
+
+                cnt += 1
+                currps += 1
+
+            logging.debug("RPS: %s", currps)
+
+        def test_schedule_empty(self):
+            executor = PBenchExecutor()
+            executor.engine = EngineEmul()
+            obj = Scheduler(executor.get_load(), StringIO("4 test\ntest\n"), logging.getLogger(""))
+            for item in obj.generate():
+                logging.debug("Item: %s", item)
+
+        def test_schedule_concurrency(self):
+            executor = PBenchExecutor()
+            executor.engine = EngineEmul()
+            executor.execution.merge({"concurrency": 5, "ramp-up": 10, "hold-for": 5})
+            obj = Scheduler(executor.get_load(), StringIO("5 test1\ntest1\n5 test2\ntest2\n"), logging.getLogger(""))
+            items = list(obj.generate())
+            logging.debug("%s", items)
+            self.assertEqual(8, len(items))
+            self.assertEqual(-1, items[5][0])  # instance became unlimited
+            self.assertEqual(1, items[6][5])  # looped payload
+
+        def test_schedule_concurrency_steps(self):
+            executor = PBenchExecutor()
+            executor.engine = EngineEmul()
+            executor.execution.merge({"concurrency": 5, "ramp-up": 10, "steps": 3})
+            obj = Scheduler(executor.get_load(), StringIO("5 test1\ntest1\n5 test2\ntest2\n"), logging.getLogger(""))
+            items = list(obj.generate())
+            logging.debug("%s", items)
+            self.assertEqual(8, len(items))
+            self.assertEqual(-1, items[5][0])  # instance became unlimited
+            self.assertEqual(1, items[6][5])  # looped payload
+
+        def test_widget(self):
+            obj = PBenchExecutor()
+            obj.engine = EngineEmul()
+            obj.settings = BetterDict()
+            obj.engine.config.merge({
+                "provisioning": "test",
+                ScenarioExecutor.EXEC: [
+                    {
+                        "throughput": 10,
+                        "hold-for": 30,
+                        "scenario": {
+                            "default-address": "http://blazedemo.com/",
+                            "requests": ["/"]
+                        }
+                    }
+                ]})
+            obj.execution = obj.engine.config['execution'][0]
+            obj.settings.merge({
+                "path": os.path.join(os.path.dirname(__file__), '..', "phantom.sh"),
+            })
+            obj.prepare()
+            obj.startup()
+            obj.get_widget()
+            self.assertTrue(isinstance(obj.widget.progress, urwid.ProgressBar))
+            self.assertEqual(obj.widget.duration, 30)
+            self.assertEqual(obj.widget.widgets[0].text, "Target: http://blazedemo.com:80")
+            obj.check()
+            obj.shutdown()
+
+        def test_improved_request_building(self):
+            obj = PBenchExecutor()
+            obj.engine = EngineEmul()
+            obj.settings = BetterDict()
+            obj.engine.config = BetterDict()
+            obj.engine.config.merge(yaml.load(open(__dir__() + "/../yaml/phantom_improved_request.yml").read()))
+            obj.execution = obj.engine.config['execution'][0]
+            obj.settings.merge({
+                "path": os.path.join(os.path.dirname(__file__), '..', "phantom.sh"),
+            })
+            obj.prepare()
+            with open(obj.pbench.schedule_file) as fds:
+                config = fds.readlines()
+
+            get_requests = [req_str.split(" ")[1] for req_str in config if req_str.startswith("GET")]
+            self.assertEqual(len(get_requests), 2)
+
+            for get_req in get_requests:
+                self.assertEqual(dict(parse.parse_qsl(parse.urlsplit(get_req).query)),
+                                 {"get_param1": "value1", "get_param2": "value2"})
+
+        def test_same_address_port(self):
+            obj = PBenchExecutor()
+            obj.engine = EngineEmul()
+            obj.settings = BetterDict()
+            obj.engine.config = BetterDict()
+            obj.engine.config.merge(yaml.load(open(__dir__() + "/../yaml/phantom_request_same_address.yml").read()))
+            obj.execution = obj.engine.config['execution'][0]
+            obj.settings.merge({
+                "path": os.path.join(os.path.dirname(__file__), '..', "phantom.sh"),
+            })
+            try:
+                obj.prepare()
+                self.fail()
+            except ValueError:
+                pass
+
+        def test_install_pbench(self):
+            obj = PBenchExecutor()
+            obj.engine = EngineEmul()
+            obj.settings = BetterDict()
+            obj.engine.config = BetterDict()
+            obj.settings.merge({"path": "/notexistent"})
+            # obj.execution = obj.engine.config['execution'][0]
+            try:
+                obj.prepare()
+                self.fail()
+            except RuntimeError as exc:
+                self.assertEquals("Please install PBench tool manually", str(exc))
+
+        def test_pbench_file_lister(self):
+            obj = PBenchExecutor()
+            obj.engine = EngineEmul()
+            obj.settings = BetterDict()
+            obj.engine.config = BetterDict()
+            obj.engine.config.merge(
+                    {ScenarioExecutor.EXEC: {"executor": "pbench", "scenario": {"script": "/opt/data/script.src"}}})
+            obj.execution = obj.engine.config['execution']
+            obj.settings.merge({
+                "path": os.path.join(os.path.dirname(__file__), '..', "phantom.sh"),
+            })
+            resource_files = obj.resource_files()
+            self.assertEqual(1, len(resource_files))
+            self.assertEqual(resource_files[0], 'script.src')
+
+
+class DataPointLogger(AggregatorListener):
+    def aggregated_second(self, data):
+        current = data[DataPoint.CURRENT]['']
+        logging.info("DataPoint %s: VU:%s RPS:%s/%s RT:%s", data[DataPoint.TIMESTAMP],
+                     current[KPISet.CONCURRENCY], current[KPISet.SAMPLE_COUNT], current[KPISet.FAILURES],
+                     current[KPISet.AVG_RESP_TIME])
