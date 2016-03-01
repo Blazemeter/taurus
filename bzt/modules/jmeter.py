68dc255fb79352f30f07bd976d2b9b1691bb3fef
greyfenrir <grey.fenrir@gmail.com>
2016-03-01 17:33:50 +0300

    just test it

--- bzt/modules/jmeter.py
+++ bzt/modules/jmeter.py
@@ -823,7 +823,6 @@ class JTLReader(ResultsReader):
     Class to read KPI JTL
     :type errors_reader: JTLErrorsReader
     """
-
     def __init__(self, filename, parent_logger, errors_filename):
         super(JTLReader, self).__init__()
         self.is_distributed = False
