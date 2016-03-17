a67c86cedce63b7cde48b03ca38b41ad6a03947f
Dmitry Pribysh <dmand@yandex.ru>
2016-03-17 11:22:55 +0300

    Protect data-sources against being None-ish value

--- bzt/modules/jmeter.py
+++ bzt/modules/jmeter.py
@@ -1349,6 +1349,8 @@ class JMeterScenarioBuilder(JMX):
 
     def __add_datasources(self):
         sources = self.scenario.get("data-sources", [])
+        if not sources:
+            return
         if not isinstance(sources, list):
             raise ValueError("data-sources is not a list")
         for idx, source in enumerate(sources):
