57b85ad6e46781501f4d3eeda770d02861e3905f
greyfenrir <grey.fenrir@gmail.com>
2016-04-01 13:27:44 +0300

    fix local_provisioning: move start_time setup and executors delay setup

--- bzt/modules/provisioning.py
+++ bzt/modules/provisioning.py
@@ -47,14 +47,14 @@ class Local(Provisioning):
             except ValueError:
                 continue
             except TypeError:
-                self.log.warning('Start time must be string type, ignored')
+                self.log.warning('Start time must be string type ("%s"), ignored', time_format[0])
                 break
             today = datetime.date.today()
             if today > date.date():
                 date = datetime.datetime(today.year, today.month, today.day, date.hour, date.minute, date.second)
             return time.mktime(date.timetuple()) - self.start_time
         else:
-            self.log.warning('Unrecognized time format: %s, ignored', shift)
+            self.log.warning('Unrecognized time format: %s ("%s" required), ignored', shift, time_formats[0])
 
         return 0
 
@@ -63,22 +63,21 @@ class Local(Provisioning):
         Call prepare on executors
         """
         super(Local, self).prepare()
-
         self.start_time = time.time()
-
         for executor in self.executors:
             self.log.debug("Preparing executor: %s", executor)
             executor.prepare()
             self.engine.prepared.append(executor)
 
+    def startup(self):
+        self.start_time = time.time()
+
+        for executor in self.executors:
             user_time_format = executor.execution.get('time-format', '')
             start_shift = self._get_start_shift(executor.execution.get('start-at', ''), user_time_format)
-            delay = dehumanize_time(executor.execution.get('delay', '0'))
+            delay = dehumanize_time(executor.execution.get('delay', 0))
             executor.delay = delay + start_shift
             self.log.debug("Delay setup: %s(start-at) + %s(delay) = %s", start_shift, delay, executor.delay)
-
-    def startup(self):
-        pass
 
     def _start_modules(self):
         for executor in self.executors:
