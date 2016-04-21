a2e351de60633c08ecc3b256b9580fe5ad5abb3d
greyfenrir <grey.fenrir@gmail.com>
2016-04-21 11:30:07 +0300

    Add functional test

--- virtualenv-test.bash
+++ virtualenv-test.bash
@@ -1,5 +1,7 @@
 #!/bin/bash -xe
 
+rm -rf build 
+
 # setup env
 virtualenv --clear build
 source build/bin/activate
@@ -10,7 +12,7 @@ pip install --upgrade colorlog pyyaml psutil lxml cssselect nose urwid coverage 
 pylint -d R0903,R0904,C0301,C0302,C0111,E1103,R0201,R0902,W0511,F0401,E0611,R0801,R0913,W0613,C0412 -f parseable bzt > build/pylint.out || echo
 
 # run unit tests
-./run-test.sh
+#./run-test.sh
 
 # re-setup env
 deactivate
@@ -26,5 +28,7 @@ echo '{"install-id": "UnitTest"}' > build/etc/bzt.d/99-zinstallID.json
 
 # run functional tests
 bzt -o execution.scenario.script=tests/jmeter/jmx/dummy.jmx -o settings.artifacts-dir="build/test/%Y-%m-%d_%H-%M-%S.%f" -o modules.jmeter.path=build/jmeter/bin/jmeter
-bzt tests/json/get-post.json -o settings.artifacts-dir="build/test/%Y-%m-%d_%H-%M-%S.%f" -o modules.jmeter.path=build/jmeter/bin/jmeter
+bzt -o settings.artifacts-dir="build/test/%Y-%m-%d_%H-%M-%S.%f" -o modules.jmeter.path=build/jmeter/bin/jmeter tests/json/get-post.json
 
+#install gatling
+bzt.sh -o settings.artifacts-dir="build/test/%Y-%m-%d_%H-%M-%S.%f" -o modules.gatling.path=build/gatling/bin/gatling.sh  tests/yaml/func_test/gatling.yml
