6f94e539bb60435769f1f807d06e2f885cdaa615
Dmitry Pribysh <dmand@yandex.ru>
2016-06-01 17:21:14 +0300

    Quote dollar-sign characters in installer script

--- build-windows-installer.sh
+++ build-windows-installer.sh
@@ -40,14 +40,14 @@ cat << EOF > taurus.nsi
   IfFileExists "C:\etc\bzt.d\99-installID.yml" file_found file_not_found
   file_not_found:
   nsExec::ExecToStack '[[ python ]] -c "import uuid; print uuid.uuid4()"'
-  Pop $0  ; return code
-  Pop $1  ; output
-  FileOpen $9 C:\etc\bzt.d\99-installID.yml w
-  FileWrite $9 "---$\r$\n"
-  FileWrite $9 "install-id: "
-  FileWrite $9 $1
-  FileWrite $9 "$\r$\n"
-  FileClose $9
+  Pop \$0  ; return code
+  Pop \$1  ; output
+  FileOpen \$9 C:\etc\bzt.d\99-installID.yml w
+  FileWrite \$9 "---\$\r\$\n"
+  FileWrite \$9 "install-id: "
+  FileWrite \$9 \$1
+  FileWrite \$9 "\$\r\$\n"
+  FileClose \$9
   file_found:
 
 [% endblock %]
