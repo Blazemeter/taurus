ab33ff012c7b1371ea80754e8d5bea71f00be522
Dmitry Pribysh <dmand@yandex.ru>
2016-06-01 17:30:25 +0300

    Add urwid to the list of manually included packages

--- build-windows-installer.sh
+++ build-windows-installer.sh
@@ -19,6 +19,7 @@ ln -s "../../bzt" bzt
 wget -O lxml.whl https://pypi.python.org/packages/f1/c7/e19d317cc948095abc872a6e6ae78ac80260f2b45771dfa7a7ce86865f5b/lxml-3.6.0-cp27-none-win32.whl#md5=b2f7e0c83e48e334fcd6b5b3412f18c4
 wget -O psutil.whl https://pypi.python.org/packages/58/a5/2ccc9f6180ea769005405381f6b0d01fe1268f20cc85877b02c04c27d306/psutil-4.2.0-cp27-cp27m-win32.whl#md5=0caae8cea2d0ffe3b6ce6d2b25a9271a
 wget -O selenium.whl https://pypi.python.org/packages/1e/27/47f73510c6b80d1ff0829474947537ae9ab8d516cc48c6320b7f3677fa54/selenium-2.53.2-py2.py3-none-any.whl#md5=81ce6cbf3ed030bdaf921fae58024a4f
+wget -O urwid.tar.gz https://pypi.python.org/packages/source/u/urwid/urwid-1.3.1.tar.gz
 
 mkdir pynsist_pkgs
 
@@ -26,10 +27,12 @@ mkdir pynsist_pkgs
 7z x psutil.whl -opsutil
 7z x lxml.whl -olxml
 7z x selenium.whl -oselenium
+tar -xf urwid.tar.gz
 
 cp -r lxml/lxml pynsist_pkgs
 cp -r psutil/psutil pynsist_pkgs
 cp -r selenium/selenium pynsist_pkgs
+cp -r urwid-1.3.1/urwid pynsist_pkgs
 
 # create NSIS script
 cat << EOF > taurus.nsi
