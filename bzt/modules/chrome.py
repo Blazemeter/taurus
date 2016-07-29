4da0909404673551e1902aef00fa4219de018723
Dmitry Pribysh <dmand@yandex.ru>
2016-07-29 13:02:50 +0300

    Collect gc times and aggregate it into a 'gc-time' metric

--- bzt/modules/chrome.py
+++ bzt/modules/chrome.py
@@ -95,6 +95,7 @@ class ChromeTraceReader(object):
 class ChromeMetricExtractor(object):
     def __init__(self):
         self.tracing_start_ts = None
+        self.tracing_duration = 0.0
         self.process_names = {}  # pid -> str
         self.process_labels = {}  # pid -> str
         self.memory_per_process = defaultdict(OrderedDict)  # pid -> (ts -> float)
@@ -104,10 +105,18 @@ class ChromeMetricExtractor(object):
         self.page_load_times = {}
         self.js_heap_size_used = defaultdict(OrderedDict)  # pid -> (ts -> heap_size)
         self.js_event_listeners = defaultdict(OrderedDict)  # pid -> (ts -> int)
+        self.gc_times = defaultdict(dict)  # pid -> dict(gc_start_time, heap_before_gc, gc_end_time, heap_after_gc)
+
+    def convert_ts(self, ts):
+        if self.tracing_start_ts is not None:
+            return float(ts) / 1000000 - self.tracing_start_ts
 
     def feed_event(self, event):
         if event.get("timestamp") and self.tracing_start_ts is None:
             self.tracing_start_ts = event["timestamp"]
+        if event.get("ts"):
+            ts = self.convert_ts(event['ts'])
+            self.tracing_duration = max(self.tracing_duration, ts)
 
         if event.get("cat") == "disabled-by-default-memory-infra":
             if event.get("name") == "periodic_interval":
@@ -118,57 +127,66 @@ class ChromeMetricExtractor(object):
             self.process_network_event(event)
         elif event.get("cat") in ["devtools.timeline", "disabled-by-default-devtools.timeline"]:
             self.process_devtools_event(event)
+        elif event.get("cat") == "devtools.timeline,v8":
+            self.process_devtools_event(event)
         elif event.get("cat") == "blink.user_timing":
             self.process_user_timing_event(event)
 
     def process_user_timing_event(self, event):
         if event.get("name") == "loadEventStart":
-            ts = float(event['ts']) / 1000000
-            if self.tracing_start_ts is not None:
-                ts = ts - self.tracing_start_ts
+            ts = self.convert_ts(event['ts'])
             self.page_load_times[event['pid']] = ts
 
     def process_devtools_event(self, event):
         if event.get("name") == "ResourceSendRequest":
-            ts = float(event["ts"]) / 1000000
+            ts = self.convert_ts(event['ts'])
             request_id = event["args"]["data"]["requestId"]
             self.requests[request_id]["send_req_time"] = ts
             self.requests[request_id]["method"] = event["args"]["data"]["requestMethod"]
             self.requests[request_id]["url"] = event["args"]["data"]["url"]
         elif event.get("name") == "ResourceReceiveResponse":
-            ts = float(event["ts"]) / 1000000
+            ts = self.convert_ts(event['ts'])
             request_id = event["args"]["data"]["requestId"]
             self.requests[request_id]["recv_resp_time"] = ts
             self.requests[request_id]["mime"] = event["args"]["data"]["mimeType"]
             self.requests[request_id]["status_code"] = event["args"]["data"]["statusCode"]
         elif event.get("name") == "ResourceReceivedData":
-            ts = float(event["ts"]) / 1000000
+            ts = self.convert_ts(event['ts'])
             request_id = event["args"]["data"]["requestId"]
             self.requests[request_id]["recv_data_time"] = ts
             self.requests[request_id]["size"] = event["args"]["data"]["encodedDataLength"]
         elif event.get("name") == "ResourceFinish":
-            ts = float(event["ts"]) / 1000000
+            ts = self.convert_ts(event['ts'])
             request_id = event["args"]["data"]["requestId"]
             self.requests[request_id]["finish_time"] = ts
             self.requests[request_id]["did_fail"] = event["args"]["data"]["didFail"]
             if event["args"]["data"].get("networkTime"):
                 self.requests[request_id]["network_time"] = event["args"]["data"]["networkTime"]
         elif event.get("name") == "UpdateCounters":
-            ts = float(event["ts"]) / 1000000
+            ts = self.convert_ts(event['ts'])
             pid = event["pid"]
             self.js_event_listeners[pid][ts] = event["args"]["data"]["jsEventListeners"]
             self.js_heap_size_used[pid][ts] = float(event["args"]["data"]["jsHeapSizeUsed"]) / 1024 / 1024
+        elif event.get("name") in ["MajorGC", "MinorGC"]:
+            ts = self.convert_ts(event['ts'])
+            pid = event["pid"]
+            if event.get("ph") == "B":  # GC begin
+                self.gc_times[pid]['gc_start_time'] = ts
+                self.gc_times[pid]['heap_before_gc'] = float(event['args']['usedHeapSizeBefore']) / 1024 / 1024
+            elif event.get("ph") == "E":  # GC end
+                self.gc_times[pid]['gc_end_time'] = ts
+                self.gc_times[pid]['heap_after_gc'] = float(event['args']['usedHeapSizeAfter']) / 1024 / 1024
 
     def process_network_event(self, event):
         if event.get("name") == "Resource":
             if event.get("ph") == "S":  # download starts
-                ts = float(event["ts"]) / 1000000
+                ts = self.convert_ts(event['ts'])
                 download_id = event["id"]
                 request_id = event["args"]["data"]["requestId"]
                 self.resources[download_id]["start_time"] = ts
                 self.resources[download_id]["request_id"] = request_id
             elif event.get("ph") == "F":  # download finished
-                ts = float(event["ts"]) / 1000000
+                ts = self.convert_ts(event['ts'])
                 download_id = event["id"]
                 self.resources[download_id]["end_time"] = ts
 
@@ -181,9 +199,7 @@ class ChromeMetricExtractor(object):
 
     def process_memory_event(self, event):
         pid = event["pid"]
-        ts = float(event["ts"]) / 1000000
-        if self.tracing_start_ts is not None:
-            ts = ts - self.tracing_start_ts
+        ts = self.convert_ts(event['ts'])
         dumps = event['args']['dumps']
         if 'process_totals' not in dumps:
             return
@@ -213,20 +229,28 @@ class ChromeMetricExtractor(object):
     def calc_user_timing_metrics(self):
         for pid, page_load_ts in iteritems(self.page_load_times):
             if pid in self.process_labels:  # if it's a renderer process for a tab
-                yield 0.0, "page-load", page_load_ts
-                break  # NOTE: should we break here?
+                yield self.tracing_duration, "page-load", page_load_ts
 
     def calc_js_metrics(self):
         for pid in sorted(self.js_heap_size_used):
             if pid in self.process_labels:
                 for ts, value in iteritems(self.js_heap_size_used[pid]):
-                    metric = '%s-js-heap-size-used' % pid
+                    metric = 'js-heap-size-used'
                     yield ts, metric, value
         for pid in sorted(self.js_event_listeners):
             if pid in self.process_labels:
                 for ts, value in iteritems(self.js_event_listeners[pid]):
-                    metric = '%s-js-event-listeners' % pid
+                    metric = 'js-event-listeners'
                     yield ts, metric, value
+
+    def calc_gc_metrics(self):
+        total_gc_time = 0.0
+        for pid, gc_record in iteritems(self.gc_times):
+            if pid in self.process_labels:
+                if 'gc_start_time' in gc_record and 'gc_end_time' in gc_record:
+                    gc_duration = gc_record['gc_end_time'] - gc_record['gc_start_time']
+                    total_gc_time += gc_duration
+        yield self.tracing_duration, 'gc-time', total_gc_time
 
     def get_metrics(self):
         # yields (offset, metric, value)
@@ -234,7 +258,8 @@ class ChromeMetricExtractor(object):
         for metric in itertools.chain(self.calc_memory_metrics(),
                                       self.calc_network_metrics(),
                                       self.calc_user_timing_metrics(),
-                                      self.calc_js_metrics()):
+                                      self.calc_js_metrics(),
+                                      self.calc_gc_metrics()):
             yield metric
 
 
@@ -264,13 +289,18 @@ class ChromeLogClient(MonitoringClient):
 
         res = []
         for offset, metric, value in self.extractor.get_metrics():
-            self.log.info("%s: %s=%s", offset, metric, value)
+            if isinstance(value, float):
+                svalue = "%.6f" % value
+            else:
+                svalue = str(value)
+            self.log.info("%s: %s=%s", offset, metric, svalue)
             item = {
                 "ts": start_time + offset,
                 "source": "chrome",
                 metric: value
             }
             res.append(item)
+        self.log.info("tracing duration: %s", self.extractor.tracing_duration)
         return res
 
     def disconnect(self):
