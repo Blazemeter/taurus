97bdf73af13a9bfaa5adffde4ce989bbddd79f0e
Dmitry Pribysh <dmand@yandex.ru>
2016-08-12 13:56:46 +0300

    Add python-based test script for chrome profiler

---
+++ tests/chrome/test_trace.py
@@ -0,0 +1,65 @@
+import json
+import time
+from selenium import webdriver
+import unittest
+
+
+trace_cats = [
+    "blink.net",
+    "blink.user_timing",
+    "v8",
+    "devtools.timeline",
+    "disabled-by-default-memory-infra",
+    "disabled-by-default-devtools.timeline",
+    "disabled-by-default-devtools.timeline.frame",
+]
+
+args = [
+    "--no-sandbox",
+    "--enable-tracing",
+    "--native",
+]
+
+
+class ChromeTest(unittest.TestCase):
+    def setUp(self):
+        self.driver = webdriver.Chrome(
+            desired_capabilities = {
+                'loggingPrefs': { 'performance': 'ALL' },
+                'chromeOptions': {
+                   "args" : args,
+                   "perfLoggingPrefs" : {
+                       "traceCategories": ",".join(trace_cats),
+                   },
+                }
+            },
+        )
+        self.start_time = time.time()
+        self.profile = None
+        self.driver.execute_script(":startProfile")
+
+    def test_local_fractals(self):
+        self.driver.get('http://blazedemo.com/')
+
+    def tearDown(self):
+        # save chrome trace (viewable by chrome://tracing)
+        with open(r"trace.json", 'w') as f:
+            f.write("[\n")
+            lines = [json.dumps({"name": "start_time", "args": {"timestamp": self.start_time}, "ts": 0, "cat": "__metadata", "ph": "M"})]
+            for d in self.driver.get_log('performance'):
+                event = json.loads(d['message'])
+                if event['message']['method'] == 'Tracing.dataCollected':
+                    lines.append(json.dumps(event['message']['params']))
+            f.write(",\n".join(lines))
+            f.write("\n]\n")
+        # save Chrome CPU profile (viewable by Chrome devtools)
+        self.profile = self.driver.execute_script(":endProfile")
+        if self.profile:
+            with open("js.cpuprofile", 'w') as f:
+                f.write(json.dumps(self.profile["profile"]))
+        # close browser
+        self.driver.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
