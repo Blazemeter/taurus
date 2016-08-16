ee0b09b5687d8e96e30bce0e214366f70c7729ce
Dmitry Pribysh <dmand@yandex.ru>
2016-08-16 17:10:30 +0300

    Update docs, move example script to examples/selenium/chrome-performance dir

--- examples/selenium/chrome-performance/test_chrome_perf.py
+++ examples/selenium/chrome-performance/test_chrome_perf.py
@@ -2,8 +2,9 @@ import json
 import time
 from selenium import webdriver
 import unittest
-
-
+ 
+ 
+# Chrome tracing categories that are required to calculate performance stats
 trace_cats = [
     "blink.net",
     "blink.user_timing",
@@ -13,16 +14,18 @@ trace_cats = [
     "disabled-by-default-devtools.timeline",
     "disabled-by-default-devtools.timeline.frame",
 ]
-
+ 
+# Chrome arguments to enable logging performance data
 args = [
     "--no-sandbox",
     "--enable-tracing",
     "--native",
 ]
-
-
+ 
+ 
 class ChromeTest(unittest.TestCase):
     def setUp(self):
+        # configure chromedriver to redirect Chrome tracing data to performance logs
         self.driver = webdriver.Chrome(
             desired_capabilities = {
                 'loggingPrefs': { 'performance': 'ALL' },
@@ -35,24 +38,23 @@ class ChromeTest(unittest.TestCase):
             },
         )
         self.start_time = time.time()
-
+ 
     def test_local_fractals(self):
-        self.driver.get('http://blazedemo.com/')
-
+        self.driver.get('https://github.com/')
+ 
     def tearDown(self):
-        # save chrome trace (viewable by chrome://tracing)
+        # save chrome tracing data (viewable by chrome://tracing) to 'trace.json' file
         with open(r"trace.json", 'w') as f:
-            f.write("[\n")
-            lines = [json.dumps({"name": "start_time", "args": {"timestamp": self.start_time}, "ts": 0, "cat": "__metadata", "ph": "M"})]
-            for d in self.driver.get_log('performance'):
-                event = json.loads(d['message'])
+            start_event = {"name": "start_time", "args": {"timestamp": self.start_time}, "ts": 0, "cat": "__metadata", "ph": "M"}
+            trace = [start_event]
+            for log_entry in self.driver.get_log('performance'):
+                event = json.loads(log_entry['message'])
                 if event['message']['method'] == 'Tracing.dataCollected':
-                    lines.append(json.dumps(event['message']['params']))
-            f.write(",\n".join(lines))
-            f.write("\n]\n")
+                    trace.append(event['message']['params'])
+            f.write(json.dumps(trace))
         # close browser
         self.driver.quit()
-
-
+ 
+ 
 if __name__ == '__main__':
     unittest.main()
