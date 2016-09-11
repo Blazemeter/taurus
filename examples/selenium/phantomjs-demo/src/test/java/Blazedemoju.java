200a3c7130d40e1569c17cbddf7a5a2e5aa6eae6
Andrey Pohilko <apc4@ya.ru>
2016-09-11 17:32:26 +0300

    add phantomjs example

---
+++ examples/selenium/phantomjs-demo/src/test/java/Blazedemoju.java
@@ -0,0 +1,40 @@
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriverService;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.concurrent.TimeUnit;
+
+public class Blazedemoju {
+    private PhantomJSDriver wd;
+
+    @Before
+    public void setUp() throws Exception {
+        DesiredCapabilities caps = new DesiredCapabilities();
+        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "phantomjs");
+        wd = new PhantomJSDriver(caps);
+        wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
+    }
+
+    @Test
+    public void test() {
+        wd.get("http://blazedemo.com/");
+        if (!wd.findElement(By.xpath("//div[3]/form/select[1]//option[3]")).isSelected()) {
+            wd.findElement(By.xpath("//div[3]/form/select[1]//option[3]")).click();
+        }
+        if (!wd.findElement(By.xpath("//div[3]/form/select[2]//option[6]")).isSelected()) {
+            wd.findElement(By.xpath("//div[3]/form/select[2]//option[6]")).click();
+        }
+        wd.findElement(By.cssSelector("input.btn.btn-primary")).click();
+        wd.findElement(By.xpath("//table[@class='table']/tbody/tr[4]/td[1]/input")).click();
+        wd.findElement(By.cssSelector("input.btn.btn-primary")).click();
+    }
+
+    @After
+    public void tearDown() {
+        wd.quit();
+    }
+}
