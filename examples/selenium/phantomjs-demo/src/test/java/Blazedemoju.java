9d34b111ef5464adf7eb2542557fe44479500643
Andrey Pohilko <apc4@ya.ru>
2016-09-12 22:00:25 +0300

    PhantomKS example

--- examples/selenium/phantomjs-demo/src/test/java/Blazedemoju.java
+++ examples/selenium/phantomjs-demo/src/test/java/Blazedemoju.java
@@ -14,13 +14,13 @@ public class Blazedemoju {
     @Before
     public void setUp() throws Exception {
         DesiredCapabilities caps = new DesiredCapabilities();
-        caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "phantomjs");
+        //caps.setCapability(PhantomJSDriverService.PHANTOMJS_EXECUTABLE_PATH_PROPERTY, "phantomjs");
         wd = new PhantomJSDriver(caps);
         wd.manage().timeouts().implicitlyWait(60, TimeUnit.SECONDS);
     }
 
     @Test
-    public void test() {
+    public void test() throws Exception {
         wd.get("http://blazedemo.com/");
         if (!wd.findElement(By.xpath("//div[3]/form/select[1]//option[3]")).isSelected()) {
             wd.findElement(By.xpath("//div[3]/form/select[1]//option[3]")).click();
@@ -29,8 +29,14 @@ public class Blazedemoju {
             wd.findElement(By.xpath("//div[3]/form/select[2]//option[6]")).click();
         }
         wd.findElement(By.cssSelector("input.btn.btn-primary")).click();
+
+        Thread.sleep(100);
         wd.findElement(By.xpath("//table[@class='table']/tbody/tr[4]/td[1]/input")).click();
+
+        Thread.sleep(200);
         wd.findElement(By.cssSelector("input.btn.btn-primary")).click();
+        
+        Thread.sleep(500);
     }
 
     @After
