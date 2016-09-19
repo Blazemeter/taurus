ec691ecf7b67ff256b1f8ec0f7a9fdd2012db0c5
Dmitry Pribysh <dmand@yandex.ru>
2016-09-19 20:00:28 +0300

    Output Taurus-compatible stdout from RSpec formatter

--- bzt/resources/rspec_taurus_formatter.rb
+++ bzt/resources/rspec_taurus_formatter.rb
@@ -1,16 +1,25 @@
 require 'json'
 
 class TaurusFormatter
-  RSpec::Core::Formatters.register self, :dump_summary, :close, :example_passed, :example_failed, :example_pending, :example_started
+  RSpec::Core::Formatters.register self, :close, :example_passed, :example_failed, :example_pending, :example_started
 
   def initialize output
     @output = output
     @report_path = ENV.fetch("TAURUS_REPORT_FILE", "report.ldjson")
     @report = File.open(@report_path, 'w')
     @started_at = nil
+    @total_tests = 0
+    @tests_passed = 0
+  end
+
+  def report_stdout test
+    test_case = test[:test_case]
+    tests_failed = @tests_passed - @total_tests
+    @output << "#{test_case},Total:#{@total_tests} Passed:#{@tests_passed} Failed:#{tests_failed}\n"
   end
 
   def example_started _notification
+    @total_tests += 1
     @started_at = Time.new.to_f
   end
 
@@ -25,10 +34,10 @@ class TaurusFormatter
             :error_msg => nil,
             :error_trace => nil,
             :extras => nil}
-    # TODO: licalocation
+    # TODO: location
     @report << item.to_json << "\n"
-
-    @output << "."
+    @tests_passed += 1
+    report_stdout item
   end
 
   def example_failed notification # FailedExampleNotification
@@ -43,8 +52,7 @@ class TaurusFormatter
             :status => "FAILED",
             :extras => nil}
     @report << item.to_json << "\n"
-
-    @output << "F"
+    report_stdout item
   end
 
   def example_pending notification # ExampleNotification
@@ -59,16 +67,11 @@ class TaurusFormatter
             :error_trace => nil,
             :extras => nil}
     @report << item.to_json << "\n"
-
-    @output << "*"
-  end
-
-  def dump_summary notification # SummaryNotification
-    @output << "\n\nFinished in #{RSpec::Core::Formatters::Helpers.format_duration(notification.duration)}."
+    @tests_passed += 1
+    report_stdout item
   end
 
   def close _notification # NullNotification
-    @output << "\n"
     @report.close
   end
 end
