1a33e391ac889c3d881269fae5b3972a417a3b24
Dmitry Pribysh <dmand@yandex.ru>
2016-09-27 15:31:15 +0300

    Bring debug output back, fix looping and exiting for timeouted tests

--- bzt/resources/mocha-taurus-plugin.js
+++ bzt/resources/mocha-taurus-plugin.js
@@ -1,9 +1,9 @@
 var Mocha = require('mocha'),
     fs = require('fs'),
-    path = require('path');
+    path = require('path'),
+    debug = require('debug')('taurus');
 
 function TaurusReporter(runner, config) {
-    console.log("creating reporter");
     Mocha.reporters.Base.call(this, runner);
 
     var self = this;
@@ -12,56 +12,80 @@ function TaurusReporter(runner, config) {
     var testStartTime = null;
 
     runner.on('start', function() {
-        console.log('start');
-        reportStream = fs.createWriteStream(config.reportFile || 'report.ldjson');
+        reportStream = fs.createWriteStream(config.reportFile || 'report.ldjson', { 'flags': 'a' });
     });
 
     runner.on('suite', function(suite) {
-        console.log('suite');
+        debug('suite');
     });
 
     runner.on('suite end', function(suite) {
-        console.log('suite end');
+        debug('suite end');
     });
 
     runner.on('test', function(test) {
-        console.log('test');
         testStartTime = epoch();
     });
 
     runner.on('test end', function(test) {
-        console.log('test end');
+        debug('test end');
         test.startTime = testStartTime;
-        if (!test.duration)
-            test.duration = 0.0;
         var item = reportItem(test, test.err || {});
         reportStream.write(JSON.stringify(item) + "\n");
     });
 
     runner.on('pending', function(test) {
-        console.log('pending');
         test.state = "pending";
     });
 
     runner.on('pass', function(test) {
-        console.log('pass');
+
     });
 
     runner.on('fail', function(test, err) {
-        console.log('fail');
+
     });
 
     runner.on('end', function() {
-        console.log('end');
-        reportStream && reportStream.end();
+        if (reportStream)
+            reportStream.end();
     });
 };
 
+function reportItem(test, err) {
+    // Test properties:
+    // 'title', 'fn', 'body', 'timedOut', 'pending', 'type', 'file',
+    // 'parent', 'ctx', 'callback', 'timer', 'skip', 'duration',
+    // 'state','speed'
+
+    // Test methods:
+    // 'isPending', 'retries', 'currentRetry', 'fullTitle',
+
+    var stateStatus = {
+        "passed": "PASSED",
+        "failed": "FAILED",
+        "pending": "SKIPPED"
+    };
+
+    return {
+        test_case: test.title,
+        test_suite: test.fullTitle(),
+        status: stateStatus[test.state],
+        start_time: test.startTime,
+        duration: (test.duration) ? test.duration / 1000.0 : 0.0,
+        error_msg: err.message || null,
+        error_trace: err.stack || null,
+        extras: {
+            file: test.file || null
+        }
+    };
+}
+
 function parseCmdline(argv) {
-    options = {iterations: 0, holdFor: 0};
-    args = argv.slice(2);
+    var options = {iterations: 0, holdFor: 0};
+    var args = argv.slice(2);
     while (args) {
-        arg = args.shift();
+        var arg = args.shift();
         if (!arg)
             break;
         switch (arg) {
@@ -113,38 +137,15 @@ function epoch() {
     return (new Date()).getTime() / 1000.0;
 }
 
-function reportItem(test, err) {
-    // Test properties:
-    // 'title', 'fn', 'body', 'timedOut', 'pending', 'type', 'file',
-    // 'parent', 'ctx', 'callback', 'timer', 'skip', 'duration',
-    // 'state','speed'
-
-    // Test methods:
-    // 'isPending', 'retries', 'currentRetry', 'fullTitle',
-
-    stateStatus = {
-        "passed": "PASSED",
-        "failed": "FAILED",
-        "pending": "SKIPPED"
-    };
-
-    return {
-        test_case: test.title,
-        test_suite: test.fullTitle(),
-        status: stateStatus[test.state],
-        start_time: test.startTime,
-        duration: test.duration / 1000.0,
-        error_msg: err.message || null,
-        error_trace: err.stack || null,
-        extras: {
-            file: test.file || null
-        }
-    };
-}
-
 function prepareMocha(config) {
+    // clear 'require' cache to avoid mocha's test rediscovery issues
+    // TODO: fix/report?
+    Object.keys(require.cache).forEach(function(key) { delete require.cache[key] })
+
     var engine = new Mocha({
         reporter: TaurusReporter,
+        bail: false,
+
         reporterOptions: {
             reportFile: config.reportFile
         }
@@ -152,13 +153,13 @@ function prepareMocha(config) {
 
     var stat = fs.statSync(config.testSuite);
 
-    if (stat.isFile(config.testSuite))
+    if (stat.isFile(config.testSuite)) {
         engine.addFile(config.testSuite);
+    }
     else if (stat.isDirectory(config.testSuite)) {
         fs.readdirSync(config.testSuite).filter(function(file){
             return file.substr(-3) === '.js';
         }).forEach(function(file){
-            console.log('adding test suite file', file);
             engine.addFile(
                 path.join(config.testSuite, file)
             );
@@ -172,24 +173,29 @@ function prepareMocha(config) {
 
 function runMocha() {
     var config = parseCmdline(process.argv);
-    loopMocha(config, 0, epoch());
+
+    if (fs.existsSync(config.reportFile))
+        fs.unlinkSync(config.reportFile);
+
+    loopMocha(config, 0, epoch(), function() { process.exit(0); });
 }
 
-function loopMocha(config, iterations, startTime) {
-    console.log(epoch(), config, iterations);
+function loopMocha(config, iterations, startTime, done) {
     if (iterations >= config.iterations) {
-        console.log(epoch(), "iteration limit reached");
+        debug('iteration limit reached');
+        done();
         return;
     }
     var offset = epoch() - startTime;
     if (config.holdFor > 0 && offset > config.holdFor) {
-        console.log(epoch(), "duration limit reached");
+        debug('duration limit reached');
+        done();
         return;
     }
+    debug('iteration #%s', iterations);
     var engine = prepareMocha(config);
-    console.log(engine);
     engine.run(function() {
-        loopMocha(config, iterations + 1, startTime);
+        loopMocha(config, iterations + 1, startTime, done);
     });
 }
 
