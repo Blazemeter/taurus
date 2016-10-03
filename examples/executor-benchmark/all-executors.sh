c435d38adbc8d69950bd80c2d2762fc94c312b8c
Andrey Pohilko <apc4@ya.ru>
2016-10-03 11:38:30 +0300

    Update benchmark code

--- examples/executor-benchmark/all-executors.sh
+++ examples/executor-benchmark/all-executors.sh
@@ -2,11 +2,9 @@
 for B in vu hits; do
   for E in gatling jmeter pbench; do
     bzt scenario.yml exec-$B.yml service.yml -o execution.0.executor=$E -o modules.blazemeter.report-name="$B keep-alive $E" -o scenarios.dummy.keepalive=true
-    echo Cooldown for 1m...
     sleep 60
 
     bzt scenario.yml exec-$B.yml service.yml -o execution.0.executor=$E -o modules.blazemeter.report-name="$B connect $E" -o scenarios.dummy.keepalive=false
-    echo Cooldown for 1m...
     sleep 60
   done
 done
