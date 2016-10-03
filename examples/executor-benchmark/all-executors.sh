a6805e74bc062db645c7d492ae67f32975cf263c
Andrey Pohilko <apc4@ya.ru>
2016-10-03 10:40:08 +0300

    add vu to benchmark script

--- examples/executor-benchmark/all-executors.sh
+++ examples/executor-benchmark/all-executors.sh
@@ -1,12 +1,12 @@
 #! /bin/sh -xe
-for E in gatling jmeter pbench; do
-    echo Running $E
-
-    bzt scenario.yml exec-hits.yml service.yml -o execution.0.executor=$E -o modules.blazemeter.report-name="hits keep-alive $E" -o scenarios.dummy.keepalive=true
+for B in vu hits; do
+  for E in gatling jmeter pbench; do
+    bzt scenario.yml exec-$B.yml service.yml -o execution.0.executor=$E -o modules.blazemeter.report-name="$B keep-alive $E" -o scenarios.dummy.keepalive=true
     echo Cooldown for 1m...
     sleep 60
 
-    bzt scenario.yml exec-hits.yml service.yml -o execution.0.executor=$E -o modules.blazemeter.report-name="hits connect $E" -o scenarios.dummy.keepalive=false
+    bzt scenario.yml exec-$B.yml service.yml -o execution.0.executor=$E -o modules.blazemeter.report-name="$B connect $E" -o scenarios.dummy.keepalive=false
     echo Cooldown for 1m...
     sleep 60
+  done
 done
