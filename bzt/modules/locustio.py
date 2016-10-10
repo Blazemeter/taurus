711b4e04dcf82ab881db2af69ba77823fe09da15
Andrey Pohilko <apc4@ya.ru>
2016-10-10 12:11:08 +0300

    Try to make headers sorted

--- bzt/modules/locustio.py
+++ bzt/modules/locustio.py
@@ -373,7 +373,8 @@ from locust import HttpLocust, TaskSet, task
             statement = "self.client.%s(%s)"
         headers = OrderedDict()
         if global_headers:
-            headers.update(global_headers)
+            sorted_headers = OrderedDict(sorted(global_headers.items(), key=lambda t: t[0]))
+            headers.update(sorted_headers)
         if req.headers:
             headers.update(req.headers)
         task.append(self.gen_statement(statement % (method, self.__get_params_line(req, timeout, headers))))
