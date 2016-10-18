1d682805a730fa3b072ce39c3b153f9637b98a1c
Andrey Pohilko <apc4@ya.ru>
2016-10-18 11:05:09 +0300

    Trying to fix jenkins

--- virtualenv-test.bash
+++ virtualenv-test.bash
@@ -7,7 +7,7 @@ virtualenv --clear build
 source build/bin/activate
 
 # install depends
-pip install --upgrade colorlog pyyaml psutil lxml cssselect nose urwid coverage six pylint selenium==2.53.1 progressbar33 locustio pyvirtualdisplay pynsist
+pip install --upgrade colorlog pyyaml psutil lxml cssselect nose urwid coverage six pylint selenium progressbar33 locustio pyvirtualdisplay pynsist
 
 pylint -d R0903,R0904,C0301,C0302,C0111,E1103,R0201,R0902,W0511,F0401,E0611,R0801,R0913,W0613,C0412,I0011 --extension-pkg-whitelist=lxml -f parseable bzt > build/pylint.out || echo
 
@@ -26,6 +26,8 @@ source build/bin/activate
 cd build # cd is to make it not find bzt package from sources
 pip install --upgrade ../dist/bzt-*.tar.gz
 pip install locustio
+pip install --upgrade  selenium==2.53.1
+pip show selenium
 cd ..
 
 echo '{"install-id": "UnitTest"}' > build/etc/bzt.d/99-zinstallID.json
