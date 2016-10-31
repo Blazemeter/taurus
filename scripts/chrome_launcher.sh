ea7ce6fa8e62a3c21bff8492511736127597fdf3
Dmitri Pribysh <dmand@yandex.ru>
2016-10-31 20:16:56 +0300

    Minimize chrome launcher even further

--- scripts/chrome_launcher.sh
+++ scripts/chrome_launcher.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
-#
-# Copyright (c) 2011 The Chromium Authors, Taurus project. All rights reserved.
-# Use of this source code is governed by a BSD-style license.
 
-export CHROME_WRAPPER="`readlink -f "$0"`"
-HERE="`dirname "$CHROME_WRAPPER"`"
+export CHROME_WRAPPER=$(readlink -f "$0")
+HERE=$(dirname "$CHROME_WRAPPER")
 
-exec -a "$0" "$HERE/chrome" --no-sandbox "$@"
 exec -a "$0" /etc/alternatives/google-chrome --no-sandbox "$@"
