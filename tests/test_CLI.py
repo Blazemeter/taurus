2ec4413084dbac2a3a5b55378e6af937b98ed939
Andrey Pokhilko <apc4@ya.ru>
2016-11-01 10:01:27 +0200

    improve test coverage

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -193,5 +193,8 @@ class TestConfigOverrider(BZTestCase):
 
     def test_override_delete_from_list(self):
         self.config["items"] = [1, 2, 3]
+        self.config["dict"] = {"1":1, "2":2, "3":3}
         self.obj.apply_overrides(['items.^1=null'], self.config)
+        self.obj.apply_overrides(['dict.^2=null'], self.config)
         self.assertEqual(self.config.get("items"), [1, 3])
+        self.assertEqual(self.config.get("dict"), {"1":1, "3":3})
