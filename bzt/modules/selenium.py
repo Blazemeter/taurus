02c7d3d4316fceabb2b13dc7688d8873608b43b5
Dmitri Pribysh <dmand@yandex.ru>
2016-11-01 15:42:15 +0300

    Revert "Experiments, experiments"
    This reverts commit 43b14c5dd5a0238e129ea776f2edccf5cd68e8e7.

--- bzt/modules/selenium.py
+++ bzt/modules/selenium.py
@@ -1276,9 +1276,7 @@ from selenium.common.exceptions import NoAlertPresentException
             setup_method_def.append(log_set)
             setup_method_def.append(self.gen_statement("cls.driver = webdriver.Firefox(profile)"))
         elif browser == 'Chrome':
-            setup_method_def.append(self.gen_statement("chrome_options = webdriver.ChromeOptions()"))
-            setup_method_def.append(self.gen_statement("chrome_options.add_argument('--no-sandbox')"))
-            statement = "cls.driver = webdriver.Chrome(service_log_path=%s, chrome_options=chrome_options)"
+            statement = "cls.driver = webdriver.Chrome(service_log_path=%s)"
             setup_method_def.append(self.gen_statement(statement % repr(self.wdlog)))
         else:
             setup_method_def.append(self.gen_statement("cls.driver = webdriver.%s()" % browser))
