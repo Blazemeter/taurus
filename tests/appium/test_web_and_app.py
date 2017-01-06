e834b4284eaa574f5c9ef602e08ef13993e770dc
greyfenrir <grey.fenrir@gmail.com>
2017-01-06 15:45:27 +0300

    add service for startup of appium and emulator, example of test and config

---
+++ tests/appium/test_web_and_app.py
@@ -0,0 +1,41 @@
+import re
+import time
+import logging
+
+from appium import webdriver
+from unittest import TestCase
+
+
+class AppiumTest(TestCase):
+    def setUp(self):
+        self.log = logging.getLogger('')
+        desired_caps = {}
+        desired_caps['platformName'] = 'Android'
+        desired_caps['deviceName'] = 'Android Emulator'
+        desired_caps['browserName'] = 'Chrome'
+        self.log.info("connect to webdriver...")
+        self.driver = webdriver.Remote('http://localhost:4723/wd/hub', desired_caps)
+        time.sleep(3)
+
+    def tearDown(self):
+        self.driver.quit()
+
+
+class AppiumWebTest(AppiumTest):
+    def test_web(self):
+        self.log.info('connected.')
+        self.log.info('open url into browser...')
+        time.sleep(3)
+        element = self.driver.find_elements_by_id('com.android.chrome:id/terms_accept')
+        self.driver.get('http://blazedemo.com')
+        # btn = driver.find_element_by_name("Find Flights")
+        # btn.click()
+        self.log.info('done.')
+        body = self.driver.page_source
+        re_pattern = re.compile(r'Buenos Aires')
+        self.log.info("found: %s", len(re.findall(re_pattern, body)) > 0)
+
+
+class AppiumAppTest(AppiumTest):
+    def test_app(self):
+        pass
