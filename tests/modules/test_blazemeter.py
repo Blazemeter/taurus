161d5225242a1712e6000fff4a9135056da4c0e1
Andrey Pohilko <apc4@ya.ru>
2017-01-07 14:50:50 +0300

    :(

--- tests/modules/test_blazemeter.py
+++ tests/modules/test_blazemeter.py
@@ -32,7 +32,8 @@ class BZMock(object):
             'https://a.blazemeter.com/api/v4/multi-tests?workspaceId=1&name=Taurus+Cloud+Test': {"result": []},
             'https://a.blazemeter.com/api/v4/tests?workspaceId=1&name=Taurus+Cloud+Test': {"result": []},
             'https://a.blazemeter.com/api/v4/projects?workspaceId=1': {"result": []},
-            'https://a.blazemeter.com/api/v4/web/elfinder/1?cmd=open&target=s1_Lw': {"files": []}
+            'https://a.blazemeter.com/api/v4/web/elfinder/1?cmd=open&target=s1_Lw': {"files": []},
+            'https://a.blazemeter.com/api/v4/web/elfinder/1?target=s1_Lw&cmd=open': {"files": []},
         }
 
         self.mock_post = {}
