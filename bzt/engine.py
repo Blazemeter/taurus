6bd6fd01886d14ed39c6c609022ac8ca6901a625
Dmitri Pribysh <dmand@yandex.ru>
2017-03-07 00:08:56 +0300

    Attempt to level down log file verbosity after 'startup' phase
    Using engine-level hooks.
    
    Isn't really pretty, but there's so much that can be done about it.

--- bzt/engine.py
+++ bzt/engine.py
@@ -79,6 +79,8 @@ class Engine(object):
         self.prepared = []
         self.started = []
         self.default_cwd = None
+        self.post_startup_hook = None
+        self.pre_shutdown_hook = None
 
     def configure(self, user_configs, read_config_files=True):
         """
@@ -142,6 +144,8 @@ class Engine(object):
         exc_info = None
         try:
             self._startup()
+            if self.post_startup_hook is not None:
+                self.post_startup_hook()
             self._wait()
         except BaseException as exc:
             self.log.debug("%s:\n%s", exc, traceback.format_exc())
@@ -150,6 +154,8 @@ class Engine(object):
         finally:
             self.log.warning("Please wait for graceful shutdown...")
             try:
+                if self.pre_shutdown_hook is not None:
+                    self.pre_shutdown_hook()
                 self._shutdown()
             except BaseException as exc:
                 self.log.debug("%s:\n%s", exc, traceback.format_exc())
--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -122,6 +122,27 @@ class TestCLI(BZTestCase):
             if os.path.exists(artifacts_dir):
                 shutil.rmtree(artifacts_dir)
 
+    def test_logging_verbosity_adjustment(self):
+        try:
+            was_verbose = self.verbose
+            self.verbose = False
+            ret = self.obj.perform([
+                __dir__() + "/json/mock_normal.json",
+            ])
+            self.assertEquals(0, ret)
+            log_lines = open(os.path.join(self.obj.engine.artifacts_dir, "bzt.log")).readlines()
+            checking = False
+            for line in log_lines:
+                if "Leveling down" in line:
+                    checking = True
+                elif "Leveling up" in line:
+                    checking = False
+                else:
+                    if checking:
+                        self.assertNotIn("DEBUG", line)
+        finally:
+            self.verbose = was_verbose
+
 
 class TestConfigOverrider(BZTestCase):
     def setUp(self):
