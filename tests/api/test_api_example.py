4326bfd3ab549223e1fbedffeb8ce35ce11d8020
Dmitri Pribysh <dmand@yandex.ru>
2017-03-20 16:12:59 +0300

    Add a basic tests case for generated script

---
+++ tests/api/test_api_example.py
@@ -0,0 +1,17 @@
+from apirus import ApiTestCase
+
+
+class MyTest(ApiTestCase):
+    def test_index(self):
+        response = self.request('http://example.com')
+        self.assertOk(response)
+
+    def test_api_version(self):
+        response = self.get('https://api.github.com')
+        self.assertOk(response)
+
+    def test_github_api(self):
+        response = self.get('https://api.github.com')
+        self.assertOk(response)
+        repos = self.get('https://api.github.com/users/kennethreitz/repos')
+        self.assertOk(repos)
