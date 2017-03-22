55e691b58e6264c2cb14a134219f816de286c341
Andrey Pohilko <apc4@ya.ru>
2017-03-22 20:08:21 +0300

    Require modern requests lib

--- setup.py
+++ setup.py
@@ -66,7 +66,7 @@ setup(
     install_requires=[
         'pyyaml', 'psutil > 3, != 5.1.1', 'colorlog', 'colorama', 'lxml == 3.6.0',
         'cssselect', 'urwid', 'six', 'nose',
-        'selenium', 'progressbar33', 'pyvirtualdisplay', 'requests', ],
+        'selenium', 'progressbar33', 'pyvirtualdisplay', 'requests>=2.11.1', ],
     packages=['bzt', 'bzt.six', 'bzt.modules', 'bzt.resources'],
     entry_points={
         'console_scripts': [
