9732d98c7eb4df90005f746c4717460e5eade554
Dmitri Pribysh <dmand@yandex.ru>
2017-03-30 16:39:06 +0300

    Restructure tests

--- tests/test_ApiritifFramework.py
+++ tests/test_ApiritifFramework.py
@@ -4,34 +4,35 @@ import unittest
 from tests import __dir__
 
 
+def run_test_script(script_path):
+    loader = unittest.TestLoader()
+    suite = loader.discover(os.path.dirname(script_path), pattern=os.path.basename(script_path))
+    return unittest.TextTestRunner(verbosity=2).run(suite)
+
+
 class TestApiritif(unittest.TestCase):
-    @staticmethod
-    def run_test_script(script_path):
-        loader = unittest.TestLoader()
-        suite = loader.discover(os.path.dirname(script_path), pattern=os.path.basename(script_path))
-        return unittest.TextTestRunner(verbosity=2).run(suite)
 
     def test_example(self):
-        result = self.run_test_script(__dir__() + "/apiritif/test_api_example.py")
+        result = run_test_script(__dir__() + "/apiritif/test_api_example.py")
         self.assertTrue(result.wasSuccessful())
         self.assertEqual(result.testsRun, 1)
 
     def test_cookies(self):
-        result = self.run_test_script(__dir__() + "/apiritif/test_cookies.py")
+        result = run_test_script(__dir__() + "/apiritif/test_cookies.py")
         self.assertTrue(result.wasSuccessful())
         self.assertEqual(result.testsRun, 1)
 
     def test_assertions(self):
-        result = self.run_test_script(__dir__() + "/apiritif/test_assertions.py")
+        result = run_test_script(__dir__() + "/apiritif/test_assertions.py")
         self.assertTrue(result.wasSuccessful())
         self.assertEqual(result.testsRun, 3)
 
     def test_methods(self):
-        result = self.run_test_script(__dir__() + "/apiritif/test_methods.py")
+        result = run_test_script(__dir__() + "/apiritif/test_methods.py")
         self.assertTrue(result.wasSuccessful())
         self.assertEqual(result.testsRun, 6)
 
     def test_body(self):
-        result = self.run_test_script(__dir__() + "/apiritif/test_body_data.py")
+        result = run_test_script(__dir__() + "/apiritif/test_body_data.py")
         self.assertTrue(result.wasSuccessful())
         self.assertEqual(result.testsRun, 3)
