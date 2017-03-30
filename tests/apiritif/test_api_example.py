cdede4dc699115a9d4abee9c4fe2e1edb4624909
Dmitri Pribysh <dmand@yandex.ru>
2017-03-30 15:53:08 +0300

    Simplify apiritif test example

--- tests/apiritif/test_api_example.py
+++ tests/apiritif/test_api_example.py
@@ -2,16 +2,6 @@ from apiritif import APITestCase
 
 
 class MyTest(APITestCase):
-    def test_index(self):
+    def test_example(self):
         response = self.request('http://example.com')
         self.assertOk(response)
-
-    def test_api_version(self):
-        response = self.get('https://api.github.com', headers={'user-agent': 'me'})
-        self.assertOk(response)
-
-    def test_github_api(self):
-        response = self.get('https://api.github.com', headers={'user-agent': 'me'})
-        self.assertOk(response)
-        repos = self.get('https://api.github.com/users/kennethreitz/repos', headers={'user-agent': 'me'})
-        self.assertOk(repos)
