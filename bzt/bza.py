5c9c01caaf45e9527e15d0719ce36863e14a82d4
greyfenrir <grey.fenrir@gmail.com>
2017-04-04 11:29:24 +0300

    unify usage of Blazemeter API in proxy2jmx (#533)
    * add startup/configure to cloud tests
    
    * Revert "add startup/configure to cloud tests"
    
    This reverts commit e78766086cc3e486f5e0d2fa7be4ebdd6e9ac617.
    
    * move interaction with blazemeter into BZAProxy, fix tests
    
    * add changefile
    
    * fix
    
    * move BZAProxy to bza lib, rely requests on base address

--- bzt/bza.py
+++ bzt/bza.py
@@ -5,6 +5,7 @@ it may become separate library in the future. Things like imports and logging sh
 import base64
 import json
 import logging
+import time
 from collections import OrderedDict
 
 import requests
@@ -45,7 +46,7 @@ class BZAObject(dict):
                     continue
                 self.__setattr__(attr, proto.__getattribute__(attr))
 
-    def _request(self, url, data=None, headers=None, method=None):
+    def _request(self, url, data=None, headers=None, method=None, raw_result=False):
         """
         :param url: str
         :type data: Union[dict,str]
@@ -90,6 +91,9 @@ class BZAObject(dict):
         self.log.debug("Response: %s", resp[:self.logger_limit] if resp else None)
         if response.status_code >= 400:
             raise TaurusNetworkError("API call error %s: %s %s" % (url, response.status_code, response.reason))
+
+        if raw_result:
+            return resp
 
         try:
             result = json.loads(resp) if len(resp) else {}
@@ -594,3 +598,44 @@ class Session(BZAObject):
     def get_logs(self):
         url = self.address + "/api/v4/sessions/%s/reports/logs" % self['id']
         return self._request(url)['result']['data']
+
+
+class BZAProxy(BZAObject):
+    def __init__(self):
+        super(BZAProxy, self).__init__()
+        self.delay = 5
+
+    def stop(self):
+        self._request(self.address + '/api/latest/proxy/recording/stop', method='POST')
+
+    def start(self):
+        self._request(self.address + '/api/latest/proxy/recording/start', method='POST')
+
+    def get_jmx(self):
+        # wait for availability
+        while True:
+            response = self._request(self.address + '/api/latest/proxy')
+            if response['result']['smartjmx'] == "available":
+                break
+            time.sleep(self.delay)
+
+        response = self._request(self.address + '/api/latest/proxy/download?format=jmx&smart=true', raw_result=True)
+        return response
+
+    def get_addr(self):
+        response = self._request(self.address + '/api/latest/proxy')
+
+        proxy_info = response['result']
+        if proxy_info:
+            self.log.info('Using existing recording proxy...')
+            if proxy_info['status'] == 'active':
+                self.log.info('Proxy is active, stop it')
+                self.stop()
+        else:
+            self.log.info('Creating new recording proxy...')
+            response = self._request(self.address + '/api/latest/proxy', method='POST')
+            proxy_info = response['result']
+
+        self._request(self.address + '/api/latest/proxy/recording/clear', method='POST')
+
+        return 'http://%s:%s' % (proxy_info['host'], proxy_info['port'])
