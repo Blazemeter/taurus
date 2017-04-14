e2900b23017d057428c428b826c106fef8772b36
Andrey Pohilko <apc4@ya.ru>
2017-04-14 11:56:20 +0300

    Test passes

--- java/grinder_logger/src/test/java/org/blazemeter/taurus/TaurusAppenderTest.java
+++ java/grinder_logger/src/test/java/org/blazemeter/taurus/TaurusAppenderTest.java
@@ -3,7 +3,10 @@ package org.blazemeter.taurus;
 import ch.qos.logback.classic.spi.LoggingEvent;
 import net.grinder.engine.process.TestRegistryAccessor;
 import net.grinder.script.Grinder;
+import net.grinder.util.logback.BufferedEchoMessageEncoder;
 import org.junit.Test;
+
+import java.io.ByteArrayOutputStream;
 
 import static org.junit.Assert.assertEquals;
 
@@ -15,7 +18,18 @@ public class TaurusAppenderTest {
         new net.grinder.script.Test(1, "label");
 
         TaurusAppender appender = new TaurusAppender();
-        appender.writeOut(new LoggingEvent());
+        BufferedEchoMessageEncoder encoder = new BufferedEchoMessageEncoder();
+        ByteArrayOutputStream os = new ByteArrayOutputStream();
+        encoder.init(os);
+        appender.setEncoder(encoder);
+        LoggingEvent event = new LoggingEvent();
+        event.setMessage("Tada!");
+        appender.setOutputStream(os);
+        appender.start();
+        appender.writeOut(event);
+        appender.stop();
+
+        assertEquals("Test name for 1: label\nTada!\n", os.toString());
     }
 
 }
