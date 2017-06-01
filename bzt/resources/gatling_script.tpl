96a5636b78f343dc006a77bfcc2feb9b07a19d6d
Dmitri Pribysh <dmand@yandex.ru>
2017-06-01 15:27:10 +0300

    Refactor Gatling script generation to generate formatted scripts (#585)
    * Refactor Gatling script generation to generate formatted scripts
    
    * Add a changefile
    
    * Rename 'str' variable to silence Codacy

--- bzt/resources/gatling_script.tpl
+++ bzt/resources/gatling_script.tpl
@@ -5,37 +5,38 @@ import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
 class %(class_name)s extends Simulation {
-    val _t_concurrency = Integer.getInteger("concurrency", 1).toInt
-    val _t_ramp_up = Integer.getInteger("ramp-up", 0).toInt
-    val _t_hold_for = Integer.getInteger("hold-for", 0).toInt
-    val _t_throughput = Integer.getInteger("throughput")
-    val _t_iterations = Integer.getInteger("iterations")
+  val concurrency = Integer.getInteger("concurrency", 1).toInt
+  val rampUpTime = Integer.getInteger("ramp-up", 0).toInt
+  val holdForTime = Integer.getInteger("hold-for", 0).toInt
+  val throughput = Integer.getInteger("throughput")
+  val iterationLimit = Integer.getInteger("iterations")
 
-    val _duration = _t_ramp_up + _t_hold_for
+  val durationLimit = rampUpTime + holdForTime
 
-    var httpConf = %(httpConf)s
-    var _scn = scenario("Taurus Scenario")
+  var httpConf = %(httpConf)s
+  var _scn = scenario("Taurus Scenario")
 
-    var _exec = %(_exec)s
+  var _exec = %(_exec)s
 
-    if (_t_iterations == null)
-        _scn = _scn.forever{_exec}
-     else
-        _scn = _scn.repeat(_t_iterations.toInt){_exec}
+  if (iterationLimit == null)
+    _scn = _scn.forever{_exec}
+  else
+    _scn = _scn.repeat(iterationLimit.toInt){_exec}
 
-    val _users =
-        if (_t_ramp_up > 0)
-            rampUsers(_t_concurrency) over (_t_ramp_up seconds)
-        else
-            atOnceUsers(_t_concurrency)
+  val _users =
+    if (rampUpTime > 0)
+      rampUsers(concurrency) over (rampUpTime seconds)
+    else
+      atOnceUsers(concurrency)
 
-    var _setUp = setUp(_scn.inject(_users).protocols(httpConf))
+  var _setUp = setUp(_scn.inject(_users).protocols(httpConf))
 
-    if (_t_throughput != null)
-        _setUp = _setUp.throttle(
-          reachRps(_t_throughput) in (_t_ramp_up),
-          holdFor(Int.MaxValue))
+  if (throughput != null)
+    _setUp = _setUp.throttle(
+      reachRps(throughput) in (rampUpTime),
+      holdFor(Int.MaxValue)
+    )
 
-    if (_duration > 0)
-        _setUp.maxDuration(_duration)
+  if (durationLimit > 0)
+    _setUp.maxDuration(durationLimit)
 }
--- tests/resources/gatling/generated1.scala
+++ tests/resources/gatling/generated1.scala
@@ -4,49 +4,51 @@ import io.gatling.core.Predef._
 import io.gatling.http.Predef._
 import scala.concurrent.duration._
 
-class TaurusSimulation_140130467712208 extends Simulation {
-    val _t_concurrency = Integer.getInteger("concurrency", 1).toInt
-    val _t_ramp_up = Integer.getInteger("ramp-up", 0).toInt
-    val _t_hold_for = Integer.getInteger("hold-for", 0).toInt
-    val _t_throughput = Integer.getInteger("throughput")
-    val _t_iterations = Integer.getInteger("iterations")
+class TaurusSimulation_139886093198672 extends Simulation {
+  val concurrency = Integer.getInteger("concurrency", 1).toInt
+  val rampUpTime = Integer.getInteger("ramp-up", 0).toInt
+  val holdForTime = Integer.getInteger("hold-for", 0).toInt
+  val throughput = Integer.getInteger("throughput")
+  val iterationLimit = Integer.getInteger("iterations")
 
-    val _duration = _t_ramp_up + _t_hold_for
+  val durationLimit = rampUpTime + holdForTime
 
-    var httpConf = http.baseURL("http://blazedemo.com")
-		.header("H1", "V1")
+  var httpConf = http.baseURL("http://blazedemo.com")
+    .header("H1", "V1")
 
-    var _scn = scenario("Taurus Scenario")
+  var _scn = scenario("Taurus Scenario")
 
-    var _exec = exec(
-			http("/reserve.php").post("/reserve.php")
-				.header("H2", "V2")
-				.body(StringBody("""Body Content"""))
-				.check(
-					substring("""bootstrap.min""").notExists)
-			.disableFollowRedirect
-		).pause(1).exec(
-			http("/").get("/")
-		).pause(2)
+  var _exec = exec(
+    http("/reserve.php").post("/reserve.php")
+      .header("H2", "V2")
+      .body(StringBody("""Body Content"""))
+      .check(
+        substring("""bootstrap.min""").notExists
+      )
+      .disableFollowRedirect
+  ).pause(1).exec(
+    http("/").get("/")
+  ).pause(2)
 
-    if (_t_iterations == null)
-        _scn = _scn.forever{_exec}
-     else
-        _scn = _scn.repeat(_t_iterations.toInt){_exec}
+  if (iterationLimit == null)
+    _scn = _scn.forever{_exec}
+  else
+    _scn = _scn.repeat(iterationLimit.toInt){_exec}
 
-    val _users =
-        if (_t_ramp_up > 0)
-            rampUsers(_t_concurrency) over (_t_ramp_up seconds)
-        else
-            atOnceUsers(_t_concurrency)
+  val _users =
+    if (rampUpTime > 0)
+      rampUsers(concurrency) over (rampUpTime seconds)
+    else
+      atOnceUsers(concurrency)
 
-    var _setUp = setUp(_scn.inject(_users).protocols(httpConf))
+  var _setUp = setUp(_scn.inject(_users).protocols(httpConf))
 
-    if (_t_throughput != null)
-        _setUp = _setUp.throttle(
-          reachRps(_t_throughput) in (_t_ramp_up),
-          holdFor(Int.MaxValue))
+  if (throughput != null)
+    _setUp = _setUp.throttle(
+      reachRps(throughput) in (rampUpTime),
+      holdFor(Int.MaxValue)
+    )
 
-    if (_duration > 0)
-        _setUp.maxDuration(_duration)
+  if (durationLimit > 0)
+    _setUp.maxDuration(durationLimit)
 }
