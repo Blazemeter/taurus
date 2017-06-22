1883c09f87baa40653be062f594604b7698a02a0
Dmitri Pribysh <dmand@yandex.ru>
2017-06-22 14:57:42 +0300

    Use mock for testing
    Because we don't want another blinking test.

--- tests/test_bza.py
+++ tests/test_bza.py
@@ -3,11 +3,18 @@ from unittest import skipUnless
 from bzt.bza import User
 from bzt.six import PY3, text_type
 from tests import BZTestCase
+from tests.mocks import BZMock
 
 
 class TestBZAClient(BZTestCase):
     @skipUnless(PY3, "Py3-only test")
     def test_bza_py3_unicode_token(self):
+        mock = BZMock()
+        mock.mock_get.update({
+            'https://a.blazemeter.com/api/v4/web/version': {"result": {}},
+        })
+
         user = User()
+        mock.apply(user)
         user.token = text_type("something:something")
         user.ping()
