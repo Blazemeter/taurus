424e5804e31d30e874f95f584b5043051d321c2e
Dmitri Pribysh <dmand@yandex.ru>
2017-07-27 17:55:12 +0300

    Add initial version of NUnit plugin

--- bzt/modules/csharp.py
+++ bzt/modules/csharp.py
@@ -14,16 +14,18 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import os
-import time
 
-from bzt import TaurusConfigError, ToolError
+from bzt import TaurusConfigError
 from bzt.modules import SubprocessedExecutor
-from bzt.engine import HavingInstallableTools, Scenario
-from bzt.utils import get_full_path, shell_exec, TclLibrary, RequiredTool, MirrorsManager
+from bzt.engine import HavingInstallableTools
+from bzt.utils import get_full_path
 
 
 class NUnitExecutor(SubprocessedExecutor, HavingInstallableTools):
     def install_required_tools(self):
+        # TODO: check for mono (in case of non-windows) and nuget
+        # TODO: `nuget install NUNit` and NUnit.Console
+        # TODO: `nuget install Selenium?`
         pass
 
     def prepare(self):
@@ -33,38 +35,7 @@ class NUnitExecutor(SubprocessedExecutor, HavingInstallableTools):
             raise TaurusConfigError("Script not passed to runner %s" % self)
 
         self.install_required_tools()
-        self._build_solution()
         self.reporting_setup(suffix=".ldjson")
-
-    def _build_solution(self):
-        script_dir = self.script
-        sln_files = [
-            filename
-            for filename in os.listdir(script_dir)
-            if filename.endswith('.sln')
-        ]
-        if not sln_files:
-            raise TaurusConfigError("Script dir for NUnit should contain '.sln' file")
-        sln_file = sln_files[0]
-        # NOTE: might need 'nuget restore SeleniumSuite/packages.config' first to install deps
-        cmdline = ["xbuild", "/p:Configuration=Release", sln_file]  # or is it `msbuild`?
-
-        with open(self.engine.create_artifact("build", ".out"), 'ab') as build_out, \
-            open(self.engine.create_artifact("build", ".err"), 'ab') as build_err:
-            self.log.debug("Building the solution: %s", cmdline)
-            process = shell_exec(cmdline, cwd=script_dir, stdout=build_out, stderr=build_err)
-            ret_code = process.poll()
-
-            while ret_code is None:
-                self.log.debug("Building the solution'...")
-                time.sleep(1)
-                ret_code = process.poll()
-
-        if ret_code != 0:
-            self.log.debug("build exit code: %s", ret_code)
-            with open(build_out.name) as err_file:
-                out = err_file.read()
-            raise ToolError("Build exited with code: %s\n %s" % (ret_code, out.strip()))
 
     def _collect_files(self, extensions):
         file_list = []
@@ -84,7 +55,7 @@ class NUnitExecutor(SubprocessedExecutor, HavingInstallableTools):
         #   SeleniumSuite/bin/Release/SeleniumSuite.dll -noresult
         script_dir = self.script
         target_assembly = self._collect_files({".dll"})
-        cmdline = ["mono", "packages/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe"]
+        cmdline = ["packages/NUnit.ConsoleRunner.3.7.0/tools/nunit3-console.exe"]
         cmdline += target_assembly
         cmdline += ["-noresult"]
         self._start_subprocess(cmdline, cwd=script_dir)
---
+++ dotnet/NUnitRunner/NUnitRunner/Program.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Xml;
+
+using Mono.Options;
+
+using NUnit.Engine;
+using NUnit.Engine.Extensibility;
+
+
+namespace NUnitRunner
+{
+    public class NUnitRunner : ITestEventListener
+    {
+		public void OnTestEvent(string report)
+        {
+            Console.WriteLine(report);
+		}
+
+		public static void Main(string[] args)
+        {
+
+			var shouldShowHelp = false;
+            int iterations = 0;
+            float holdFor = 0;
+            string reportFile = "report.ldjson";
+            string testAssembly = null;
+
+            // thses are the available options, not that they set the variables
+            var options = new OptionSet {
+                { "i|iterations=", "number of iterations over test suite to make.", (int n) => iterations = n },
+                { "d|duration=", "duration of test suite execution.", (float h) => holdFor = h },
+                { "r|report-file=", "Name of report file", r => reportFile = r },
+                { "t|target=", "Test suite", t => testAssembly = t },
+                { "h|help", "show this message and exit", h => shouldShowHelp = h != null },
+            };
+
+			try
+			{
+				options.Parse(args);
+			}
+			catch (OptionException e)
+			{
+				Console.WriteLine(e.Message);
+				Console.WriteLine("Try running with '--help' for more information.");
+				return;
+			}
+
+            Console.WriteLine("Iterations: {0}", iterations);
+            Console.WriteLine("Hold for: {0}", holdFor);
+            Console.WriteLine("Report file: {0}", reportFile);
+            Console.WriteLine("Target: {0}", testAssembly);
+
+            // TODO: print help if `shouldShowHelp`
+
+            if (testAssembly == null)
+                throw new Exception("Target test suite wasn't provided");
+
+            if (iterations == 0)
+                if (holdFor > 0)
+                    iterations = int.MaxValue;
+                else
+                    iterations = 1;
+
+			NUnitRunner listener = new NUnitRunner();
+
+			ITestEngine engine = TestEngineActivator.CreateInstance();
+			TestPackage package = new TestPackage(testAssembly);
+			ITestRunner runner = engine.GetRunner(package);
+
+            DateTime startTime = DateTime.Now;
+            for (int i = 0; i < iterations; i++)
+            {
+                runner.Run(listener, TestFilter.Empty);
+                TimeSpan offset = DateTime.Now - startTime;
+                if (holdFor > 0 && offset.TotalSeconds > holdFor)
+                    break;
+            }
+
+			Console.WriteLine("Hello World!");
+        }
+    }
+}
