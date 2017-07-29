3991e8fb0abd0d2f5717d22e26ddcbea86d977d6
Dmitri Pribysh <dmand@yandex.ru>
2017-07-29 15:28:06 +0300

    Support writing Taurus-compatible reports

---
+++ dotnet/NUnitRunner/NUnitRunner/NUnitTestClass.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+
+namespace NUnitRunner
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+        [Test()]
+        public void TestCase()
+        {
+            NUnitRunner.Main(new [] { "--iterations", "1", "--target", "../../NUnitSelenium/SeleniumSuite/bin/Release/SeleniumSuite.dll"});
+        }
+    }
+}
