fee5a9b713e9aa3d9b8d833147aa4462738cd3b3
greyfenrir <grey.fenrir@gmail.com>
2017-08-24 11:24:24 +0300

    debug

--- bzt/modules/__init__.py
+++ bzt/modules/__init__.py
@@ -101,7 +101,8 @@ class SubprocessedExecutor(ReportableExecutor, FileLister, SelfDiagnosable):
         if ret_code is not None:
             if ret_code != 0:
                 msg = "Test runner %s (%s) has failed with retcode %s"
-                raise ToolError(msg % (self.label, self.__class__.__name__, ret_code), self.get_error_diagnostics())
+                raise ToolError(msg % (self.label, self.__class__.__name__, ret_code) + '\n' +
+                                str(self.get_error_diagnostics()))
             return True
         return False
 
