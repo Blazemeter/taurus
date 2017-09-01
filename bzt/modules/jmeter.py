24582cbb66ba4d306be1a4f215b4f532bbc7b5f4
greyfenrir <grey.fenrir@gmail.com>
2017-09-02 02:44:12 +0300

    Feat/install for jmx (#654)
    * add logic for plugin detection, add change file
    
    * refactoring pmgr call, add test template
    
    * replace pmgr error with warning, add tests
    
    * add explanation to doc
    
    * fix doc
    
    * fix doc
    
    * replace jmeter with mock in engine tests
    
    * add switcher for install-for-jmx, make jmx tests simpler
    
    * fix tests
    
    * fix tests
    
    * move communicate to six
    
    * fix tests
    
    * fix misprint
    
    * use bzt.six.communicate for auto decoding
    
    * win check
    
    * win check
    
    * extended win check
    
    * new win check
    
    * wincheck
    
    * winchk
    
    * Update jmeter.py

--- bzt/modules/jmeter.py
+++ bzt/modules/jmeter.py
@@ -39,7 +39,8 @@ from bzt.modules.functional import FunctionalAggregator, FunctionalResultsReader
 from bzt.modules.provisioning import Local
 from bzt.modules.soapui import SoapUIScriptConverter
 from bzt.requests_model import ResourceFilesCollector
-from bzt.six import iteritems, string_types, StringIO, etree, binary_type, parse, unicode_decode, numeric_types
+from bzt.six import communicate
+from bzt.six import iteritems, string_types, StringIO, etree, parse, unicode_decode, numeric_types
 from bzt.utils import get_full_path, EXE_SUFFIX, MirrorsManager, ExceptionalDownloader, get_uniq_name
 from bzt.utils import shell_exec, BetterDict, guess_csv_dialect, ensure_is_dict, dehumanize_time
 from bzt.utils import unzip, RequiredTool, JavaVM, shutdown_process, ProgressBarContext, TclLibrary
@@ -57,8 +58,9 @@ class JMeterExecutor(ScenarioExecutor, WidgetProvider, FileLister, HavingInstall
     """
     MIRRORS_SOURCE = "https://jmeter.apache.org/download_jmeter.cgi"
     JMETER_DOWNLOAD_LINK = "https://archive.apache.org/dist/jmeter/binaries/apache-jmeter-{version}.zip"
-    PLUGINS_MANAGER = 'https://search.maven.org/remotecontent?filepath=' \
-                      'kg/apc/jmeter-plugins-manager/0.15/jmeter-plugins-manager-0.15.jar'
+    PLUGINS_MANAGER_VERSION = "0.16"
+    PLUGINS_MANAGER = 'https://search.maven.org/remotecontent?filepath=kg/apc/jmeter-plugins-manager/'\
+            '{ver}/jmeter-plugins-manager-{ver}.jar'.format(ver=PLUGINS_MANAGER_VERSION)
     CMDRUNNER = 'https://search.maven.org/remotecontent?filepath=kg/apc/cmdrunner/2.0/cmdrunner-2.0.jar'
     JMETER_VER = "3.2"
     UDP_PORT_NUMBER = None
@@ -278,6 +280,10 @@ class JMeterExecutor(ScenarioExecutor, WidgetProvider, FileLister, HavingInstall
                 is_jmx_generated = True
             else:
                 raise TaurusConfigError("You must specify either a JMX file or list of requests to run JMeter")
+
+        # check for necessary plugins and install them if needed
+        if self.settings.get("detect-plugins", True):
+            self.tool.install_for_jmx(self.original_jmx)
 
         if self.engine.aggregator.is_functional:
             flags = {"connectTime": True}
@@ -1501,13 +1507,10 @@ class JMeter(RequiredTool):
         try:
             with tempfile.NamedTemporaryFile(prefix="jmeter", suffix="log", delete=False) as jmlog:
                 jm_proc = shell_exec([self.tool_path, '-j', jmlog.name, '--version'], stderr=subprocess.STDOUT)
-                jmout, jmerr = jm_proc.communicate()
+                jmout, jmerr = communicate(jm_proc)
                 self.log.debug("JMeter check: %s / %s", jmout, jmerr)
 
             os.remove(jmlog.name)
-
-            if isinstance(jmout, binary_type):
-                jmout = jmout.decode()
 
             if "is too low to run JMeter" in jmout:
                 raise ToolError("Java version is too low to run JMeter")
@@ -1517,6 +1520,26 @@ class JMeter(RequiredTool):
         except OSError:
             self.log.debug("JMeter check failed.")
             return False
+
+    def _pmgr_call(self, params):
+        cmd = [self._pmgr_path()] + params
+        proc = shell_exec(cmd)
+        return communicate(proc)
+
+    def install_for_jmx(self, jmx_file):
+        if not os.path.isfile(jmx_file):
+            self.log.warning("Script %s not found" % jmx_file)
+            return
+
+        try:
+            out, err = self._pmgr_call(["install-for-jmx", jmx_file])
+            self.log.debug("Try to detect plugins for %s\n%s\n%s", jmx_file, out, err)
+        except BaseException as exc:
+            self.log.warning("Failed to detect plugins for %s: %s", jmx_file, exc)
+            return
+
+        if err and "Wrong command: install-for-jmx" in err:     # old manager
+            self.log.debug("pmgr can't discover jmx for plugins")
 
     def __install_jmeter(self, dest):
         if self.download_link:
@@ -1555,7 +1578,7 @@ class JMeter(RequiredTool):
         self.log.debug("Trying: %s", cmd)
         try:
             proc = shell_exec(cmd)
-            out, err = proc.communicate()
+            out, err = communicate(proc)
             self.log.debug("Install PluginsManager: %s / %s", out, err)
         except BaseException as exc:
             raise ToolError("Failed to install PluginsManager: %s" % exc)
@@ -1593,10 +1616,14 @@ class JMeter(RequiredTool):
                 env['JVM_ARGS'] = jvm_args
 
             proc = shell_exec(cmd)
-            out, err = proc.communicate()
+            out, err = communicate(proc)
             self.log.debug("Install plugins: %s / %s", out, err)
         except BaseException as exc:
             raise ToolError("Failed to install plugins %s: %s" % (plugin_str, exc))
+
+    def _pmgr_path(self):
+        dest = get_full_path(self.tool_path, step_up=2)
+        return os.path.join(dest, 'bin', 'PluginsManagerCMD' + EXE_SUFFIX)
 
     def install(self):
         dest = get_full_path(self.tool_path, step_up=2)
@@ -1608,7 +1635,7 @@ class JMeter(RequiredTool):
         direct_install_tools = [  # source link and destination
             [JMeterExecutor.PLUGINS_MANAGER, plugins_manager_path],
             [JMeterExecutor.CMDRUNNER, cmdrunner_path]]
-        plugins_manager_cmd = os.path.join(dest, 'bin', 'PluginsManagerCMD' + EXE_SUFFIX)
+        plugins_manager_cmd = self._pmgr_path()
 
         self.__install_jmeter(dest)
         self.__download_additions(direct_install_tools)
---
+++ tests/modules/jmeter/test_JMeterTool.py
@@ -0,0 +1,34 @@
+from . import MockJMeter
+from tests import BZTestCase, RESOURCES_DIR
+
+
+class TestJMeterTool(BZTestCase):
+    def setUp(self):
+        super(TestJMeterTool, self).setUp()
+        self.obj = MockJMeter()
+
+    def test_old_plugin(self):
+        self.sniff_log(self.obj.log)
+        self.obj.reaction = [{'output': ('one', 'Wrong command: install-for-jmx')}]
+        self.obj.install_for_jmx(RESOURCES_DIR + "/jmeter/jmx/http.jmx")
+
+        # mustn't call old plugin for detection
+        msg = "pmgr can't discover jmx for plugins"
+        self.assertIn(msg, self.log_recorder.debug_buff.getvalue())
+
+    def test_new_plugin(self):
+        self.sniff_log(self.obj.log)
+
+        self.obj.reaction = [{"output": ("one", "two"), "raise": RuntimeError("runtime error")}]
+        self.obj.install_for_jmx(RESOURCES_DIR + "/jmeter/jmx/http.jmx")
+
+        self.assertIn("Failed to detect plugins", self.log_recorder.warn_buff.getvalue())
+
+    def test_wrong_jmx_name(self):
+        self.sniff_log(self.obj.log)
+
+        self.obj.reaction = [{"output": ("one", "two")}]
+        jmx_file = RESOURCES_DIR + "/jmeter/jmx/really_wrong_name.jmx"
+        self.obj.install_for_jmx(RESOURCES_DIR + "/jmeter/jmx/really_wrong_name.jmx")
+
+        self.assertIn(jmx_file + " not found", self.log_recorder.warn_buff.getvalue())
