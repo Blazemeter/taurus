36c4281c3e20fb18000d5f359b439660e2de9e4c
greyfenrir <grey.fenrir@gmail.com>
2017-09-13 18:03:25 +0300

    add mac debug

--- tests/ci/travis/build_brew_formula.sh
+++ tests/ci/travis/build_brew_formula.sh
@@ -19,9 +19,9 @@ FORMULA_FILE="${BUILD_DIR}/bzt.rb"
 #   echo | ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install)"
 # fi
 
-echo `find $(brew --prefix) -type d -name "Formula" -exec ls -d -1 {}`
+echo "`find $(brew --prefix) -type d -name "Formula" -exec ls -d -1 {}`"
 BREW_FORMULA="$(brew --prefix)/Homebrew/Library/Taps/homebrew/homebrew-core/Formula/bzt.rb"
-echo $BREW_FORMULA
+echo "$BREW_FORMULA"
 
 # write header to formula
 cat << EOF > "${FORMULA_FILE}"
@@ -74,7 +74,9 @@ brew reinstall bzt -vvv
 brew test bzt
 brew audit --strict --online bzt
 
-echo "*** ${FORMULA_FILE}:"
+echo "formulas path:`find $(brew --prefix) -type d -name "Formula" -exec ls -d -1 {}`"
+echo "$BREW_FORMULA=${BREW_FORMULA}"
+echo "*** ff:${FORMULA_FILE}:"
 echo ">>>>> start of formula"
 cat ${FORMULA_FILE}
 echo ">>>>> end of formula"
