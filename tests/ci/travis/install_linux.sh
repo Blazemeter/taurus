c973f560257c48640415fd3509c560d7d8c6a6b0
greyfenrir <grey.fenrir@gmail.com>
2017-09-13 12:35:28 +0300

    merge from master

--- tests/ci/travis/install_linux.sh
+++ tests/ci/travis/install_linux.sh
@@ -4,6 +4,6 @@ mono --version
 nvm install 4.2
 nvm use 4.2
 gem install rspec
-pip install colorlog jsonpath-rw pyyaml psutil lxml cssselect urwid six selenium progressbar33 locustio pyvirtualdisplay astunparse ipaddress
-pip install git+https://github.com/Blazemeter/apiritif.git@master
-pip install --upgrade codecov nose nose-exclude
+pip install pip --upgrade
+pip install -r requirements.txt
+pip install codecov nose-exclude locustio
