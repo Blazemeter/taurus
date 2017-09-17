72dc3de7ce6da0cfe89a1345d16b84795339631f
greyfenrir <grey.fenrir@gmail.com>
2017-09-17 23:35:53 +0300

    add libyaml installation

--- tests/ci/travis/install_osx.sh
+++ tests/ci/travis/install_osx.sh
@@ -1,9 +1,3 @@
 #!/bin/sh -e
 
-mono --version
-nvm install 4.2
-nvm use 4.2
-gem install rspec
-pip install pip --upgrade
-pip install -r requirements.txt
-pip install codecov nose-exclude locustio
+gem install libyaml
