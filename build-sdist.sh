7dd294cb216885b5493fa2222dc162c1f85cf181
Andrey Pohilko <apc4@ya.ru>
2017-09-25 10:23:52 +0300

    Make build script to rebuild Java resources

--- build-sdist.sh
+++ build-sdist.sh
@@ -3,6 +3,24 @@
 echo "Cleaning environment"
 python $(dirname $0)/setup.py clean
 
+echo "Building Grinder plugin"
+pushd $(dirname $0)/java/grinder_logger
+mvn clean package
+cp target/*.jar ../../bzt/resources/
+popd
+
+echo "Building JUnit plugin"
+pushd $(dirname $0)/java/JUnit_plugin
+mvn clean package
+cp target/*.jar ../../bzt/resources/
+popd
+
+echo "Building TestNG plugin"
+pushd $(dirname $0)/java/TestNG_plugin
+mvn clean package
+cp target/*.jar ../../bzt/resources/
+popd
+
 echo "Building NUnit plugin"
 pushd $(dirname $0)/dotnet/NUnitRunner
 ./rebuild.sh
