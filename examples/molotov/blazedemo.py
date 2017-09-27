3f322a03a4f38907c7002cd69f0cce2699a91ea6
Dmitri Pribysh <dmand@yandex.ru>
2017-09-27 13:56:13 +0300

    Simplify molotov example test

--- examples/molotov/blazedemo.py
+++ examples/molotov/blazedemo.py
@@ -5,5 +5,3 @@ import molotov
 async def scenario_one(session):
     async with session.get('http://blazedemo.com/') as resp:
         assert resp.status == 200
-        body = await resp.text()
-        assert "Welcome" in body
