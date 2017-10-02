537b99c3c3b5dba28d4010777c2537b50aec02d5
greyfenrir <grey.fenrir@gmail.com>
2017-10-02 09:53:28 +0300

    new target url

--- tests/ci/build-brew.sh
+++ tests/ci/build-brew.sh
@@ -85,7 +85,7 @@ cat << EOF >> "${FORMULA}"
   end
 
   test do
-    test_cmd = "#{bin}/bzt -o execution.0.executor=nose -o execution.0.scenario.requests.0=http://blazedemo.com"
+    test_cmd = "#{bin}/bzt -o execution.0.executor=nose -o execution.0.scenario.requests.0=http://gettaurus.org"
     output_text = pipe_output(test_cmd)
     assert_match "INFO: Samples count: 1, 0.00% failures", output_text
   end
