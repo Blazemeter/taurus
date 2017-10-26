af24154fedecf30d2fc333eed2e3da636e39d6a9
Dmitri Pribysh <dmand@yandex.ru>
2017-10-26 13:00:38 +0300

    Introduce Webdriver.io support for Taurus (#683)
    * Start working on webdriver.io plugin
    
    * Finalize the implementation of wdio support
    
    * Add diagnostics to ToolError message
    
    * Code style fixes
    
    * Attempt to fix tests
    
    * Another code style fix
    
    * Use config filename when passing cmdline
    
    * Debugging
    
    * Add script dir to a NODE_PATH
    
    * Don't juggle with NODE_PATH at all
    
    * Experiments
    
    * Finally figure out NODE_PATH issue (hopefully)
    
    * Use another mechanism for selenium/driver/browser tests
    
    * Swtich tests to firefox
    
    * Tweaks
    
    * Hopefully fix for windows
    
    * Switch to node 6.11 for mac builds
    
    * Another windows fix
    
    * Attempt to optimize for windows
    
    * Refactor tests to not use webdriver infra
    
    * Add example
    
    * Fix error capturing and exit code
    
    * Use find_file for `cwd` in shellexec
    
    * Add full wdio example
    
    * Try to force node 6 for appveyor
    
    * Another attempt
    
    * Improvements
    
    * Add docs
    
    * Add a changefile
    
    * Update supported executor list

--- tests/modules/selenium/test_javascript.py
+++ tests/modules/selenium/test_javascript.py
@@ -4,7 +4,8 @@ import time
 
 from os.path import join, exists, dirname
 from bzt.modules import javascript
-from bzt.utils import get_full_path
+from bzt.modules.javascript import WebdriverIOExecutor
+from bzt.utils import get_full_path, shell_exec, is_windows
 from tests import BUILD_DIR, RESOURCES_DIR
 from tests.modules.selenium import SeleniumTestCase
 
@@ -89,7 +90,7 @@ class TestSeleniumMochaRunner(SeleniumTestCase):
         self.assertEqual(len(lines), 9)
 
     def test_install_mocha(self):
-        dummy_installation_path = get_full_path(BUILD_DIR + "selenium-taurus/mocha")
+        dummy_installation_path = get_full_path(BUILD_DIR + "selenium-taurus/nodejs")
         mocha_link = get_full_path(RESOURCES_DIR + "selenium/mocha-3.1.0.tgz")
         wd_link = get_full_path(RESOURCES_DIR + "selenium/selenium-webdriver-1.0.0.tgz")
 
@@ -124,3 +125,75 @@ class TestSeleniumMochaRunner(SeleniumTestCase):
             javascript.SELENIUM_WEBDRIVER_NPM_PACKAGE_NAME = orig_wd_package
             if old_node_path:
                 os.environ["NODE_PATH"] = old_node_path
+
+
+class TestWebdriverIOExecutor(SeleniumTestCase):
+    def test_prepare(self):
+        self.obj.execution.merge({
+            "runner": "wdio",
+            "scenario": {
+                "script": RESOURCES_DIR + "selenium/js-mocha/bd_scenarios.js"
+            }
+        })
+        self.obj.prepare()
+        self.assertIsInstance(self.obj.runner, WebdriverIOExecutor)
+
+    def run_command(self, cmdline, stream_name, cwd):
+        out = open(self.obj.engine.create_artifact(stream_name, ".out"), "wt")
+        err = open(self.obj.engine.create_artifact(stream_name, ".err"), "wt")
+        process = shell_exec(args=cmdline, stdout=out, stderr=err, cwd=cwd)
+        while process.poll() is None:
+            time.sleep(0.5)
+        out.close()
+        err.close()
+        self.obj.log.debug("%s out: %s", cmdline, open(out.name, 'r').read())
+        self.obj.log.debug("%s err: %s", cmdline, open(err.name, 'r').read())
+
+    def full_run(self, config, script_dir):
+        self.configure(config)
+
+        self.run_command(["npm.cmd" if is_windows() else "npm", "install"], "npm-install", script_dir)
+
+        self.obj.prepare()
+        self.obj.startup()
+        while not self.obj.check():
+            time.sleep(1)
+        self.obj.shutdown()
+
+    def test_simple(self):
+        self.full_run({
+            'execution': {
+                "runner": "wdio",
+                "scenario": {
+                    "script": RESOURCES_DIR + "selenium/js-wdio/wdio.conf.js",
+                }
+            },
+        }, script_dir=RESOURCES_DIR + "selenium/js-wdio")
+        self.assertTrue(exists(self.obj.runner.report_file))
+        lines = open(self.obj.runner.report_file).readlines()
+        self.assertEqual(len(lines), 1)
+
+    def test_hold(self):
+        self.full_run({
+            'execution': {
+                'hold-for': '5s',
+                'scenario': {'script': RESOURCES_DIR + 'selenium/js-wdio/wdio.conf.js'},
+                'runner': 'wdio',
+            },
+        }, script_dir=RESOURCES_DIR + "selenium/js-wdio")
+        self.assertTrue(exists(self.obj.runner.report_file))
+        duration = time.time() - self.obj.start_time
+        self.assertGreater(duration, 5)
+
+    def test_iterations(self):
+        self.full_run({
+            'execution': {
+                'iterations': 3,
+                'scenario': {'script': RESOURCES_DIR + 'selenium/js-wdio/wdio.conf.js'},
+                'runner': 'wdio'
+            },
+        }, script_dir=RESOURCES_DIR + "selenium/js-wdio")
+
+        self.assertTrue(exists(self.obj.runner.report_file))
+        lines = open(self.obj.runner.report_file).readlines()
+        self.assertEqual(len(lines), 3)
