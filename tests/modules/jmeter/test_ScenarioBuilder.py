2ecaef07ac1a522a4667d270c0ec50926bb557d0
greyfenrir <grey.fenrir@gmail.com>
2017-11-16 14:11:24 +0300

    refactoring of tests

--- tests/modules/jmeter/test_ScenarioBuilder.py
+++ tests/modules/jmeter/test_ScenarioBuilder.py
@@ -1,6 +1,6 @@
 from . import MockJMeterExecutor
 from bzt.modules.jmeter import JMeterScenarioBuilder
-from tests import BZTestCase
+from tests import BZTestCase, RESOURCES_DIR
 from tempfile import NamedTemporaryFile
 from bzt.six import etree
 
@@ -24,20 +24,79 @@ class TestScenarioBuilder(BZTestCase):
         self.obj.scenario.data.merge(scenario)
         self.obj.executor.settings["version"] = version
 
+    @staticmethod
+    def get_plugin_json_extractor_config(xml_tree):
+        cfg = {}
+        block_name = "com.atlantbh.jmeter.plugins.jsonutils.jsonpathextractor.JSONPathExtractor"
+        blocks = xml_tree.findall(".//%s" % block_name)
+        for block in blocks:
+            varname = block.find(".//stringProp[@name='VAR']")
+            jsonpath = block.find(".//stringProp[@name='JSONPATH']")
+            default = block.find(".//stringProp[@name='DEFAULT']")
+            subject = block.find(".//stringProp[@name='SUBJECT']")
+            from_variable = block.find(".//stringProp[@name='VARIABLE']")
+            varname = varname.text
+            jsonpath = jsonpath.text
+            if default is not None:
+                default = default.text
+            if (subject is not None) and subject.text == "VAR" and (from_variable is not None):
+                from_variable = from_variable.text
+            else:
+                from_variable = None
+            cfg[varname] = {"jsonpath": jsonpath, "default": default, "from_variable": from_variable}
+        return cfg
+
+    @staticmethod
+    def get_internal_json_extractor_config(xml_tree):
+        cfg = {}
+        blocks = xml_tree.findall(".//JSONPostProcessor")
+        for block in blocks:
+            varname = block.find(".//stringProp[@name='VAR']")
+            jsonpath = block.find(".//stringProp[@name='JSONPostProcessor.jsonPathExprs']")
+            default = block.find(".//stringProp[@name='JSONPostProcessor.defaultValues']")
+            match_num = block.find(".//stringProp[@name='JSONPostProcessor.match_numbers']")
+            scope = block.find(".//stringProp[@name='Sample.scope']")
+            from_variable = block.find(".//stringProp[@name='Scope.variable']")
+            concat = block.find(".//booleanProp[@name='JSONPostProcessor.compute_concat']")
+            varname = varname.text
+            jsonpath = jsonpath.text
+            if default is not None:
+                default = default.text
+            if (scope is not None) and scope.text == "variable" and (from_variable is not None):
+                scope = scope.text
+                from_variable = from_variable.text
+            else:
+                from_variable = None
+
+            cfg[varname] = {"jsonpath": jsonpath, "default": default, "scope": scope,
+                            "from_variable": from_variable, "match_num": match_num, "concat": concat}
+
+        return cfg
+
+    def test_plugin_config_reader(self):
+        xml_tree = etree.fromstring(open(RESOURCES_DIR + "jmeter/jmx/json_extractors.jmx", "rb").read())
+        target = {}
+        self.assertEqual(target, self.get_plugin_json_extractor_config(xml_tree))
+
     def test_old_jmeter(self):
         """ versions before 3.0 must use JSON plugin for extracting purposes """
         self.configure(scenario={"requests": [{
                 "url": "http://blazedemo.com",
-                "extract-jsonpath": {"IP": "$.net[0].ip"}}]},
+                "extract-jsonpath": {
+                    "IP": "$.net[0].ip",
+                    "URL": {
+                        "jsonpath": "$.net[1].url",
+                        "default": "def",
+                        "from-variable": "Jm_VaR"}}}]},
             version="2.13")
         self.obj.save(self.jmx)
         xml_tree = etree.fromstring(open(self.jmx, "rb").read())
-        block_name = "com.atlantbh.jmeter.plugins.jsonutils.jsonpathextractor.JSONPathExtractor"
-        blocks = xml_tree.findall(".//%s" % block_name)
-        self.assertEqual(1, len(blocks))
-        self.assertEqual("IP", blocks[0].find(".//stringProp[@name='VAR']").text)
-        self.assertEqual("$.net[0].ip", blocks[0].find(".//stringProp[@name='JSONPATH']").text)
-        self.assertEqual("NOT_FOUND", blocks[0].find(".//stringProp[@name='DEFAULT']").text)
+        cfg = self.get_plugin_json_extractor_config(xml_tree)
+        self.assertEqual(2, len(cfg))
+        target = {
+            "IP": {"jsonpath": "$.net[0].ip", "default": "NOT_FOUND", "from_variable": None},
+            "URL": {"jsonpath": "$.net[1].url", "default": "def", "from_variable": "Jm_VaR"}}
+        self.assertEqual(target, cfg)
 
     def test_new_extractor(self):
         """ versions after 3.0 use integrated JSON extractor """
@@ -47,10 +106,8 @@ class TestScenarioBuilder(BZTestCase):
             version="3.3")
         self.obj.save(self.jmx)
         xml_tree = etree.fromstring(open(self.jmx, "rb").read())
-        prefix = "JSONPostProcessor"
-        blocks = xml_tree.findall(".//%s" % prefix)
-        self.assertEqual(1, len(blocks))
-        self.assertEqual("IP", blocks[0].find(".//stringProp[@name='%s.referenceNames']" % prefix).text)
-        self.assertEqual("$.net[0].ip", blocks[0].find(".//stringProp[@name='%s.jsonPathExprs']" % prefix).text)
-        self.assertEqual("NOT_FOUND", blocks[0].find(".//stringProp[@name='%s.defaultValues']" % prefix).text)
+        cfg = self.get_internal_json_extractor_config(xml_tree)
+        self.assertEqual(1, len(cfg))
+        target = {"IP": {"jsonpath": "$.net[0].ip", "default": "NOT_FOUND"}}
+        self.assertEqual(target, cfg)
 
