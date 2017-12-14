fee9ec69df4c32646cff482b51edfd23849d35a7
greyfenrir <grey.fenrir@gmail.com>
2017-12-14 10:35:46 +0300

    debug

--- bzt/modules/molotov.py
+++ bzt/modules/molotov.py
@@ -165,25 +165,27 @@ class Molotov(RequiredTool):
         import subprocess
         PYTHON = os.environ.get('PYTHON', '')
 
+        python = PYTHON + '/python'
         try:
-            py_out = subprocess.check_output(['where', 'python3'], stderr=subprocess.STDOUT)
+            py_out = subprocess.check_output([python, '--version'], stderr=subprocess.STDOUT)
         except BaseException as exc:
             py_out = exc
 
         try:
-            pip_out = subprocess.check_output(['python3', '-m', 'pip', 'list'], stderr=subprocess.STDOUT)
+            pip_list = subprocess.check_output([python, '-m', 'pip', 'list'], stderr=subprocess.STDOUT)
         except BaseException as exc:
-            pip_out = exc
+            pip_list = exc
 
         raise ToolError(
             "You must install molotov tool (version 1.4 or greater) to use it\n"
             "tool_path: {tool_path}\n"
-            "exc: {exc}\nout: {out}\n"
-            "$PYTHON: PYTHON"
+            "molotov --version: {exc}\n"
+            "out: {out}\n"
+            "$PYTHON: {PYTHON}\n"
             "python: {python}\n"
-            "pip list: {pip_out}".format(
+            "pip list: {pip_list}".format(
                 tool_path=self.tool_path, exc=self.exc, out=self.out,
-                python=py_out, pip_out=pip_out, PYTHON=PYTHON))
+                python=py_out, pip_list=pip_list, PYTHON=PYTHON))
 
 
 class MolotovReportReader(ResultsReader):
