caefddb460457aa0bf9703321e21603d7bf65f3a
greyfenrir <grey.fenrir@gmail.com>
2017-12-14 17:54:15 +0300

    fix memory leaks in JTLReader tests

--- tests/modules/jmeter/test_JTLReader.py
+++ tests/modules/jmeter/test_JTLReader.py
@@ -10,13 +10,27 @@ from tests.mocks import EngineEmul
 from bzt.modules.aggregator import DataPoint
 
 
-class TestJTLReader(BZTestCase):
+def close_file(obj):
+    if obj and obj.file and obj.file.fds:
+        obj.file.fds.close()
+
+
+class TestFuncJTLReader(BZTestCase):
+    def setUp(self):
+        super(TestFuncJTLReader, self).setUp()
+        self.obj = None
+
+    def configure(self, jtl_file):
+        engine = EngineEmul()
+        self.obj = FuncJTLReader(jtl_file, engine, logging.getLogger(''))
+
+    def tearDown(self):
+        close_file(self.obj)
+        super(TestFuncJTLReader, self).tearDown()
+
     def test_functional_reader_pass(self):
-        engine_obj = EngineEmul()
-        obj = FuncJTLReader(RESOURCES_DIR + "/jmeter/jtl/resource-errors-no-fail.jtl",
-                            engine_obj,
-                            logging.getLogger(''))
-        samples = list(obj.read(last_pass=True))
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/resource-errors-no-fail.jtl")
+        samples = list(self.obj.read(last_pass=True))
         self.assertEqual(2, len(samples))
         first = samples[0]
         self.assertEqual(first.test_case, "HTTP Request")
@@ -28,11 +42,8 @@ class TestJTLReader(BZTestCase):
         self.assertEqual(first.error_trace, "")
 
     def test_functional_reader_failed(self):
-        engine_obj = EngineEmul()
-        obj = FuncJTLReader(RESOURCES_DIR + "/jmeter/jtl/standard-errors.jtl",
-                            engine_obj,
-                            logging.getLogger(''))
-        samples = list(obj.read(last_pass=True))
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/standard-errors.jtl")
+        samples = list(self.obj.read(last_pass=True))
         self.assertEqual(185, len(samples))
         first = samples[0]
         self.assertEqual(first.test_case, "http://blazedemo.com/some-more-or-less-long-label")
@@ -43,11 +54,8 @@ class TestJTLReader(BZTestCase):
         self.assertEqual(first.error_msg, "The operation lasted too long")
 
     def test_functional_reader_broken(self):
-        engine_obj = EngineEmul()
-        obj = FuncJTLReader(RESOURCES_DIR + "/jmeter/jtl/standard-errors.jtl",
-                            engine_obj,
-                            logging.getLogger(''))
-        samples = list(obj.read(last_pass=True))
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/standard-errors.jtl")
+        samples = list(self.obj.read(last_pass=True))
         self.assertEqual(185, len(samples))
         sample = samples[8]
         self.assertEqual(sample.test_case, "http://blazedemo.com/some-more-or-less-long-label")
@@ -59,11 +67,8 @@ class TestJTLReader(BZTestCase):
         self.assertTrue(sample.error_trace.startswith("java.net.SocketTimeoutException: Read timed out"))
 
     def test_functional_reader_extras(self):
-        engine_obj = EngineEmul()
-        obj = FuncJTLReader(RESOURCES_DIR + "/jmeter/jtl/trace.jtl",
-                            engine_obj,
-                            logging.getLogger(''))
-        samples = list(obj.read(last_pass=True))
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/trace.jtl")
+        samples = list(self.obj.read(last_pass=True))
         self.assertEqual(1, len(samples))
         sample = samples[0]
         self.assertIsNotNone(sample.extras)
@@ -73,28 +78,22 @@ class TestJTLReader(BZTestCase):
             'requestHeaders', 'requestMethod', 'requestSize', 'requestURI',
             'responseBody', 'responseBodySize', 'responseCode', 'responseHeaders',
             'responseMessage', 'responseSize',
-            "threadId", "threadGroup",
-        ]
+            "threadId", "threadGroup"]
         for field in set(fields) - set(FuncJTLReader.FILE_EXTRACTED_FIELDS):
             self.assertIn(field, sample.extras)
         self.assertEqual(sample.extras["requestURI"], "http://blazedemo.com/")
         self.assertEqual(sample.extras["requestMethod"], "GET")
 
     def test_functional_reader_artifact_files(self):
-        engine_obj = EngineEmul()
-        obj = FuncJTLReader(RESOURCES_DIR + "/jmeter/jtl/trace.jtl",
-                            engine_obj,
-                            logging.getLogger(''))
-        samples = list(obj.read(last_pass=True))
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/trace.jtl")
+        samples = list(self.obj.read(last_pass=True))
         self.assertEqual(1, len(samples))
-        self.assertTrue(os.path.exists(os.path.join(engine_obj.artifacts_dir, "sample-responseBody.bin")))
+        sample_path = os.path.join(self.obj.engine.artifacts_dir, "sample-responseBody.bin")
+        self.assertTrue(os.path.exists(sample_path))
 
     def test_functional_reader_extras_assertions(self):
-        engine_obj = EngineEmul()
-        obj = FuncJTLReader(RESOURCES_DIR + "/jmeter/jtl/trace.jtl",
-                            engine_obj,
-                            logging.getLogger(''))
-        samples = list(obj.read(last_pass=True))
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/trace.jtl")
+        samples = list(self.obj.read(last_pass=True))
         self.assertEqual(1, len(samples))
         sample = samples[0]
         self.assertIsNotNone(sample.extras)
@@ -108,55 +107,76 @@ class TestJTLReader(BZTestCase):
                                   "errorMessage": "Test failed: text expected to contain /something/"})
 
     def test_functional_reader_extras_empty_body(self):
-        engine_obj = EngineEmul()
-        obj = FuncJTLReader(RESOURCES_DIR + "/jmeter/jtl/cookies.jtl",
-                            engine_obj,
-                            logging.getLogger(''))
-        samples = list(obj.read(last_pass=True))
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/cookies.jtl")
+        samples = list(self.obj.read(last_pass=True))
         self.assertEqual(2, len(samples))
         sample = samples[1]
         self.assertIsNotNone(sample.extras)
         self.assertEqual(sample.extras["requestCookies"], {'hello': 'world', 'visited': 'yes'})
 
     def test_functional_reader_extract(self):
-        engine_obj = EngineEmul()
-        obj = FuncJTLReader(RESOURCES_DIR + "/jmeter/jtl/crash_trace.jtl",
-                            engine_obj,
-                            logging.getLogger(''))
-        samples = list(obj.read(last_pass=True))
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/crash_trace.jtl")
+        samples = list(self.obj.read(last_pass=True))
         self.assertNotEqual(len(samples), 0)
 
+
+class TestJTLErrorsReader(BZTestCase):
+    def setUp(self):
+        super(TestJTLErrorsReader, self).setUp()
+        self.obj = None
+
+    def configure(self, jtl_file):
+        self.obj = JTLErrorsReader(jtl_file, logging.getLogger(''))
+
+    def tearDown(self):
+        close_file(self.obj)
+        super(TestJTLErrorsReader, self).tearDown()
+
     def test_nonstandard_errors_format(self):
-        obj = JTLErrorsReader(RESOURCES_DIR + "/jmeter/jtl/nonstandard-errors.jtl", logging.getLogger(''))
-        obj.read_file()
-        values = obj.get_data(sys.maxsize)
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/nonstandard-errors.jtl")
+        self.obj.read_file(final_pass=True)
+        values = self.obj.get_data(sys.maxsize)
         self.assertNotEquals(values[''][0]['msg'].find('Cannot find function error in object FirefoxDriver'), -1)
 
     def test_standard_errors_format(self):
-        obj = JTLErrorsReader(RESOURCES_DIR + "/jmeter/jtl/standard-errors.jtl", logging.getLogger(''))
-        obj.read_file()
-        values = obj.get_data(sys.maxsize)
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/standard-errors.jtl")
+        self.obj.read_file(final_pass=True)
+        values = self.obj.get_data(sys.maxsize)
         self.assertEquals(3, len(values))
 
+
+class TestJTLReader(BZTestCase):
+    def setUp(self):
+        super(TestJTLReader, self).setUp()
+        self.obj = None
+
+    def configure(self, jtl_file):
+        self.obj = JTLReader(jtl_file, logging.getLogger(''))
+
+    def tearDown(self):
+        if self.obj:
+            close_file(self.obj.csvreader)
+            close_file(self.obj.errors_reader)
+        super(TestJTLReader, self).tearDown()
+
     def test_tranctl_jtl(self):
-        obj = JTLReader(RESOURCES_DIR + "/jmeter/jtl/tranctl.jtl", logging.getLogger(''), None)
-        values = [x for x in obj.datapoints(True)]
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/tranctl.jtl")
+        values = [x for x in self.obj.datapoints(final_pass=True)]
         self.assertEquals(1, len(values))
 
     def test_tabs_jtl(self):
-        obj = JTLReader(RESOURCES_DIR + "/jmeter/jtl/tabs.jtl", logging.getLogger(''), None)
-        values = [x for x in obj.datapoints(True)]
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/tabs.jtl")
+        values = [x for x in self.obj.datapoints(final_pass=True)]
         self.assertEquals(4, len(values))
 
-    def test_areader_unicode(self):
-        reader = JTLReader(RESOURCES_DIR + "/jmeter/jtl/unicode.jtl", logging.getLogger(''), None)
-        reader.ignored_labels = [u"Тест.Эхо"]
-        for point in reader.datapoints():
+    def test_reader_unicode(self):
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/unicode.jtl")
+        self.obj.ignored_labels = [u"Тест.Эхо"]
+        for point in self.obj.datapoints(final_pass=True):
             cumulative = point[DataPoint.CUMULATIVE]
             self.assertIn(u"САП.АутентифицироватьРасш", cumulative)
             self.assertNotIn(u"Тест.Эхо", cumulative)
 
     def test_jtl_doublequoting(self):
-        obj = JTLReader(RESOURCES_DIR + "/jmeter/jtl/doublequoting.jtl", logging.getLogger(), None)
-        list(obj.datapoints(True))
-
+        self.configure(RESOURCES_DIR + "/jmeter/jtl/doublequoting.jtl")
+        list(self.obj.datapoints(final_pass=True))
