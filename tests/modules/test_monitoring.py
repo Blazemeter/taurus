cfd1f819417b01fa3ba3eb4356c613b3d4ece7ae
greyfenrir <grey.fenrir@gmail.com>
2017-12-21 10:27:54 +0300

    fix test

--- tests/modules/test_monitoring.py
+++ tests/modules/test_monitoring.py
@@ -159,7 +159,7 @@ class TestMonitoring(BZTestCase):
             psutil.net_io_counters = lambda: None
             psutil.disk_io_counters = lambda: None
 
-            client.monitor.get_resource_stats()  # should throw no exception
+            client.monitor.get_resource_stats(['cpu'])  # should throw no exception
         finally:
             psutil.net_io_counters = net_io_counters
             psutil.disk_io_counters = disk_io_counters
