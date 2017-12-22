32551dfb8d3256afb7eca865a1217401f2f8db71
Dmitri Pribysh <dmand@yandex.ru>
2017-12-22 13:49:15 +0300

    Play around

--- bzt/hdr_histogram.py
+++ bzt/hdr_histogram.py
@@ -20,12 +20,10 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-from __future__ import division, print_function
+from __future__ import division
 
 import ctypes
 from abc import abstractmethod
-from builtins import range
-from builtins import object
 import math
 import sys
 
