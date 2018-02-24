2aa16bd69864d05264c337c89d322799a083eed1
Dmitri Pribysh <dmand@yandex.ru>
2018-02-24 11:44:47 +0300

    Remove unused import

--- bzt/modules/aggregator.py
+++ bzt/modules/aggregator.py
@@ -16,7 +16,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import copy
-import json
 import logging
 import re
 from abc import abstractmethod
