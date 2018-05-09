fab8b2509c6e3b4ff3ad09bf31e68352c4325390
David <3dgiordano@gmail.com>
2018-05-08 21:44:54 -0300

    Window Manager, serial window (SIDE/SideeX), handlers and win name

--- bzt/resources/selenium_taurus_extras.py
+++ bzt/resources/selenium_taurus_extras.py
@@ -15,6 +15,7 @@ limitations under the License.
 """
 
 from string import Template as StrTemplate
+from selenium.common.exceptions import NoSuchWindowException
 
 
 class Apply(StrTemplate):
@@ -40,3 +41,38 @@ class Template:
         self.tmpl.template = template
         self.tmpl.variables = self.variables
         return str(self.tmpl)
+
+
+class WindowManager:
+
+    def __init__(self, driver):
+        self.driver = driver
+        self.windows = {}
+
+    def switch(self, window_name):
+        if window_name.isdigit():
+            wnd_handlers = self.driver.window_handles
+            if len(wnd_handlers) <= int(window_name) and int(window_name) >= 0:
+                self.driver.switch_to.window(wnd_handlers[int(window_name)])
+            else:
+                raise NoSuchWindowException("Invalid Window ID: %s" % window_name)
+        else:
+            if window_name.startswith("win_ser_"):
+                if window_name == "win_ser_local":
+                    wnd_handlers = self.driver.window_handles
+                    if len(wnd_handlers) > 0:
+                        self.driver.switch_to.window(wnd_handlers[0])
+                    else:
+                        raise NoSuchWindowException("Invalid Window ID: %s" % window_name)
+                else:
+                    if window_name not in self.windows:
+                        self.windows[window_name] = self.driver.window_handles[-1]
+                        self.driver.switch_to.window(self.windows[window_name])
+                    else:
+                        self.driver.switch_to.window(self.windows[window_name])
+            else:
+                self.driver.switch_to.window(window_name)
+
+    def close(self, window_name):
+        self.switch(window_name)
+        self.driver.close()
