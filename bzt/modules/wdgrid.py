73ce2f1d1f2ac40164126c47c7817db4d7f06f14
Andrey Pohilko <apc4@ya.ru>
2018-05-31 11:28:42 +0300

    Self-contain vnc viewer for now

--- bzt/modules/wdgrid.py
+++ bzt/modules/wdgrid.py
@@ -1,19 +1,21 @@
 import copy
+import logging
 import multiprocessing
+import os
 import time
 
+import pygame
+from twisted.internet import reactor
+from vncdotool import rfb
+
 from bzt import TaurusConfigError, NormalShutdown
+from bzt import resources
 from bzt.bza import User, WDGridImages
 from bzt.engine import ScenarioExecutor
-from bzt.modules import vncviewer
 from bzt.modules.blazemeter import CloudProvisioning
 from bzt.modules.provisioning import Local
 from bzt.six import text_type, urlparse
 from bzt.utils import to_json
-
-
-def _start_vnc(params):
-    (params[0], 'secret', params[1], params[2])
 
 
 class WDGridProvisioning(Local):
@@ -57,7 +59,7 @@ class WDGridProvisioning(Local):
 
         if vncs:
             self._vncs_pool = multiprocessing.Pool(len(vncs), maxtasksperchild=1)
-            self._vncs_pool.map_async(vncviewer.main, vncs)
+            self._vncs_pool.map_async(start_vnc, vncs)
         super(WDGridProvisioning, self).startup()
 
     def shutdown(self):
@@ -222,3 +224,95 @@ class WDGridProvisioning(Local):
         if not isinstance(label, text_type):
             label = str(id(label))
         return label
+
+
+class VNCViewer(object):
+    def __init__(self, label):
+        pygame.display.set_caption(label)
+        self.alive = True
+        self.loopcounter = 0
+        self.sprites = pygame.sprite.RenderUpdates()
+        self.buttons = 0
+        self.protocol = None
+
+    def setProtocol(self, protocol):
+        """attach a protocol instance to post the events to"""
+        self.protocol = protocol
+
+    def mainloop(self):
+        if self.alive:
+            reactor.callLater(0.050, self.mainloop)
+
+
+class RFBToGUI(rfb.RFBClient, object):
+    """RFBClient protocol that talks to the GUI app"""
+
+    def _handleDecodeZRLE(self, block):
+        raise NotImplementedError()
+
+    def vncConnectionMade(self):
+        """choose appropriate color depth, resize screen"""
+        self.remoteframebuffer = self.factory.remoteframebuffer
+        self.screen = pygame.display.set_mode((self.width, self.height))
+        icon = os.path.join(os.path.dirname(os.path.abspath(resources.__file__)), "taurus.png")
+        pygame.display.set_icon(pygame.image.load(icon))
+        self.remoteframebuffer.setProtocol(self)
+        self.setEncodings([
+            rfb.COPY_RECTANGLE_ENCODING,
+            rfb.HEXTILE_ENCODING,
+            rfb.CORRE_ENCODING,
+            rfb.RRE_ENCODING,
+            rfb.RAW_ENCODING,
+        ])
+        self.setPixelFormat()
+        self.framebufferUpdateRequest()
+
+    def vncRequestPassword(self):
+        if self.factory.password is not None:
+            self.sendPassword(self.factory.password)
+        else:
+            raise ValueError("Password required")
+
+    def commitUpdate(self, rectangles=None):
+        pygame.display.update(rectangles)
+        self.framebufferUpdateRequest(incremental=1)
+
+    def updateRectangle(self, x, y, width, height, data):
+        self.screen.blit(
+            pygame.image.fromstring(data, (width, height), 'RGBX'),  # TODO color format
+            (x, y)
+        )
+
+
+class VNCFactory(rfb.RFBFactory, object):
+    """A factory for remote frame buffer connections."""
+
+    def __init__(self, remoteframebuffer, *args, **kwargs):
+        rfb.RFBFactory.__init__(self, *args, **kwargs)
+        self.remoteframebuffer = remoteframebuffer
+        self.protocol = RFBToGUI
+
+    def buildProtocol(self, addr):
+        return rfb.RFBFactory.buildProtocol(self, addr)
+
+    def clientConnectionLost(self, connector, reason):
+        self.remoteframebuffer.alive = False
+        logging.debug("connection lost: %r" % reason.getErrorMessage())
+
+    def clientConnectionFailed(self, connector, reason):
+        self.remoteframebuffer.alive = False
+        logging.warning("cannot connect to server: %r\n" % reason.getErrorMessage())
+
+
+def start_vnc(params):
+    host, password, label, display = params
+    pygame.init()
+    remoteframebuffer = VNCViewer(label)
+
+    reactor.connectTCP(host, display + 5900, VNCFactory(remoteframebuffer, password, ))
+
+    # run the application
+    reactor.callLater(0.1, remoteframebuffer.mainloop)
+    reactor.run()
+    pygame.quit()
+    logging.debug("Done: %s", id(reactor))
