aee35fb9db0882370584ea460b736556cd789ef9
Dmitri Pribysh <dmand@yandex.ru>
2018-06-18 16:38:40 +0300

    Small improvements

--- bzt/jmx/http.py
+++ bzt/jmx/http.py
@@ -54,6 +54,9 @@ class HTTPProtocolHandler(ProtocolHandler):
         return elements
 
     def get_sampler_pair(self, scenario, request):
+        if not any(request.startswith(prefix) for prefix in ['http', 'https', '/']):
+            return None, None
+
         timeout = self.safe_time(request.priority_option('timeout'))
 
         content_type = self._get_merged_ci_headers(scenario, request, 'content-type')
