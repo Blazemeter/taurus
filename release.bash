6a1ae66ae5f5f9413a6466dd457df9165855610e
Andrey Pohilko <apc4@ya.ru>
2018-07-04 16:44:53 +0300

    Remove all Java stuff

--- release.bash
+++ release.bash
@@ -1,17 +1,5 @@
 #! /bin/bash -xe
 
-echo "Building JUnit plugin"
-pushd $(dirname $0)/java/JUnit_plugin
-mvn clean package
-cp target/*.jar $(dirname $0)/../../bzt/resources/
-popd
-
-echo "Building TestNG plugin"
-pushd $(dirname $0)/java/TestNG_plugin
-mvn clean package
-cp target/*.jar $(dirname $0)/../../bzt/resources/
-popd
-
 $(dirname $0)/build-sdist.sh
 
 python $(dirname $0)/setup.py sdist bdist_wheel upload
