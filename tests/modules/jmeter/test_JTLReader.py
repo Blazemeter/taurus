15108636eff6c87278bdd11cd1c5484fa5c04a9d
Andrey Pohilko <apc4@ya.ru>
2018-08-14 16:59:11 +0300

    Fix test

--- tests/modules/jmeter/test_JTLReader.py
+++ tests/modules/jmeter/test_JTLReader.py
@@ -228,7 +228,7 @@ class TestJTLReader(BZTestCase):
         exp = [2214.4798867972772, 720.7704268609725, 606.834452578833, 828.4089170237546, 585.8142211763571,
                622.922628329711, 552.9488620851849, 693.3748292117727, 487.61621818581966, 424.71180222446503,
                251.2251128133865]
-        self.assertEqual(exp, res)
+        self.assertEqual(exp, [x[DataPoint.CURRENT][''][KPISet.STDEV_RESP_TIME] for x in json.loads(txt)])
 
     def test_kpiset_trapped_getitem(self):
         def new():
