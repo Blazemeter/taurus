a25cb5eda9b7b8320236285e60d3a48bb2be480b
Dmitri Pribysh <dmand@yandex.ru>
2018-09-24 15:55:13 +0300

    Add stubs

---
+++ tests/resources/newman/newman.bat
@@ -0,0 +1,7 @@
+@echo off
+echo %*
+
+set REPORT_FILE=%TAURUS_ARTIFACTS_DIR%\NewmanExecutor.ldjson 
+
+echo '{"test_case":"should load","test_suite":"demo","status":"PASSED","start_time":1537789085.169,"duration":0.002,"error_msg":null,"error_trace":null}' > %REPORT_FILE%
+
---
+++ tests/resources/selenium/js-mocha/mocha.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -xe
+
+# dump cmdline
+echo "$@"
+
+REPORT_FILE="$TAURUS_ARTIFACTS_DIR/MochaTester.ldjson"
+
+echo '{"test_case":"should load","test_suite":"demo","status":"PASSED","start_time":1537789085.169,"duration":0.002,"error_msg":null,"error_trace":null}' > "$REPORT_FILE"
