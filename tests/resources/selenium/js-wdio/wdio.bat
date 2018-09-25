509cdacb17cb886666f40fa664b1367da695549c
Dmitri Pribysh <dmand@yandex.ru>
2018-09-25 13:23:52 +0300

    Fix other bat files

--- tests/resources/selenium/js-wdio/wdio.bat
+++ tests/resources/selenium/js-wdio/wdio.bat
@@ -3,5 +3,5 @@ echo %*
 
 set REPORT_FILE=%TAURUS_ARTIFACTS_DIR%\WebdriverIOExecutor.ldjson 
 
-echo '{"test_case":"should load","test_suite":"demo","status":"PASSED","start_time":1537789085.169,"duration":0.002,"error_msg":null,"error_trace":null}' > %REPORT_FILE%
+echo {"test_case":"should load","test_suite":"demo","status":"PASSED","start_time":1537789085.169,"duration":0.002,"error_msg":null,"error_trace":null} > %REPORT_FILE%
 
