88666a439ac5422169f07fa45ff9b65fc5028f67
Dmitri Pribysh <dmand@yandex.ru>
2018-09-25 15:24:47 +0300

    Use mocked tool in ruby tests

--- tests/modules/selenium/test_ruby.py
+++ tests/modules/selenium/test_ruby.py
@@ -16,61 +16,51 @@ class TestSeleniumRSpecRunner(SeleniumTestCase):
                 }}})
         self.obj.prepare()
 
-    def test_rspec_full(self):
-        self.configure({
-            'execution': {
-                'scenario': {'script': RESOURCES_DIR + 'selenium/ruby/example_spec.rb'},
-            },
-        })
-        self.obj.settings.merge(self.obj.engine.config.get("modules").get("selenium"))
+    def full_run(self, config):
+        self.configure(config)
+        dummy = RESOURCES_DIR + 'selenium/ruby/ruby' + ('.bat' if is_windows() else '.sh')
         self.obj.prepare()
+        self.obj.runner.settings.merge({"interpreter": dummy})
         self.obj.startup()
         while not self.obj.check():
             time.sleep(self.obj.engine.check_interval)
         self.obj.shutdown()
+
+    def test_rspec_full(self):
+        script = RESOURCES_DIR + 'selenium/ruby/example_spec.rb'
+        self.full_run({
+            'execution': {
+                'scenario': {'script': script},
+            },
+        })
         self.assertTrue(os.path.exists(self.obj.runner.report_file))
-        lines = open(self.obj.runner.report_file).readlines()
-        self.assertEqual(len(lines), 3)
-        first, second, third = lines
-        self.assertEqual(json.loads(first)["status"], "PASSED")
-        self.assertEqual(json.loads(second)["status"], "FAILED")
-        self.assertEqual(json.loads(third)["status"], "FAILED")
+        with open(self.obj.runner.stdout_file) as fds:
+            stdout = fds.read()
+        self.assertIn('--test-suite ' + script, stdout)
 
     def test_rspec_hold(self):
-        self.configure({
+        self.full_run({
             'execution': {
                 'hold-for': '10s',
                 'scenario': {'script': RESOURCES_DIR + 'selenium/ruby/example_spec.rb'},
                 'executor': 'selenium'
             },
         })
-        self.obj.settings.merge(self.obj.engine.config.get("modules").get("selenium"))
-        self.obj.prepare()
-        self.obj.startup()
-        while not self.obj.check():
-            time.sleep(self.obj.engine.check_interval)
-        self.obj.shutdown()
-        self.assertTrue(os.path.exists(self.obj.runner.report_file))
-        duration = time.time() - self.obj.start_time
-        self.assertGreater(duration, 10)
+        with open(self.obj.runner.stdout_file) as fds:
+            stdout = fds.read()
+        self.assertIn('--hold-for 10', stdout)
 
     def test_rspec_iterations(self):
-        self.configure({
+        self.full_run({
             'execution': {
                 'iterations': 3,
                 'scenario': {'script': RESOURCES_DIR + 'selenium/ruby/example_spec.rb'},
                 'executor': 'selenium'
             },
         })
-        self.obj.settings.merge(self.obj.engine.config.get("modules").get("selenium"))
-        self.obj.prepare()
-        self.obj.startup()
-        while not self.obj.check():
-            time.sleep(self.obj.engine.check_interval)
-        self.obj.shutdown()
-        self.assertTrue(os.path.exists(self.obj.runner.report_file))
-        lines = open(self.obj.runner.report_file).readlines()
-        self.assertEqual(len(lines), 9)
+        with open(self.obj.runner.stdout_file) as fds:
+            stdout = fds.read()
+        self.assertIn('--iterations 3', stdout)
 
     def test_interpreter(self):
         self.configure({
@@ -81,9 +71,6 @@ class TestSeleniumRSpecRunner(SeleniumTestCase):
             },
         })
         self.obj.settings.merge(self.obj.engine.config.get("modules").get("selenium"))
-        dummy = RESOURCES_DIR + 'selenium/ruby/ruby-dummy'
-        dummy += '.bat' if is_windows() else ''
-        self.obj.settings.merge({"interpreter": dummy})
         self.obj.prepare()
 
     def test_rspec_report_extras(self):
