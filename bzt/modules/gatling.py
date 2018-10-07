ab783f12352ffe31090a8821a21726a93398732e
Andrey Pohilko <apc4@ya.ru>
2018-10-07 11:33:06 +0300

    Implement it

--- bzt/modules/gatling.py
+++ bzt/modules/gatling.py
@@ -20,6 +20,7 @@ import os
 import re
 import subprocess
 import time
+from collections import defaultdict
 
 from bzt import TaurusConfigError, ToolError
 from bzt.engine import ScenarioExecutor, Scenario, FileLister, HavingInstallableTools, SelfDiagnosable
@@ -215,6 +216,7 @@ class GatlingExecutor(ScenarioExecutor, WidgetProvider, FileLister, HavingInstal
     """
     Gatling executor module
     """
+
     def __init__(self):
         super(GatlingExecutor, self).__init__()
         self.script = None
@@ -535,6 +537,7 @@ class DataLogReader(ResultsReader):
         self.delimiter = "\t"
         self.dir_prefix = dir_prefix
         self.guessed_gatling_version = None
+        self._group_errors = defaultdict(set)
 
     def _extract_log_gatling_21(self, fields):
         """
@@ -600,30 +603,56 @@ class DataLogReader(ResultsReader):
             elif fields[3].strip() == "END":
                 self.concurrency -= 1
 
-        if fields[0].strip() != "REQUEST":
+        if fields[0].strip() == "GROUP":
+            return self.__parse_group(fields)
+        elif fields[0].strip() == "REQUEST":
+            return self.__parse_request(fields)
+        else:
             return None
 
-        # see LogFileDataWriter.ResponseMessageSerializer in gatling-core
-        req_name = fields[4]
-        req_hierarchy = fields[3]
-
-        label = req_hierarchy if req_hierarchy else req_name
-        t_stamp = int(fields[6]) / 1000.0
-
-        r_time = (int(fields[6]) - int(fields[5])) / 1000.0
+    def __parse_group(self, fields):
+        label = fields[3]
+        t_stamp = int(fields[5]) / 1000.0
+        r_time = int(fields[6]) / 1000.0
         latency = 0.0
         con_time = 0.0
-
         if fields[7] == 'OK':
             r_code = '200'
         else:
             _tmp_rc = fields[-1].split(" ")[-1]
             r_code = _tmp_rc if _tmp_rc.isdigit() else 'No RC'
 
+        if label in self._group_errors:
+            error = ';'.join(self._group_errors.pop(label))
+        else:
+            error = None
+        return int(t_stamp), label, r_time, con_time, latency, r_code, error
+
+    def __parse_request(self, fields):
+        # see LogFileDataWriter.ResponseMessageSerializer in gatling-core
+
         if len(fields) >= 9 and fields[8]:
             error = fields[8]
         else:
             error = None
+
+        req_hierarchy = fields[3]
+        if req_hierarchy:
+            if error:
+                self._group_errors[req_hierarchy].add(error)
+            return None
+
+        label = fields[4]
+        t_stamp = int(fields[6]) / 1000.0
+        r_time = (int(fields[6]) - int(fields[5])) / 1000.0
+        latency = 0.0
+        con_time = 0.0
+        if fields[7] == 'OK':
+            r_code = '200'
+        else:
+            _tmp_rc = fields[-1].split(" ")[-1]
+            r_code = _tmp_rc if _tmp_rc.isdigit() else 'No RC'
+
         return int(t_stamp), label, r_time, con_time, latency, r_code, error
 
     def _guess_gatling_version(self, fields):
