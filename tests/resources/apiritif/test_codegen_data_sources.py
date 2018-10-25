54b126034e46c7788b2053d4b44d00bf45b5f1bf
Dmitri Pribysh <dmand@yandex.ru>
2018-10-25 17:36:22 +0300

    Attempt to fix some tests

--- tests/resources/apiritif/test_codegen_data_sources.py
+++ tests/resources/apiritif/test_codegen_data_sources.py
@@ -8,9 +8,10 @@ import unittest
 
 import apiritif
 
-vars = {}
+vars = {
+
+}
 urls_feeder = apiritif.feeders.CSVFeeder('urls.csv', vars)
-urls_feeder.open()
 
 
 class TestAPI(unittest.TestCase):
@@ -18,4 +19,3 @@ class TestAPI(unittest.TestCase):
     def test_1_httplocalhost8000url(self):
         with apiritif.transaction('http://localhost:8000/{}'.format(vars['url'])):
             response = apiritif.http.get('http://localhost:8000/{}'.format(vars['url']))
-        urls_feeder.next()
