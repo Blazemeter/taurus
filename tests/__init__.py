53a4cd15602da5e0e8f83cfa47ea2793ef784b0e
greyfenrir <grey.fenrir@gmail.com>
2018-11-09 22:41:55 +0300

    fix imports

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,3 +2,4 @@
 from tests.base import TEST_DIR, BUILD_DIR, RESOURCES_DIR, BASE_CONFIG, ROOT_LOGGER
 from tests.base import close_reader_file, local_paths_config
 from tests.cases import BZTestCase, ExecutorTestCase
+from tests.mocks import random_datapoint
