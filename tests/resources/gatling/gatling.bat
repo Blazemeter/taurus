415701bb3bdf111f50b2e2ef689ea43e210405df
greyfenrir <grey.fenrir@gmail.com>
2018-11-18 09:36:45 +0300

    fix win tests

--- tests/resources/gatling/gatling.bat
+++ tests/resources/gatling/gatling.bat
@@ -1,11 +1,11 @@
-echo off
+@echo off
 
 set COMPILATION_CLASSPATH=""
 
 if DEFINED GATLING_HOME (
-  set GATLING_DIR=%GATLING_HOME%/gatling
-else (
+  set GATLING_PATH=%GATLING_HOME%/gatling
+) else (
   set GATLING_PATH=%~dp0
 )
 
-python -u %GATLING_PATH%/galing-fake.py %*
+python -u %GATLING_PATH%/gatling-fake.py %*
