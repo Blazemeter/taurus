630fb0b4f71138566a8b02bb3f5a2c86c4eccbcf
greyfenrir <grey.fenrir@gmail.com>
2018-11-30 21:41:04 +0300

    fix import

--- bzt/modules/shellexec.py
+++ bzt/modules/shellexec.py
@@ -13,7 +13,6 @@ WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 See the License for the specific language governing permissions and
 limitations under the License.
 """
-import tempfile
 from subprocess import CalledProcessError, PIPE
 
 from bzt import TaurusConfigError
