65b580301c626a3af295eacfca4cf6dec3d8b396
greyfenrir <grey.fenrir@gmail.com>
2018-12-10 21:14:39 +0300

    unify test classes
    SeleniumTestCase should be based on ExecutorTestCase

--- tests/modules/selenium/__init__.py
+++ tests/modules/selenium/__init__.py
@@ -1,20 +1,18 @@
-from tests import BZTestCase, local_paths_config
+from tests import local_paths_config, ExecutorTestCase
 from bzt.modules.selenium import SeleniumExecutor
-from tests.mocks import EngineEmul
 from bzt.modules.services import VirtualDisplay
 
 
-class SeleniumTestCase(BZTestCase):
-    """
-    :type obj: SubprocessedExecutor
-    """
+class SeleniumTestCase(ExecutorTestCase):
+    EXECUTOR = SeleniumExecutor
+
     def __init__(self, methodName='runTest'):
         super(SeleniumTestCase, self).__init__(methodName)
         self.obj = None
 
     def setUp(self):
         super(SeleniumTestCase, self).setUp()
-        self.engine = EngineEmul()
+
         paths = [local_paths_config()]
         self.engine.configure(paths)  # FIXME: avoid using whole engine in particular module test!
 
@@ -22,17 +20,11 @@ class SeleniumTestCase(BZTestCase):
         self.virtual_display.engine = self.engine
         self.virtual_display.startup()
 
-        self.obj = SeleniumExecutor()
-        self.obj.engine = self.engine
         self.obj.settings = self.engine.config.get("modules").get("selenium")
 
     def tearDown(self):
         self.virtual_display.shutdown()
         super(SeleniumTestCase, self).tearDown()
 
-    def configure(self, config):
-        self.obj.engine.config.merge(config)
-        self.obj.execution = self.obj.engine.config.get('execution')
-        if isinstance(self.obj.execution, list):
-            self.obj.execution = self.obj.execution[0]
+
 
