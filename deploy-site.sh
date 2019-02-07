89e66ead65b53ce80ebde643ade456d472e5a428
Artem Fedorov <artem.fedorov@blazemeter.com>
2019-02-07 19:28:05 +0300

    create kuber from jenkins

--- deploy-site.sh
+++ deploy-site.sh
@@ -7,4 +7,6 @@ gcloud auth configure-docker
 docker tag  ${IMAGE_TAG} gcr.io/${PROJECT_ID}/${IMAGE_TAG}
 docker push gcr.io/${PROJECT_ID}/${IMAGE_TAG}
 gcloud container clusters get-credentials taurus-site
-kubectl set image deployment/taurus-site taurus-site=gcr.io/${PROJECT_ID}/${IMAGE_TAG}
+#kubectl set image deployment/taurus-site taurus-site=gcr.io/${PROJECT_ID}/${IMAGE_TAG}
+kubectl run taurus-site --image=gcr.io/${PROJECT_ID}/${IMAGE_TAG} --port 80
+kubectl expose deployment taurus-site --type=LoadBalancer --port 80 --target-port 80
