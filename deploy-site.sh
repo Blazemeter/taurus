c8cfee9c314e99038405cbe5988eb6b2a68b0fcf
Artem Fedorov <artem.fedorov@blazemeter.com>
2019-02-07 18:19:07 +0300

    add deploy site steps

--- deploy-site.sh
+++ deploy-site.sh
@@ -1,1 +1,10 @@
 #!/bin/bash -xe
+
+docker build -t ${IMAGE_TAG} .
+
+gcloud auth configure-docker
+
+docker tag  ${IMAGE_TAG} gcr.io/${PROJECT_ID}/${IMAGE_TAG}
+docker push gcr.io/${PROJECT_ID}/${IMAGE_TAG}
+gcloud container clusters get-credentials taurus-site
+kubectl set image deployment/taurus-site taurus-site=gcr.io/${PROJECT_ID}/${IMAGE_TAG}
