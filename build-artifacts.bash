6c759c3158d0b849aa3f48d5d3f5bf54efcb38f0
Artem Fedorov <artem.fedorov@blazemeter.com>
2019-02-07 21:06:15 +0300

    install docker

--- build-artifacts.bash
+++ build-artifacts.bash
@@ -5,9 +5,13 @@ PROJECT_ID="blazemeter-taurus-website-prod"
 CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
 echo "deb http://packages.cloud.google.com/apt ${CLOUD_SDK_REPO} main" | tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -
+add-apt-repository \
+   "deb [arch=amd64] https://download.docker.com/linux/ubuntu \
+   $(lsb_release -cs) \
+   stable"
 
 apt-get update -y
-apt-get install -y --force-yes gcc-mingw-w64-x86-64 nsis composer zip google-cloud-sdk kubectl
+apt-get install -y --force-yes gcc-mingw-w64-x86-64 nsis composer zip google-cloud-sdk kubectl docker-ce docker-ce-cli containerd.io
 apt-get install -y libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev libtk8.5 libgdm-dev libdb4o-cil-dev libpcap-dev
 
 gcloud auth activate-service-account --key-file ${KEY_FILE}
