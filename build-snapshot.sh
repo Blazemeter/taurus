47596a74fc2273a14667e596e74b7adb8b238726
Artem Fedorov <artem.fedorov@blazemeter.com>
2019-02-07 16:12:28 +0300

    add build snapshots

--- build-snapshot.sh
+++ build-snapshot.sh
@@ -1,1 +1,20 @@
 #!/bin/bash -xe
+
+# copy snapshot to storage
+gsutil cp dist/bzt-*.whl gs://taurus-site/snapshots/
+
+# cleanup site dir
+rm -r site
+mkdir site
+cd site
+
+# clone base site
+gsutil cp gs://taurus-site/site.zip site.zip
+unzip site.zip -d .
+rm site.zip
+
+# add snapshots
+mkdir snapshots
+gsutils cp gs://taurus-site/snapshots/ snapshots
+gsutils cp gs://taurus-site/snapshots/blazemeter-pbench-extras_0.1.10.1_amd64.deb snapshots
+
