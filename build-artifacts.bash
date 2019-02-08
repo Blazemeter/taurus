37822bd764c1e3bacf31e83664ea0932fac92478
Artem Fedorov <artem.fedorov@blazemeter.com>
2019-02-08 17:56:59 +0300

    cleanup artifact build

--- build-artifacts.bash
+++ build-artifacts.bash
@@ -2,12 +2,8 @@
 BUILD_NUMBER=$1
 
 apt-get update -y
-apt-get install -y --force-yes gcc-mingw-w64-x86-64 nsis composer zip
+apt-get install -y --force-yes gcc-mingw-w64-x86-64 nsis
 apt-get install -y libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev libtk8.5 libgdm-dev libdb4o-cil-dev libpcap-dev
-
-#gcloud auth activate-service-account --key-file ${KEY_FILE}
-#gcloud config set project ${PROJECT_ID}
-#gcloud config set compute/zone us-central1-a
 
 # build source distribution
 ./build-sdist.sh
@@ -15,12 +11,3 @@ apt-get install -y libssl-dev libncurses5-dev libsqlite3-dev libreadline-dev lib
 # build a windows installer
 pip3 install virtualenv
 ./build-windows-installer.sh ./dist/bzt-*.whl
-
-
-#PROJECT_ID="blazemeter-taurus-website-prod"
-#if [ "$1" = "true" ]; then
-#./build-base-site.sh
-#fi
-
-#./build-snapshot-site.sh
-#./deploy-site.sh
