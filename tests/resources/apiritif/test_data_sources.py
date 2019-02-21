b1a02d42b6ecb7c928fb176355c54b9709778bcf
greyfenrir <grey.fenrir@gmail.com>
2019-02-21 13:30:21 +0300

    prototype

--- tests/resources/apiritif/test_data_sources.py
+++ tests/resources/apiritif/test_data_sources.py
@@ -8,38 +8,42 @@ import unittest
 
 import apiritif
 
-target = apiritif.http.target('http://localhost:8000/')
-target.keep_alive(True)
-target.auto_assert_ok(True)
-target.use_cookies(True)
-target.allow_redirects(True)
-
 reader_1 = apiritif.csv.CSVReaderPerThread('first-file.csv')
 reader_2 = apiritif.csv.CSVReaderPerThread('/second/file.csv', loop=True, quoted=False, delimiter='-')
 
 
 def setup():
+    # variable data must be saved into thread context due to multi threading load model
+    target = apiritif.http.target('http://localhost:8000/')
+    target.keep_alive(True)
+    target.auto_assert_ok(True)
+    target.use_cookies(True)
+    target.allow_redirects(True)
+
+    vars = {
+        'cn': 'cv',
+    }
     reader_1.read_vars()
     reader_2.read_vars()
+    vars.update(reader_1.get_vars())
+    vars.update(reader_2.get_vars())
+
+    apiritif.put_into_thread_store(vars, target)
 
 
 class TestAPI(unittest.TestCase):
 
     def setUp(self):
-        self.vars = {
-            'cn': 'cv',
-        }
-        self.vars.update(reader_1.get_vars())
-        self.vars.update(reader_2.get_vars())
+        self.vars, self.target = apiritif.get_from_thread_store()
 
     def test_1_an(self):
         with apiritif.transaction(self.vars['an']):
-            response = target.get(self.vars['an'])
+            response = self.target.get(self.vars['an'])
 
     def test_2_bn(self):
         with apiritif.transaction(self.vars['bn']):
-            response = target.get(self.vars['bn'])
+            response = self.target.get(self.vars['bn'])
 
     def test_3_cn(self):
         with apiritif.transaction(self.vars['cn']):
-            response = target.get(self.vars['cn'])
+            response = self.target.get(self.vars['cn'])
