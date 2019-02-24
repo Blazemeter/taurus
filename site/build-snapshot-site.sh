7286f50bd3a2931da672af6417c24e3a218badbd
Andrey Pohilko <apc4@ya.ru>
2019-02-24 14:28:34 +0300

    Try again

--- site/build-snapshot-site.sh
+++ site/build-snapshot-site.sh
@@ -6,15 +6,16 @@ gsutil cp -s regional build/nsis/*.exe gs://taurus-site/snapshots/
 
 # cleanup site dir
 mkdir site.bak
-cp -v site/* site.bak
+cp -v site/*.sh site.bak
+cp -v site/Dockerfile* site.bak
 
 rm -r site
-mv site.bak site
 
 # clone base site
 gsutil cp gs://taurus-site/site.zip site.zip
 unzip site.zip -d .
 rm site.zip
+mv site.bak/* site/
 
 # static learning course
 gsutil cp gs://taurus-site/learn.zip learn.zip
