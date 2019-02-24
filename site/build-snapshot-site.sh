628b15f4c977cafd8ec5eba8978c1e3be9fe3d61
Andrey Pohilko <apc4@ya.ru>
2019-02-24 17:15:07 +0300

    Better unzip

--- site/build-snapshot-site.sh
+++ site/build-snapshot-site.sh
@@ -7,18 +7,19 @@ gsutil cp -s regional build/nsis/*.exe gs://taurus-site/snapshots/
 # cleanup site dir
 rm -r site
 mkdir site
-cp -vr /bzt/scripts/Dockerfile* site/
 cd site
 
 
 # clone base site
 gsutil cp gs://taurus-site/site.zip site.zip
-unzip site.zip -d .
+unzip -o site.zip -d .
 rm site.zip
+
+cp -vr /bzt/scripts/Dockerfile* site/
 
 # static learning course
 gsutil cp gs://taurus-site/learn.zip learn.zip
-unzip learn.zip -d .
+unzip -o learn.zip -d .
 rm learn.zip
 
 # add snapshots
