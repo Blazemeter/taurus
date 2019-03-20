6fbcd414028ae63118dbb1f0374e176479e7b964
greyfenrir <grey.fenrir@gmail.com>
2019-03-20 15:05:37 +0300

    Feat/apiritif/set variables (#1072)
    * add test and variables setup template
    
    * fix test
    
    * improve test
    
    * tmp
    
    * add set-value generator
    
    * add documentation
    
    * fix firefox version
    
    * read user's label of 'set-variables'
    
    * exclude labels of service methods, add test
    
    * fix test

--- tests/modules/selenium/test_python.py
+++ tests/modules/selenium/test_python.py
@@ -1277,6 +1277,27 @@ class TestApiritifScriptGenerator(ExecutorTestCase):
         exp_file = RESOURCES_DIR + "/apiritif/test_data_sources.py"
         self.assertFilesEqual(exp_file, self.obj.script)
 
+    def test_vars(self):
+        self.configure({
+            "execution": [{
+                "test-mode": "apiritif",
+                "scenario": {
+                    "variables": {"an": "av"},
+                    "default-address": "http://localhost:8000/",
+                    "requests": [
+                        "${an}",
+                        {"set-variables": {
+                            "an": "another_path1",
+                            "bn": "another_path2"}},
+                        "${an}"],
+                }}]})
+
+        self.obj.engine.aggregator = ConsolidatingAggregator()
+        self.obj.prepare()
+        exp_file = RESOURCES_DIR + "/apiritif/test_vars.py"
+        self.assertIn("test_2_set_variables", self.obj.engine.aggregator.ignored_labels)
+        self.assertFilesEqual(exp_file, self.obj.script)
+
     def test_codegen_requests(self):
         self.configure({
             "execution": [{
