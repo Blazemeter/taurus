5692c0455971a1d1dadc3a2a75df82cd4e33ec4a
greyfenrir <grey.fenrir@gmail.com>
2019-03-21 13:32:01 +0300

    fix setting of environment of required tool

--- bzt/utils.py
+++ bzt/utils.py
@@ -1207,7 +1207,9 @@ class RequiredTool(object):
         return
 
     def call(self, *args, **kwargs):
-        kwargs["env"] = self.env.get().update(kwargs.get("env", {}))
+        mixed_env = self.env.get()
+        mixed_env.update(kwargs.get("env", {}))
+        kwargs["env"] = mixed_env
         return exec_and_communicate(*args, **kwargs)
 
     def check_if_installed(self):
