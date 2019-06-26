044ea76b27ead22527b73f100abd831b684cfa16
greyfenrir <grey.fenrir@gmail.com>
2019-06-26 13:29:07 +0300

    fix linux events

--- tests/modules/test_GUIScreen.py
+++ tests/modules/test_GUIScreen.py
@@ -51,18 +51,12 @@ class TestGUIScreen(BZTestCase):
 
         if hasattr(obj, 'font'):
             old_font_size = obj.font['size']
-            if is_linux():
-                obj.root.event_generate("<Control-4>")
-            else:
-                obj.root.event_generate("<Control-MouseWheel>", delta=120)
+            obj.root.event_generate("<Control-MouseWheel>", delta=120)
             if old_font_size > 0:
                 self.assertGreater(obj.font['size'], old_font_size)
             else:
                 self.assertLess(obj.font['size'], old_font_size)
-            if is_linux():
-                obj.root.event_generate("<Control-5>")
-            else:
-                obj.root.event_generate("<Control-MouseWheel>", delta=-120)
+            obj.root.event_generate("<Control-MouseWheel>", delta=-120)
 
             self.assertEqual(obj.font['size'], old_font_size)
 
