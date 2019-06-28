cbb1d6b0f826800a5b033df061181fdd842d957d
greyfenrir <grey.fenrir@gmail.com>
2019-06-28 15:38:04 +0300

    fix deploy

--- site/deploy-site.sh
+++ site/deploy-site.sh
@@ -14,13 +14,14 @@ else
     SNAPSHOT_HEADER="## Latest Unstable Snapshots"
     WHL_SNAPSHOT="Python wheel package: [bzt-${TAURUS_VERSION}.${BUILD_NUMBER}-py2.py3-none-any.whl](${GOOGLE_STORAGE}snapshots\/bzt-${TAURUS_VERSION}.${BUILD_NUMBER}-py2.py3-none-any.whl)"
     EXE_SNAPSHOT="Windows installer: [TaurusInstaller_${TAURUS_VERSION}.${BUILD_NUMBER}_x64.exe](${GOOGLE_STORAGE}snapshots\/TaurusInstaller_${TAURUS_VERSION}.${BUILD_NUMBER}_x64.exe)"
-    UNSTABLE_SNAPSHOT=${SNAPSHOT_HEADER}/\n/\n${WHL_SNAPSHOT}/\n/\n${EXE_SNAPSHOT}
+    UNSTABLE_SNAPSHOT=${SNAPSHOT_HEADER}\\n\\n${WHL_SNAPSHOT}\\n\\n${EXE_SNAPSHOT}
 
     gsutil cp -s regional dist/*.whl gs://taurus-site/snapshots/
     gsutil cp -s regional build/nsis/*.exe gs://taurus-site/snapshots/
 fi
 
-sed -ri "s/RELEASE_SNAPSHOT/${GOOGLE_STORAGE}releases\/TaurusInstaller_${TAURUS_VERSION}_x64.exe/" site/dat/docs/Installation.md
+RELEASE_SNAPSHOT="${GOOGLE_STORAGE}releases\/TaurusInstaller_${TAURUS_VERSION}_x64.exe"
+sed -ri "s/RELEASE_SNAPSHOT/${RELEASE_SNAPSHOT}/" site/dat/docs/Installation.md
 sed -ri "s/UNSTABLE_SNAPSHOT/${UNSTABLE_SNAPSHOT}/" site/dat/docs/Installation.md
 
 python site/Taurus/kwindexer.py site/dat/docs site/dat/docs/KeywordIndex.md
