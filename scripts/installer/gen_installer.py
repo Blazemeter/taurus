1c83df9619943f88f0c881c128bd839315e33216
greyfenrir <grey.fenrir@gmail.com>
2019-10-25 16:05:00 +0300

    refactoring

--- scripts/installer/gen_installer.py
+++ scripts/installer/gen_installer.py
@@ -9,6 +9,8 @@ import configparser
 import requests
 
 
+STORAGE_URL = "https://storage.googleapis.com/taurus-site/extras/"
+
 def run_cmd(label, cmdline, **kwargs):
     print(label + " " + str(cmdline))
     subprocess.run(cmdline, check=True, **kwargs)
@@ -120,14 +122,14 @@ def download_tkinter(archive_url, archive_filename):
         z.extractall()
 
 
-def add_extra_wheels(wheel_dir, links):
-    for link in links:
+def add_extra_wheels(wheel_dir, pkgs):
+    for pkg in pkgs:
+        link = STORAGE_URL + pkg
         print("Downloading pre-built wheel: %s" % link)
         r = requests.get(link, headers={'User-Agent': 'Automation'}, stream=True)
         r.raise_for_status()
 
-        file_name = link.split('/')[-1]
-        full_filename = os.path.join(wheel_dir, file_name)
+        full_filename = os.path.join(wheel_dir, pkg)
         with open(full_filename, 'wb') as f:
             for chunk in r.iter_content(chunk_size=1024):
                 if chunk:  # filter out keep-alive new chunks
@@ -140,8 +142,7 @@ def main():
         sys.exit(1)
     bzt_dist = sys.argv[1]
 
-    # todo: add to our storage
-    tkinter_link = "https://github.com/mu-editor/mu_tkinter/releases/download/0.3/pynsist_tkinter_3.6_64bit.zip"
+    tkinter_link = STORAGE_URL + "pynsist_tkinter_3.6_64bit.zip"
 
     pynsist_config = "installer-gen.cfg"
     wheel_dir = "build/wheels"
@@ -150,12 +151,11 @@ def main():
     tkinter_archive = tempfile.NamedTemporaryFile(prefix="tkinter-libs", suffix=".zip")
     download_tkinter(tkinter_link, tkinter_archive.name)
     fetch_all_wheels(bzt_dist, wheel_dir)
-    extra_links = [
-        "https://storage.googleapis.com/taurus-site/extras/fuzzyset-0.0.19-cp37-cp37m-win_amd64.whl",
-        "https://storage.googleapis.com/taurus-site/extras/python_Levenshtein-0.12.0-cp37-cp37m-win_amd64.whl"
-        "https://storage.googleapis.com/taurus-site/extras/urwid-2.0.1-cp37-cp37m-win_amd64.whl"
-    ]
-    add_extra_wheels(wheel_dir, extra_links)
+    extra_wheels = [
+        "fuzzyset-0.0.19-cp37-cp37m-win_amd64.whl",
+        "python_Levenshtein-0.12.0-cp37-cp37m-win_amd64.whl",
+        "urwid-2.0.1-cp37-cp37m-win_amd64.whl"]
+    add_extra_wheels(wheel_dir, extra_wheels)
     for pkg in additional_packages:
         fetch_all_wheels(pkg, wheel_dir)
     dependencies = extract_all_dependencies(wheel_dir)
