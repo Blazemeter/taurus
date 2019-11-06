80df542351d3ecab468447d6342e24e1313cd653
Alla Levental <clnlbsmrn@gmail.com>
2019-11-06 11:07:55 +0300

    migrate docker image to py3 (#1185)

--- examples/selenium/rspec_example.rb
+++ examples/selenium/rspec_example.rb
@@ -1,24 +1,24 @@
 require "selenium-webdriver"
 require "rspec"
 
-describe "Google Search" do
+describe "Blazedemo" do
 
   before(:each) do
     @driver = Selenium::WebDriver.for :firefox
-    @base_url = "https://www.google.com/"
+    @base_url = "http://blazedemo.com"
     @accept_next_alert = true
     @driver.manage.timeouts.implicit_wait = 30
   end
  
   after(:each) do
-    @driver.quit   
+    @driver.quit
   end
- 
-  it "search Taurus on google" do
-    @driver.get(@base_url + "/")
-    @driver.find_element(:id, "lst-ib").clear
-    @driver.find_element(:id, "lst-ib").send_keys "BlazeMeter Taurus"
-    @driver.find_element(:id, "lst-ib").send_keys :return
+
+  it "blazedemo.com example" do
+    @driver.get(@base_url + "/purchase.php")
+    @driver.find_element(:id, "inputName").clear
+    @driver.find_element(:id, "inputName").send_keys "First Last"
+    @driver.find_element(:id, "inputName").send_keys :return
     sleep 1.0
   end
 
