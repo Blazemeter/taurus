c1e6d825ce7ab3697e502201c1c16f14df019f26
greyfenrir <grey.fenrir@gmail.com>
2020-01-04 13:33:38 +0300

    start travis

--- scripts/installer/bzt_win.py
+++ scripts/installer/bzt_win.py
@@ -3,5 +3,6 @@ import os, sys
 sys.path.insert(0, 'pkgs')
 os.environ["PATH"] += os.pathsep + os.path.join(os.path.dirname(sys.executable), 'Scripts')
 
+
 def main():
     sys.exit(os.system("cmd /k bzt --help"))
