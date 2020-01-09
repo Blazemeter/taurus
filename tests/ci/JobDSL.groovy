4a973a2d14a1840ea39c9c24aa2b7dd3f64e7549
Aliaksandr <aranich@ya.ru>
2020-01-09 12:33:35 +0300

    Add jenkins configuration for docker image from branch

---
+++ tests/ci/JobDSL.groovy
@@ -0,0 +1,22 @@
+multibranchPipelineJob('TAURUS-IMAGE-BUILDER'){
+    branchSources {
+        git {
+            id('1') // IMPORTANT: use a constant and unique identifier per branch source
+            remote('https://github.com/Blazemeter/taurus.git')
+            credentialsId('github-token')
+            includes('*')
+        }
+    }
+    configure {
+        it / factory(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowBranchProjectFactory') {
+            owner(class: 'org.jenkinsci.plugins.workflow.multibranch.WorkflowMultiBranchProject', reference: '../..')
+            scriptPath("Jenkinsfile-image-builder") //Set a specific scriptPath in MultiBranchPipelineJob DSL
+        }
+    }
+    orphanedItemStrategy {
+        discardOldItems {
+            daysToKeep(1)
+            numToKeep(100)
+        }
+    }
+}
