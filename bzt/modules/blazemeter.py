fcad9267836c386e2454930bc81adcdd653d53b4
greyfenrir <grey.fenrir@gmail.com>
2020-01-10 13:51:32 +0300

    replace func

--- bzt/modules/blazemeter.py
+++ bzt/modules/blazemeter.py
@@ -1017,14 +1017,15 @@ class ProjectFinder(object):
         router.send_report_email = send_report_email
         return router
 
-    def _default_or_create_project(self, proj_name):
+    def _create_project_or_use_default(self, workspace, proj_name):
         if proj_name:
-            return self.workspaces.first().create_project(proj_name)
+            return workspace.create_project(proj_name)
         else:
             info = self.user.fetch()
+            self.log.debug("Looking for default project: %s", info['defaultProject']['id'])
             project = self.workspaces.projects(ident=info['defaultProject']['id']).first()
             if not project:
-                project = self.workspaces.first().create_project("Taurus Tests Project")
+                project = workspace.create_project("Taurus Tests Project")
             return project
 
 
