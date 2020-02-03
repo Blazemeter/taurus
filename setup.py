0bfb7d3b72a07cbe342f13626c6e4adc937a1b41
corvustristis <clnlbsmrn@gmail.com>
2020-02-03 12:22:17 +0300

    Fix docs

--- setup.py
+++ setup.py
@@ -76,8 +76,6 @@ setup(
         'Operating System :: MacOS',
         'Operating System :: POSIX :: Linux',
 
-        'Programming Language :: Python :: 3.5',
-        'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
     ],
     python_requires='>=3.5',
