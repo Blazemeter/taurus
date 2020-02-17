08993164e960699c0c578996d1894676ec9c56ab
corvustristis <clnlbsmrn@gmail.com>
2020-02-17 13:51:56 +0300

    Remove exec from locust

--- tests/modules/test_locustIOExecutor.py
+++ tests/modules/test_locustIOExecutor.py
@@ -23,6 +23,7 @@ from tests import ExecutorTestCase, RESOURCES_DIR, ROOT_LOGGER
 @unittest.skipIf(PY2, "py3.6+ only")
 class TestLocustIOExecutor(ExecutorTestCase):
     EXECUTOR = LocustIOExecutor
+    CMD_LINE = None
 
     @classmethod
     def setUpClass(cls):
@@ -31,6 +32,9 @@ class TestLocustIOExecutor(ExecutorTestCase):
     def setUp(self):
         super(TestLocustIOExecutor, self).setUp()
         self.obj.engine.config['provisioning'] = 'local'
+
+    def start_subprocess(self, args, env, cwd=None, **kwargs):
+        self.CMD_LINE = args
 
     def test_simple(self):
         self.configure({"execution": {
@@ -42,14 +46,13 @@ class TestLocustIOExecutor(ExecutorTestCase):
             }
         }})
         self.obj.prepare()
+        self.obj.engine.start_subprocess = self.start_subprocess
         self.obj.startup()
-        try:
-            while not self.obj.check():
-                time.sleep(self.obj.engine.check_interval)
-        except RuntimeError:  # FIXME: not good, but what to do?
-            pass
-        self.obj.shutdown()
+        self.obj.post_process()
         self.assertFalse(self.obj.has_results())
+        self.assertTrue('-f' in self.CMD_LINE)
+        file_val = self.CMD_LINE[self.CMD_LINE.index('-f')+1]
+        self.assertEqual(file_val, os.path.normpath(RESOURCES_DIR + "locust/simple.py"))
 
     def test_locust_widget(self):
         self.configure({"execution": {
@@ -63,9 +66,9 @@ class TestLocustIOExecutor(ExecutorTestCase):
         }})
 
         self.obj.prepare()
+        self.obj.engine.start_subprocess = lambda **kwargs: None
         self.obj.startup()
         self.obj.get_widget()
-        self.obj.check()
         self.assertEqual(self.obj.widget.duration, 30)
         self.assertTrue(self.obj.widget.widgets[0].text.endswith("simple.py"))
         self.obj.shutdown()
@@ -109,14 +112,9 @@ class TestLocustIOExecutor(ExecutorTestCase):
         }})
 
         self.obj.prepare()
+        self.obj.engine.start_subprocess = lambda **kwargs: None
         self.obj.startup()
         self.obj.get_widget()
-        try:
-            self.obj.check()
-            time.sleep(2)
-            self.obj.check()
-        except RuntimeError:
-            ROOT_LOGGER.warning("Do you use patched locust for non-GUI master?")
         self.obj.shutdown()
         self.obj.post_process()
         self.assertFalse(self.obj.has_results())
@@ -206,8 +204,6 @@ class TestLocustIOExecutor(ExecutorTestCase):
                 "requests": ["http://blazedemo.com/"]}}})
 
         self.obj.prepare()
-        self.obj.startup()
-        self.obj.check()
         self.obj.shutdown()
         self.obj.post_process()
 
@@ -268,8 +264,6 @@ class TestLocustIOExecutor(ExecutorTestCase):
                         "url": "http://blazedemo.com"}]}}})
 
         self.obj.prepare()
-        self.obj.startup()
-        self.obj.shutdown()
         debug_buff = self.log_recorder.debug_buff.getvalue()
         self.assertNotIn("'--host='", debug_buff)
 
@@ -286,10 +280,6 @@ class TestLocustIOExecutor(ExecutorTestCase):
             }
         }})
         self.obj.prepare()
-        self.obj.startup()
-        while not self.obj.check():
-            time.sleep(self.obj.engine.check_interval)
-        self.obj.shutdown()
         self.obj.post_process()
 
         kpi_path = os.path.join(self.obj.engine.artifacts_dir, "kpi.jtl")
@@ -300,30 +290,6 @@ class TestLocustIOExecutor(ExecutorTestCase):
             header_line = jtl[0].strip()
             expected = "timeStamp,label,method,elapsed,bytes,responseCode,responseMessage,success,allThreads,Latency"
             self.assertEqual(header_line, expected)
-
-    def test_jtl_quoting_issue(self):
-        self.configure({"execution": {
-            "concurrency": 1,
-            "iterations": 1,
-            "scenario": {
-                "default-address": "http://httpbin.org/status/503",
-                "requests": [
-                    "/"
-                ]
-            }
-        }})
-        self.obj.prepare()
-        self.obj.startup()
-        while not self.obj.check():
-            time.sleep(self.obj.engine.check_interval)
-        self.obj.shutdown()
-        self.obj.post_process()
-
-        kpi_path = os.path.join(self.obj.engine.artifacts_dir, "kpi.jtl")
-        self.assertTrue(os.path.exists(kpi_path))
-
-        reader = JTLReader(kpi_path, self.obj.log)
-        list(reader.datapoints())
 
     def test_diagnostics(self):
         self.configure({"execution": {
@@ -337,10 +303,6 @@ class TestLocustIOExecutor(ExecutorTestCase):
             }
         }})
         self.obj.prepare()
-        self.obj.startup()
-        while not self.obj.check():
-            time.sleep(self.obj.engine.check_interval)
-        self.obj.shutdown()
         self.obj.post_process()
         diagnostics = self.obj.get_error_diagnostics()
         self.assertIsNotNone(diagnostics)
