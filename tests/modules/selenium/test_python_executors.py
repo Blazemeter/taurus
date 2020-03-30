72e7958a75cc49bcea6054892f48136b31300b02
greyfenrir <grey.fenrir@gmail.com>
2020-03-31 02:54:49 +0300

    del 8

--- tests/modules/selenium/test_python_executors.py
+++ tests/modules/selenium/test_python_executors.py
@@ -35,95 +35,8 @@ class MockWebDriver(object):
         return [element for element in self.content if element == target]
 
 
-
 class TestPyTestExecutor(ExecutorTestCase):
     EXECUTOR = PyTestExecutor
-
-    def test_full_single_script(self):
-        self.obj.execution.merge({
-            "iterations": 1,
-            "scenario": {
-                "script": RESOURCES_DIR + "selenium/pytest/test_statuses.py"
-            }
-        })
-        self.obj.prepare()
-        try:
-            self.obj.startup()
-            while not self.obj.check():
-                time.sleep(self.obj.engine.check_interval)
-        finally:
-            self.obj.shutdown()
-        self.obj.post_process()
-        self.assertFalse(self.obj.has_results())
-        self.assertNotEquals(self.obj.process, None)
-
-    def test_statuses(self):
-        self.obj.execution.merge({
-            "scenario": {
-                "script": RESOURCES_DIR + "selenium/pytest/test_statuses.py"
-            }
-        })
-        self.obj.prepare()
-        try:
-            self.obj.startup()
-            while not self.obj.check():
-                time.sleep(self.obj.engine.check_interval)
-        finally:
-            self.obj.shutdown()
-        self.obj.post_process()
-        with open(self.obj.report_file) as fds:
-            report = [json.loads(line) for line in fds.readlines() if line]
-        self.assertEqual(4, len(report))
-        self.assertEqual(["PASSED", "FAILED", "FAILED", "SKIPPED"], [item["status"] for item in report])
-
-        failed_item = report[1]
-        assertions = failed_item["assertions"]
-        self.assertEqual(1, len(assertions))
-        assertion = assertions[0]
-        self.assertEqual('assert (2 + (2 * 2)) == 8', assertion['error_msg'])
-        self.assertTrue(assertion['failed'])
-        self.assertEqual('AssertionError: assert (2 + (2 * 2)) == 8', assertion['name'])
-        self.assertIsNotNone(assertion.get('error_trace'))
-
-    def test_iterations(self):
-        self.obj.execution.merge({
-            "iterations": 10,
-            "scenario": {
-                "script": RESOURCES_DIR + "selenium/pytest/test_single.py"
-            }
-        })
-        self.obj.prepare()
-        try:
-            self.obj.startup()
-            while not self.obj.check():
-                time.sleep(self.obj.engine.check_interval)
-        finally:
-            self.obj.shutdown()
-        self.obj.post_process()
-        with open(self.obj.report_file) as fds:
-            report = [json.loads(line) for line in fds.readlines() if line]
-        self.assertEqual(10, len(report))
-        self.assertTrue(all(item["status"] == "PASSED" for item in report))
-
-    def test_hold(self):
-        self.obj.execution.merge({
-            "hold-for": "3s",
-            "scenario": {
-                "script": RESOURCES_DIR + "selenium/pytest/test_single.py"
-            }
-        })
-        self.obj.prepare()
-        try:
-            start_time = time.time()
-            self.obj.startup()
-            while not self.obj.check():
-                time.sleep(self.obj.engine.check_interval)
-        finally:
-            self.obj.shutdown()
-            end_time = time.time()
-        self.obj.post_process()
-        duration = end_time - start_time
-        self.assertGreaterEqual(duration, 3.0)
 
     def test_blazedemo(self):
         self.obj.engine.check_interval = 0.1
@@ -144,23 +57,5 @@ class TestPyTestExecutor(ExecutorTestCase):
             report = [json.loads(line) for line in fds.readlines() if line]
         self.assertEqual(2, len(report))
 
-    def test_additional_args(self):
-        additional_args = "--foo --bar"
-        self.obj.execution.merge({
-            "scenario": {
-                "additional-args": additional_args,
-                "script": RESOURCES_DIR + "selenium/pytest/test_single.py"
-            }
-        })
-        self.obj.runner_path = RESOURCES_DIR + "selenium/pytest/bin/runner.py"
-        self.obj.prepare()
-        try:
-            self.obj.startup()
-            while not self.obj.check():
-                time.sleep(self.obj.engine.check_interval)
-        finally:
-            self.obj.shutdown()
-        with open(self.obj.stdout.name) as fds:
-            stdout = fds.read()
-            self.assertIn(additional_args, stdout)
+
 
