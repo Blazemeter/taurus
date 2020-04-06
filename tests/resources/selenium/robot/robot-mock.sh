8162e66cd08e60ed81d75e7e981f3a05fcde9907
Alla Levental <clnlbsmrn@gmail.com>
2020-04-06 12:12:45 +0300

    Remove execs from python executor tests (#1255)
    * Remove execs from python executors
    
    * Robot
    
    * Pytest
    
    * Add tests
    
    * Fix .bat & .sh
    
    * Small fix
    
    * Robot & PyTest
    
    * Fixes
    
    * Small fixes
    
    * Fixes

---
+++ tests/resources/selenium/robot/robot-mock.sh
@@ -0,0 +1,8 @@
+#!/bin/sh -xe
+
+# dump cmdline
+echo "$@"
+
+REPORT_FILE="$TAURUS_ARTIFACTS_DIR/RobotExecutor.ldjson"
+
+echo '{"test_suite": "Test", "test_case": "User can create an account and log in", "status": "PASSED", "start_time": 1582199961.0, "duration": 0.076, "error_msg": null, "error_trace": null, "extras": {"assertions": []}, "assertions": [], "subsamples": [], "path": []}' > "$REPORT_FILE"
