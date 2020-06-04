58deda92e55f6e304b8f5b87719f5b6a20fe492c
aleksandrOranskiy <aranich@ya.ru>
2020-06-04 11:44:41 +0300

    improve jenkins setup (#1317)
    WAP-18084-job-dsl-added-files
    Co-authored-by: aleksandrOranskiy <alexander.oranskiy@broadcom.com>

---
+++ tests/ci/JobDsl_stable.groovy
@@ -0,0 +1,29 @@
+pipelineJob('TAURUS-COMMUNITY-STABLE'){
+    properties {
+        githubProjectUrl('http://github.com/Blazemeter/taurus')
+        disableConcurrentBuilds()
+        pipelineTriggers {
+            triggers {
+                scm('H/30 * * * *')
+            }
+        }
+    }
+    logRotator {
+        daysToKeep(30)
+        numToKeep(30)
+    }
+    definition {
+        cpsScm {
+            scm{
+                git{
+                    remote {
+                        url('http://github.com/Blazemeter/taurus.git')
+                        branch('refs/tags/*')
+                        credentials('github-token')
+                    }
+                }
+            }
+            scriptPath('Jenkinsfile')
+        }
+    }
+}
