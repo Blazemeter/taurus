fabcdfa6fa020e1bd9dfaace9788ebd452d43b44
Chandrasekar Sankarram <ckrams@yahoo.com>
2020-07-20 09:57:36 -0700

    Update cli.py
    revising code logic to allow test coverage increase for existing code

--- bzt/cli.py
+++ bzt/cli.py
@@ -492,8 +492,9 @@ class ConfigOverrider(object):
                     i = self.__apply_mult_override(i, key, replace_value)
         if isinstance(obj, dict):            
             for k, v in obj.items():
-                obj[k] = self.__apply_mult_override(v, key, replace_value)
-        if isinstance(obj, dict) and key in obj:
+                if key in v:
+                    obj[k] = self.__apply_mult_override(v, key, replace_value)
+        if key in obj:
             obj[key] = replace_value
         return obj
 
