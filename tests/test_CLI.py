f44dd2c2ce445674784d4c4c0b2c04db2d41959b
Chandrasekar Sankarram <ckrams@yahoo.com>
2020-07-31 10:51:23 -0700

    Update test_CLI.py

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -331,6 +331,7 @@ class TestConfigOverrider(BZTestCase):
         self.config["items"] = [1, 2, 3]
         self.config["dict"] = {"listObj":[{"k1":"v1"}, {"k2":"v2"}, {"k3":"v3"}],"lislis":[1,2,3,4],"k1":"v3","test.k4":"v4"}
         self.obj.apply_overrides(['items.*1=v2'], self.config)
-        self.obj.apply_overrides(['dict.*k1=v2', 'dict.*k4=test-wildcard-override'], self.config)
+        self.obj.apply_overrides(['dict.*k1=v2'], self.config) 
+        self.obj.apply_overrides(['dict.*k4=test-wildcard-override'], self.config)
         self.assertEqual(self.config.get("dict"), {'listObj': [{'k1': 'v2'}, {'k2': 'v2'}, {'k3': 'v3'}], 'lislis': [1, 2, 3, 4], 'k1': 'v2', 'test.k4': 'test-wildcard-override')
         self.assertEqual(self.config.get("items"), [1, 2, 3])
