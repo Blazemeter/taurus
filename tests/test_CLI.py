d24b8a44b38be7024029d44f5af7ce3bdde1c475
Chandrasekar Sankarram <ckrams@yahoo.com>
2020-07-31 20:11:05 -0700

    Update test_CLI.py

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -330,10 +330,7 @@ class TestConfigOverrider(BZTestCase):
     def test_override_multiple(self):
         self.config["items"] = [1, 2, 3]
         self.config["dict"] = {"listObj":[{"k1":"v1"}, {"k2":"v2"}, {"k3":"v3"}],"lislis":[1,2,3,4],"k1":"v3"}
-#        self.config["dict"] = {"listObj":[{"k1":"v1"}, {"k2":"v2"}, {"k3":"v3"}, {"default-address":"v1"}],"lislis":[1,2,3,4],"k1":"v3","default-address":"v2"}
         self.obj.apply_overrides(['items.*1=v2'], self.config)
         self.obj.apply_overrides(['dict.*k1=v2'], self.config)
-#        self.obj.apply_overrides(['dict.*default-address=foo.com'], self.config)
         self.assertEqual(self.config.get("dict"), {'listObj': [{'k1': 'v2'}, {'k2': 'v2'}, {'k3': 'v3'}], 'lislis': [1, 2, 3, 4], 'k1': 'v2'})
-#        self.assertEqual(self.config.get("dict"), {'listObj': [{'k1': 'v2'}, {'k2': 'v2'}, {'k3': 'v3'}, {'default-address': 'foo.com'}], 'lislis': [1, 2, 3, 4], 'k1': 'v2','default-address': 'foo.com'})
         self.assertEqual(self.config.get("items"), [1, 2, 3])
