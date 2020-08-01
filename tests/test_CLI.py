b43a459b88e1df8cdea6b2f76548e51418300299
Chandrasekar Sankarram <ckrams@yahoo.com>
2020-07-31 17:31:06 -0700

    Update test_CLI.py

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -333,6 +333,6 @@ class TestConfigOverrider(BZTestCase):
         self.obj.apply_overrides(['items.*1=v2'], self.config)
         self.obj.apply_overrides(['dict.*k1=v2'], self.config)
         self.obj.apply_overrides(['dict.*default-address=foo.com'], self.config)
-        print (self.config["dict"])
+        print ("dict values after override:"+ self.config["dict"])
         self.assertEqual(self.config.get("dict"), {'listObj': [{'k1': 'v2'}, {'k2': 'v2'}, {'k3': 'v3'}, {'k4.default-address': 'foo.com'}, {'k5.default-address': 'foo.com'}], 'lislis': [1, 2, 3, 4], 'k1': 'v2'})
         self.assertEqual(self.config.get("items"), [1, 2, 3])
