d6fec7956d916f298dfa2690d353f31904140982
Chandrasekar Sankarram <ckrams@yahoo.com>
2020-08-02 15:51:49 -0700

    Update test_CLI.py

--- tests/test_CLI.py
+++ tests/test_CLI.py
@@ -333,6 +333,6 @@ class TestConfigOverrider(BZTestCase):
         self.obj.apply_overrides(['items.*1=v2'], self.config)
         self.obj.apply_overrides(['dict.*k1=v2'], self.config)
         self.obj.apply_overrides(['dict.*k4=foo.com'], self.config)
-        self.obj.apply_overrides(['dict.*k5'=foo.com'], self.config)
+        self.obj.apply_overrides(['dict.*k5=foo.com'], self.config)
         self.assertEqual(self.config.get("dict"), {'listObj': [{'k1': 'v2'}, {'k2': 'v2'}, {'k3': 'v3'}, {'k4': 'foo.com'}], 'lislis': [1, 2, 3, 4], 'k1': 'v2', 'k4': 'foo.com','k5': 'foo.com'})
         self.assertEqual(self.config.get("items"), [1, 2, 3])
