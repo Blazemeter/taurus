35fc49750233dc52ef1855c60a16916edf1fe342
Chandrasekar Sankarram <ckrams@yahoo.com>
2020-08-02 16:17:40 -0700

    Update cli.py

--- bzt/cli.py
+++ bzt/cli.py
@@ -493,7 +493,7 @@ class ConfigOverrider(object):
         if isinstance(obj, dict):            
             for k, v in obj.items():
                 obj[k] = self.__apply_mult_override(v, key, replace_value)
-        if isinstance(obj, (dict,list)) and key in obj:
+        if key in obj:
             obj[key] = replace_value
         return obj
 
