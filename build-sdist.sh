f9d79028b4a16e3bc04dd0c96087b058c442a0de
greyfenrir <grey.fenrir@gmail.com>
2020-09-19 09:28:34 +0300

    fix docker build environment

--- build-sdist.sh
+++ build-sdist.sh
@@ -1,24 +1,27 @@
 #!/bin/bash -xe
 
-TAURUS=$(dirname $0)
+PREV_DIR=`pwd`
+cd "$(dirname $0)"
 
 echo "Getting build info"
 VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 GIT_INFO="$(git rev-parse --abbrev-ref HEAD) $(git show --oneline -s)"
-echo "VERSION=\"$VERSION\"" > ${TAURUS}/bzt/resources/version/version.py
-echo "GIT_INFO=\"$GIT_INFO\"" > ${TAURUS}/bzt/resources/version/git_info.py
+echo "VERSION=\"$VERSION\"" > bzt/resources/version/version.py
+echo "GIT_INFO=\"$GIT_INFO\"" > bzt/resources/version/git_info.py
 
 echo "Cleaning environment"
-python3 ${TAURUS}/setup.py clean
+python3 setup.py clean
 
 echo "Building NUnit plugin"
-pushd ${TAURUS}/dotnet/NUnitRunner
-${TAURUS}/rebuild.sh
+pushd ./dotnet/NUnitRunner
+./rebuild.sh
 popd
 
 echo "Building chrome-loader.exe"
-rm -f ${TAURUS}/bzt/resources/chrome-loader.exe
-x86_64-w64-mingw32-gcc -std=c99 -o ${TAURUS}/bzt/resources/chrome-loader.exe ${TAURUS}/bzt/resources/chrome-loader.c
+rm -f bzt/resources/chrome-loader.exe
+x86_64-w64-mingw32-gcc -std=c99 -o bzt/resources/chrome-loader.exe bzt/resources/chrome-loader.c
 
 echo "Creating distribution packages"
-python3 ${TAURUS}/setup.py sdist bdist_wheel
+python3 ./setup.py sdist bdist_wheel
+
+cd "${PREV_DIR}"
