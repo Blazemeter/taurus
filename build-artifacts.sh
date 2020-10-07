37ea2b10c12f61427b01c04b07d1e32666b7e05c
greyfenrir <grey.fenrir@gmail.com>
2020-10-07 13:48:34 +0300

    clean wheel directory

--- build-artifacts.sh
+++ build-artifacts.sh
@@ -16,6 +16,7 @@ rm -f bzt/resources/chrome-loader.exe
 x86_64-w64-mingw32-gcc -std=c99 -o bzt/resources/chrome-loader.exe bzt/resources/chrome-loader.c
 
 echo "Creating distribution packages"
+rm -rf ./dist
 python3 ./setup.py sdist bdist_wheel
 
 cd "${PREV_DIR}"
