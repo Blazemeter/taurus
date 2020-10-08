8dbe56b82cac320c7aa242bfa85e8961ade4d0de
aleksandrOranskiy <alexander.oranskiy@broadcom.com>
2020-10-08 12:40:13 +0300

    fix-taurus-image-build-job

--- build-info.sh
+++ build-info.sh
@@ -3,7 +3,7 @@
 echo "Getting build info"
 VERSION=$(git describe --tags $(git rev-list --tags --max-count=1))
 GIT_INFO="$(git rev-parse --abbrev-ref HEAD) $(git show --oneline -s)"
-if [ "$1" = "true" ]; then
+if [ "$1" != "true" ]; then
     VERSION="${VERSION}.${BUILD_NUMBER}"
 fi
 echo "BUILD_NUM=\"$BUILD_NUMBER\"" > bzt/resources/version/build.py
