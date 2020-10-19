54de1b43e13deb8f98c98e77839704359f2d6b6b
Eugene <merilian@yandex.ru>
2020-10-19 10:51:33 +0300

    add new xUnit executor

--- dotnet/DotnetTestRunner/DotnetTestRunner/Models/ReportItem.cs
+++ dotnet/DotnetTestRunner/DotnetTestRunner/Models/ReportItem.cs
@@ -1,25 +1,17 @@
-﻿using System.Collections.Generic;
+﻿﻿using System.Collections.Generic;
 
-namespace NUnitRunner.Models
+ namespace DotnetTestRunner.Models
 {
     public class ReportItem
     {
         public long StartTime { get; set; }
-
         public double Duration { get; set; }
-
         public string ThreadName { get; set; }
-
         public string TestCase { get; set; }
-
         public string TestSuite { get; set; }
-
         public string Status { get; set; }
-
         public string ErrorMessage { get; set; }
-
         public string ErrorTrace { get; set; }
-
-        public Dictionary<object, object> Extras { get; set; }
+        public IDictionary<object, object> Extras { get; set; }
     }
 }
---
+++ dotnet/DotnetTestRunner/DotnetTestRunner/NUnitTestClass.cs
@@ -0,0 +1,14 @@
+﻿using NUnit.Framework;
+
+namespace DotnetTestRunner
+{
+    [TestFixture()]
+    public class NUnitTestClass
+    {
+        [Test()]
+        public void TestCase()
+        {
+            //Program.Main(new [] { "--iterations", "1", "--target", "../../NUnitSelenium/SeleniumSuite/bin/Release/SeleniumSuite.dll"});
+        }
+    }
+}
