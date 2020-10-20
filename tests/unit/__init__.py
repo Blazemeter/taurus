4ab0ca37c7528600ceb5d85dd41ab6b9374592c5
greyfenrir <grey.fenrir@gmail.com>
2020-10-20 21:23:09 +0300

    fix import order

--- tests/unit/__init__.py
+++ tests/unit/__init__.py
@@ -1,5 +1,5 @@
 """ unit test """
 from tests.unit.base import TEST_DIR, BUILD_DIR, RESOURCES_DIR, BASE_CONFIG, ROOT_LOGGER, BZT_DIR
 from tests.unit.base import close_reader_file, local_paths_config
-from tests.unit.cases import BZTestCase, ExecutorTestCase
 from tests.unit.mocks import random_datapoint, EngineEmul, BZMock
+from tests.unit.cases import BZTestCase, ExecutorTestCase
