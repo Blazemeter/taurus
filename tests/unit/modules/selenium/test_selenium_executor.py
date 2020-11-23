b48f9cb45228c1cf0124984bb6fbfe4f1f9727bf
greyfenrir <grey.fenrir@gmail.com>
2020-11-23 11:27:15 +0300

    fix

--- tests/unit/modules/selenium/test_selenium_executor.py
+++ tests/unit/modules/selenium/test_selenium_executor.py
@@ -13,7 +13,7 @@ from bzt.modules.apiritif import ApiritifNoseExecutor
 from bzt.modules.functional import LoadSamplesReader, FuncSamplesReader
 from bzt.modules.provisioning import Local
 from bzt.modules.selenium import SeleniumExecutor
-from bzt.utils import LDJSONReader, FileReader
+from bzt.utils import LDJSONReader, FileReader, is_windows
 from tests.unit import BZTestCase, RESOURCES_DIR, ROOT_LOGGER, EngineEmul
 from tests.unit.mocks import DummyListener
 from tests.unit.modules.selenium import SeleniumTestCase
