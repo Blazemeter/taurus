d5d09058420be8c99b3d7a6be85c4ce4152232e5
greyfenrir <grey.fenrir@gmail.com>
2020-12-07 13:14:40 +0300

    cosmetics

--- build-windows-installer.sh
+++ build-windows-installer.sh
@@ -20,27 +20,12 @@ fi
 
 TAURUS_DIST="$1"
 
-# install pythonz
-#curl -kL https://raw.github.com/saghul/pythonz/master/pythonz-install | bash
-
-#PYTHONZ_HOME="${HOME}/.pythonz/bin/pythonz"
-#PYTHONZ_SYSTEM="/usr/local/pythonz/bin/pythonz"
-#[ -f "$PYTHONZ_HOME" ] && PYTHONZ="$PYTHONZ_HOME" || PYTHONZ="$PYTHONZ_SYSTEM"
-
-#if ! command -v python &> /dev/null
-#then
-#    echo "create symlink for python"
-#    ln -s "$(which python3)" /usr/bin/python
-#fi
-# install specific version of python
-
 PYTHONZ=pythonz
 $PYTHONZ install 3.7.5
 
 # set up python virtualenv
 python3 -m virtualenv venv --python=$($PYTHONZ locate 3.7.5)
 PYTHON=python
-#PYTHON=$($PYTHONZ locate 3.7.5)
 
 # this is a workaround for venv bug: https://github.com/pypa/virtualenv/issues/1029
 set +u
@@ -51,8 +36,6 @@ set -u
 $PYTHON -m pip install -U pip-custom-platform wheel
 $PYTHON -m pip install pynsist==2.1 pip==19.3
 $PYTHON scripts/installer/gen_installer.py "$TAURUS_DIST"
-
-echo "gen_installer result: $?"
 
 # deactivate venv afterwards
 deactivate
