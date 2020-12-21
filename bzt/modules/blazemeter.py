bba8da704567869369df3e6365e76799b7d62ad9
Alla Levental <alla.levental@broadcom.com>
2020-12-21 11:44:57 +0300

    Fixes

--- bzt/modules/blazemeter.py
+++ bzt/modules/blazemeter.py
@@ -1544,15 +1544,13 @@ class CloudProvisioning(MasterProvisioning, WidgetProvider):
         validate_passfail = any(reporter.get('module') == 'passfail' for reporter in reporting)
 
         if validate_passfail:
-            validation_result = self.router._test.started_passfail_validation()
-            if validation_result:
+            if self.router._test.started_passfail_validation():
                 timeout = 100
                 for i in range(timeout):
-                    validation_result = self.router._test.get_passfail_validation()
-                    if validation_result:
+                    if self.router._test.get_passfail_validation():
                         return
                     self.log.warning(f"Unsuccessful Passfail validation attempt [{i+1}]. Retrying...")
-                    if i % 10:
+                    if not i % 10:
                         self.log.warning("Please keep in mind that validation can take time.")
                     sleep(1)
                 self.log.error("Unable get Passfail validation!")
