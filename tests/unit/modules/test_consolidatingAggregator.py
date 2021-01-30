151c182cb686f1475bdd5f393dc34608998a9100
greyfenrir <grey.fenrir@gmail.com>
2021-01-30 16:42:12 +0300

    fix

--- tests/unit/modules/test_consolidatingAggregator.py
+++ tests/unit/modules/test_consolidatingAggregator.py
@@ -117,7 +117,7 @@ class SmartMockReader(MockReader):
     # reader example with 'r_code' rule support
     def add_rule(self, rule):
         if rule.lower() == 'r_code':
-            get_label = self._get_label_generator(lambda kpis: kpis[4] == 200)  # r_code
+            get_label = self._get_label_generator(lambda kpis: 'succ' if kpis[4] == 200 else 'fail')  # r_code
             self.get_label = get_label
         else:
             super(SmartMockReader, self).add_rule(rule)     # just raise exception
