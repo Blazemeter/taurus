c6cad16270be58253d7979d1555011d727dd4915
greyfenrir <grey.fenrir@gmail.com>
2021-03-11 20:03:34 +0300

    fill generator logic

--- bzt/jmx/mqtt.py
+++ bzt/jmx/mqtt.py
@@ -7,7 +7,7 @@ from lxml import etree
 class MQTTProtocolHandler(ProtocolHandler):
     def get_sampler_pair(self, request):
         if request.method == 'connect':
-            mqtt = self._get_connect_method(request)
+            mqtt = self._get_connect_sampler(request)
         elif request.method == 'publish':
             mqtt = self._get_publish_method(request)
         elif request.method == 'subscribe':
@@ -15,19 +15,70 @@ class MQTTProtocolHandler(ProtocolHandler):
         elif request.method == 'disconnect':
             mqtt = self._get_disconnect_method(request)
         else:
-            raise TaurusConfigError(f'unsupported mqtt method: {request.method}')
+            raise TaurusConfigError(f'Unsupported mqtt method: {request.method}')
 
         children = etree.Element("hashTree")
         return mqtt, children
 
-    def _get_connect_method(self, request):
-        pass
+    @staticmethod
+    def _get_connect_sampler(request):
+        mqtt = etree.Element("net.xmeter.samplers.ConnectSampler",
+                             guiclass="net.xmeter.gui.ConnectSamplerUI",
+                             testclass="net.xmeter.samplers.ConnectSampler",
+                             testname=request.label)
+        mqtt.append(JMX._string_prop("mqtt.server", request.config.get("addr", "127.0.0.1")))
+        mqtt.append(JMX._string_prop("mqtt.port", "1883"))
+        mqtt.append(JMX._string_prop("mqtt.version", "3.1"))
+        mqtt.append(JMX._string_prop("mqtt.conn_timeout", "10"))
+        mqtt.append(JMX._string_prop("mqtt.protocol", "TCP"))
+        mqtt.append(JMX._string_prop("mqtt.ws_path", ""))
+        mqtt.append(JMX._bool_prop("mqtt.dual_ssl_authentication", False))
+        mqtt.append(JMX._string_prop("mqtt.clientcert_file_path", ""))
+        mqtt.append(JMX._string_prop("mqtt.clientcert_password", ""))
+        mqtt.append(JMX._string_prop("mqtt.client_id_prefix", "conn_"))
+        mqtt.append(JMX._bool_prop("mqtt.client_id_suffix", True))
+        mqtt.append(JMX._string_prop("mqtt.conn_keep_alive", "300"))
+        mqtt.append(JMX._string_prop("mqtt.conn_attampt_max", "0"))
+        mqtt.append(JMX._string_prop("mqtt.reconn_attampt_max", "0"))
+        mqtt.append(JMX._string_prop("mqtt.conn_clean_session", "true"))
+
+        return mqtt
 
     def _get_publish_method(self, request):
-        pass
+        mqtt = etree.Element("net.xmeter.samplers.PubSampler",
+                             guiclass="net.xmeter.gui.PubSamplerUI",
+                             testclass="net.xmeter.samplers.PubSampler",
+                             testname=request.label)
+        topic_missed = TaurusConfigError(f'Topic is required for request "{request.config}"')
+        mqtt.append(JMX._string_prop("mqtt.topic_name", request.config.get("topic", topic_missed)))
+        mqtt.append(JMX._string_prop("mqtt.qos_level", "0"))
+        mqtt.append(JMX._bool_prop("mqtt.add_timestamp", True))
+        mqtt.append(JMX._string_prop("mqtt.message_type", "String"))
+        mqtt.append(JMX._string_prop("mqtt.message_type_fixed_length", "1024"))
+        mqtt.append(JMX._string_prop("mqtt.message_to_sent", request.config.get("message", "")))
+        mqtt.append(JMX._bool_prop("mqtt.retained_message", False))
+
+        return mqtt
 
     def _get_subscribe_method(self, request):
-        pass
+        mqtt = etree.Element("net.xmeter.samplers.SubSampler",
+                             guiclass="net.xmeter.gui.SubSamplerUI",
+                             testclass="net.xmeter.samplers.SubSampler",
+                             testname=request.label)
+        topic_missed = TaurusConfigError(f'Topic is required for request "{request.config}"')
+        mqtt.append(JMX._string_prop("mqtt.topic_name", request.config.get("topic", topic_missed)))
+        mqtt.append(JMX._string_prop("mqtt.qos_level", "0"))
+        mqtt.append(JMX._bool_prop("mqtt.add_timestamp", False))
+        mqtt.append(JMX._bool_prop("mqtt.debug_response", False))
+        mqtt.append(JMX._string_prop("mqtt.sample_condition", "specified elapsed time (ms)"))
+        mqtt.append(JMX._string_prop("mqtt.sample_condition_value", "1000"))
+
+        return mqtt
 
     def _get_disconnect_method(self, request):
-        pass
+        mqtt = etree.Element("net.xmeter.samplers.DisConnectSampler",
+                             guiclass="net.xmeter.gui.DisConnectSamplerUI",
+                             testclass="net.xmeter.samplers.DisConnectSampler",
+                             testname=request.label)
+
+        return mqtt
