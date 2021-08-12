170f4494136693a8b88efabd1ce7baf997dde1e5
Petr <petr.grushevskiy@broadcom.com>
2021-08-12 22:21:47 +0300

    fix MicrosoftEdge browser name (#1519)
    DE510223

--- bzt/modules/_apiritif/generator.py
+++ bzt/modules/_apiritif/generator.py
@@ -902,6 +902,8 @@ from selenium.webdriver.common.keys import Keys
         browser = self.capabilities.get("browserName", "")
         browser = self.scenario.get("browser", browser)
         browser = browser.lower()  # todo: whether we should take browser as is? (without lower case)
+        if browser == "microsoftedge":    # for remote webdriver only
+            browser = "MicrosoftEdge"
         local_browsers = ["firefox", "chrome", "ie", "opera"] + mobile_browsers
 
         browser_platform = None
