8be92bdfa3481d19e870aca43d06bdf8d4a84d6d
Alla Levental <alla.levental@broadcom.com>
2021-08-27 18:36:38 +0300

    Fix imports

--- bzt/modules/_molotov.py
+++ bzt/modules/_molotov.py
@@ -16,7 +16,6 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 import os
-import sys
 from math import ceil
 
 from bzt.engine import ScenarioExecutor, HavingInstallableTools, SelfDiagnosable, FileLister
