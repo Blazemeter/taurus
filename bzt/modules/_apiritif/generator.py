db2c0f973aa25bf4eb747d2431ff6d2c2283492e
Petr <petr.grushevskiy@broadcom.com>
2021-12-29 12:39:50 +0300

    use MiniBrowser instead of safari for remote (#1611)
    add supporting of WebKitGTKOptions for remote safari in Blazemeter.
    In case of remote safari in Blazemeter docker image with webkit and MiniBrowser is used under the hood. So to support browser options for such cases we need to use WebKitGTKOptions.
    US792009
    
    Co-authored-by: greyfenrir <grey.fenrir@gmail.com>

--- bzt/modules/_apiritif/generator.py
+++ bzt/modules/_apiritif/generator.py
@@ -1037,19 +1037,19 @@ from selenium.webdriver.common.keys import Keys
 
     def _get_options(self, browser):
         if browser == 'remote':
-            if 'firefox' == self.capabilities.get('browserName'):
-                browser = 'firefox'
-            elif 'chrome' == self.capabilities.get('browserName'):
-                browser = 'chrome'
-            elif self.capabilities.get('browserName') and \
-                    self.capabilities.get('browserName').lower() in ["microsoftedge", "edge"]:
+            browser = self.capabilities.get("browserName", "").lower()
+            if browser in ["microsoftedge", "edge"]:
                 browser = 'edge'
+            elif browser == 'safari' and 'blazemeter' in self.remote_address:
+                browser = 'MiniBrowser'  # use MiniBrowser instead of safari as a remote browser in Blazemeter
         if browser == 'firefox':
             options = self._get_firefox_options()
         elif browser == 'chrome':
             options = self._get_chrome_options()
         elif browser == 'edge' and self.selenium_version.startswith("4"):
             options = self._get_edge_options()
+        elif browser == 'MiniBrowser':
+            options = self._get_webkitgtk_options()
         else:
             if self.selenium_version.startswith("4"):
                 options = [ast.Assign(targets=[ast.Name(id="options")], value=ast_call(func=ast_attr("ArgOptions")))]
@@ -1106,6 +1106,14 @@ from selenium.webdriver.common.keys import Keys
                 value=ast_call(func=ast_attr("webdriver.EdgeOptions")))]
 
         return edge_options + self._get_headless_setup()
+
+    def _get_webkitgtk_options(self):
+        webkitgtk_options = [
+            ast.Assign(
+                targets=[ast.Name(id="options")],
+                value=ast_call(func=ast_attr("webdriver.WebKitGTKOptions")))]
+
+        return webkitgtk_options + self._get_headless_setup()
 
     def _get_firefox_profile(self):
         capabilities = sorted(self.capabilities.keys())
@@ -1229,29 +1237,23 @@ from selenium.webdriver.common.keys import Keys
     def _get_selenium_options(self, browser):
         options = []
 
-        if not self.selenium_version.startswith("4"):
-            old_version = True
-            if browser != 'firefox' and browser != 'chrome':
-                self.log.warning(f'Selenium options are not supported. '
-                                 f'Browser {browser}. Selenium version {self.selenium_version}')
-                return []
-        else:
-            old_version = False
-            if browser not in ['firefox', 'chrome', 'edge']:
-                self.log.debug(
-                    f'Generating selenium options. Browser {browser}. Selenium version {self.selenium_version}')
+        old_version = self.selenium_version.startswith("3")
 
-        for opt in self.executor.settings.get(self.OPTIONS):
-            if opt == "ignore-proxy":
-                options.extend(self._get_ignore_proxy(old_version))
-            elif opt == "arguments":
-                options.extend(self._get_arguments())
-            elif opt == "experimental-options":
-                options.extend(self._get_experimental_options(browser))
-            elif opt == "preferences":
-                options.extend(self._get_preferences(browser))
-            else:
-                self.log.warning(f'Unknown option {opt}')
+        if old_version and browser not in ['firefox', 'chrome', 'MiniBrowser']:
+            self.log.warning(
+                f'Selenium options are not supported. Browser {browser}. Selenium version {self.selenium_version}')
+        else:
+            for opt in self.executor.settings.get(self.OPTIONS):
+                if opt == "ignore-proxy":
+                    options.extend(self._get_ignore_proxy(old_version))
+                elif opt == "arguments":
+                    options.extend(self._get_arguments())
+                elif opt == "experimental-options":
+                    options.extend(self._get_experimental_options(browser))
+                elif opt == "preferences":
+                    options.extend(self._get_preferences(browser))
+                else:
+                    self.log.warning(f'Unknown option {opt}')
 
         return options
 
