73f0c54062332a3c0eab291a148c520315c07b06
Pavel Marik <pmarik@perforce.com>
2023-05-05 12:07:55 +0200

    US47299-vulnerabilities

--- tests/resources/selenium/invalid/NotJUnittest.java
+++ tests/resources/selenium/invalid/NotJUnittest.java
@@ -1,8 +1,3 @@
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.firefox.FirefoxDriver;
-
 public class NotJUnittest  {
     public static void main(String[] args) {
     }
