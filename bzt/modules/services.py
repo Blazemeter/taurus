b8ffbf921b50d9cd2ec8bb47fcd572c09f980e08
dmykhaliev <dmykhaliev@perforce.com>
2023-06-22 17:49:47 +0200

    robotfr

--- bzt/modules/services.py
+++ bzt/modules/services.py
@@ -144,7 +144,7 @@ class PipInstaller(Service):
             version = self.versions.get(package, None)
             cmdline += [f"{package}=={version}"] if version else [package]
         # temporary fix for selenium US48138
-        if package not in ['selenium', 'robot']:
+        if package not in ['selenium', 'robotframework']:
             cmdline += ["--upgrade"]
         self.log.debug("pip-installer cmdline: '%s'" % ' '.join(cmdline))
         try:
