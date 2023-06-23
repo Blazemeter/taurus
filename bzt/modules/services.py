50949c1035a465ef0311ebe35a67eaa1b4d87f8d
dmykhaliev <dmykhaliev@perforce.com>
2023-06-23 08:55:31 +0200

    US48138 Block upgrade to 4.10 selenium

--- bzt/modules/services.py
+++ bzt/modules/services.py
@@ -143,7 +143,11 @@ class PipInstaller(Service):
         for package in self.packages:
             version = self.versions.get(package, None)
             cmdline += [f"{package}=={version}"] if version else [package]
-        cmdline += ["--upgrade"]
+        # temporary fix for selenium US48138
+        if package not in ['selenium']:
+            cmdline += ["--upgrade"]
+        if package in ['robotframework-seleniumlibrary']:
+            cmdline += ["--no-deps"]
         self.log.debug("pip-installer cmdline: '%s'" % ' '.join(cmdline))
         try:
             out, err = exec_and_communicate(cmdline)
