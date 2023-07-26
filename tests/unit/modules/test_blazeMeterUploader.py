31d35fc5d5f35ceaee7a2c2061d7fc52d02ca67c
avishaiw12 <aweingarten@perforce.com>
2023-07-26 11:34:48 +0300

    US48369- add aggregated second unit test no data

--- tests/unit/modules/test_blazeMeterUploader.py
+++ tests/unit/modules/test_blazeMeterUploader.py
@@ -461,6 +461,24 @@ class TestBlazeMeterUploader(BZTestCase):
         expected_concurrency_data = [{'timestamp': ts * 1000, 'concurrency': datapoint['current']['']['concurrency']}]
         self.assertEqual(concurrency_data, expected_concurrency_data)
 
+    def test_record_data_aggregated_second_no_data(self):
+        reporter = BlazeMeterUploader()
+        reporter.engine = EngineEmul()
+        reporter.parameters['signature'] = '123'
+        reporter.parameters['session-id'] = 'r-v4-5f50153f49a13'
+        reporter.parameters['master-id'] = 122362
+        reporter.parameters['calibration-id'] = 4306
+        reporter.parameters['calibration-step-id'] = 10
+        reporter.parameters["send-data"] = False
+        reporter.settings['happysocks-address'] = 'https://unknown/hs'
+        reporter.settings['monitoring-buffer-limit'] = 100
+        reporter.prepare()
+
+        reporter.aggregated_second([])
+        concurrency_data = reporter._concurrency_buffer.get_data()
+        expected_concurrency_data = []
+        self.assertEqual(concurrency_data, expected_concurrency_data)
+
     @patch('bzt.modules.blazemeter.blazemeter_reporter.HappysocksClient')
     def test_engine_metrics_no_metrics_to_send(self, mock_client_class):
         # prepare mocks
