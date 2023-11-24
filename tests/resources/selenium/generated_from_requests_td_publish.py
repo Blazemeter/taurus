387a1c61a59f74a97905245ccd7315cb714f8c37
janhavel <jan.havel@broadcom.com>
2023-11-24 11:55:13 +0100

    TG-2120: Scriptless returns info about current iteration

--- tests/resources/selenium/generated_from_requests_td_publish.py
+++ tests/resources/selenium/generated_from_requests_td_publish.py
@@ -20,7 +20,7 @@ from selenium.webdriver.support import expected_conditions as econd
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.options import ArgOptions
-from bzt.resources.selenium_extras import get_locator, waiter
+from bzt.resources.selenium_extras import get_current_iteration, get_locator, waiter
 from bzt.resources.bzm_extras import BzmExtras
 
 class TestPublishSc(unittest.TestCase):
@@ -35,18 +35,19 @@ class TestPublishSc(unittest.TestCase):
         options.set_capability('unhandledPromptBehavior', 'ignore')
         self.driver = webdriver.Firefox(profile, options=options)
         self.driver.implicitly_wait(timeout)
-        apiritif.put_into_thread_store(timeout=timeout, func_mode=False, driver=self.driver, windows={}, scenario_name='publish_sc')
+        current_iter = get_current_iteration()
+        apiritif.put_into_thread_store(timeout=timeout, func_mode=False, driver=self.driver, windows={}, scenario_name='publish_sc', current_iteration=current_iter)
         self.bzm_extras = BzmExtras({'master_publish_url': 'https://tdm.blazemeter.com/api/v1/publish?signature=8UJR9hHfsdjg9032nkvx'})
 
     def _1_None(self):
         with apiritif.smart_transaction('None'):
-            
+
             do_testdata_orchestration = self.bzm_extras.do_testdata_orchestration
-            
-            do_testdata_orchestration('publish', 'entity1', 'target1')
-            
+
+            do_testdata_orchestration('publish', 'entity1', 'target1', True)
+
             do_testdata_orchestration = self.bzm_extras.do_testdata_orchestration
-            
+
             do_testdata_orchestration('un-publish', 'entity1', 'target1')
 
     def test_publishsc(self):
