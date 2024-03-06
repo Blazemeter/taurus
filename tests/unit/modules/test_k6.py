28bcb567555640804fd6e0afef54d659db75e1b4
ben.pearson <ben.pearson@asos.com>
2024-03-06 16:38:36 +0000

    fix broken test

--- tests/unit/modules/test_k6.py
+++ tests/unit/modules/test_k6.py
@@ -110,7 +110,7 @@ class TestK6Executor(ExecutorTestCase):
         self.assertEqual(self.obj.execution.get("ramp-up"), "1")
         self.assertEqual(self.obj.execution.get("concurrency"), "2")
         self.assertEqual(self.obj.execution.get("throughput"), "3")
-        load = self.EXECUTOR.get_load(self)
+        load = self.obj.get_load()
         self.assertEqual(load.ramp_up, 1)
         self.assertEqual(load.concurrency, 2)
         self.assertEqual(load.throughput, 3)
@@ -121,7 +121,7 @@ class TestK6Executor(ExecutorTestCase):
         self.assertEqual(self.obj.execution.get("throughput"), {"local": "3"})
 
         self.obj.engine.config.get("execution")[0]["concurrency"] = "22"
-        load = self.EXECUTOR.get_load(self)
+        load = self.obj.get_load()
         self.assertEqual(load.concurrency, 22)
         self.assertEqual(self.obj.execution.get("concurrency"), {"local": "22"})
 
@@ -131,7 +131,7 @@ class TestK6Executor(ExecutorTestCase):
         self.assertFalse(self.obj.execution.get("concurrency"))
         self.assertFalse(self.obj.execution.get("throughput"))
         self.assertFalse(self.obj.execution.get("iterations"))
-        load = self.EXECUTOR.get_load(self)
+        load = self.obj.get_load()
         self.assertEqual(load.ramp_up, None)
         self.assertEqual(load.concurrency, 0)
         self.assertEqual(load.throughput, 0)
@@ -146,7 +146,7 @@ class TestK6Executor(ExecutorTestCase):
             "throughput": "6",
             "steps": "7",
         }})
-        load = self.EXECUTOR.get_load(self)
+        load = self.obj.get_load()
         self.assertEquals(2, load.concurrency)
         self.assertEquals(3, load.hold)
         self.assertEquals(4, load.ramp_up)
