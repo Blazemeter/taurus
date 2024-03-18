e351d7ffb0874a25733cd88d296c7f64a13d0349
dmykhaliev <dmykhaliev@perforce.com>
2024-03-18 15:06:08 +0100

    Ubuntu upgrade #7

--- dotnet.sh
+++ dotnet.sh
@@ -2,7 +2,7 @@
 
 echo "installing dotnet-sdk-6.0.420"
 curl -Lo dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/b521d7d2-108b-43d9-861a-58b2505a125a/0023553690a68328b33bc30a38f151db/dotnet-sdk-6.0.420-linux-x64.tar.gz
-mkdir -p $HOME/dotnet && tar zxf dotnet.tar.gz -C $HOME/dotnet
-export DOTNET_ROOT=$HOME/dotnet
-export PATH=$PATH:$HOME/dotnet
+mkdir -p /dotnet && tar zxf dotnet.tar.gz -C /dotnet
+export DOTNET_ROOT=/dotnet
+export PATH=$PATH:/dotnet
 dotnet --version
