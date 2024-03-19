9659aa55a9b798ed5c7c77fd28c1705c0461a9d5
dmykhaliev <dmykhaliev@perforce.com>
2024-03-19 14:51:52 +0100

    clean up

--- dotnet.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -xe
-
-echo "installing dotnet-sdk-6.0.420"
-curl -Lo dotnet.tar.gz https://download.visualstudio.microsoft.com/download/pr/b521d7d2-108b-43d9-861a-58b2505a125a/0023553690a68328b33bc30a38f151db/dotnet-sdk-6.0.420-linux-x64.tar.gz
-mkdir -p /dotnet && tar zxf dotnet.tar.gz -C /dotnet
-export DOTNET_ROOT=/dotnet
-export PATH=$PATH:/dotnet
-dotnet --version
