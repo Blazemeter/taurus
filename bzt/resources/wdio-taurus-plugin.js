1bca2bb2fb7de6f9abd460d6cac119f709d7f3af
dmykhaliev <dmykhaliev@perforce.com>
2024-07-19 22:16:15 +0200

    try fix wdio #3

--- bzt/resources/wdio-taurus-plugin.js
+++ bzt/resources/wdio-taurus-plugin.js
@@ -1,5 +1,5 @@
 const fs = require("fs");
-const { Launcher } = await import('@wdio/cli');
+const {Launcher} = require('@wdio/cli');
 
 function epoch() {
     return (new Date()).getTime() / 1000.0;
