c839d363647d2605e89159957195b4fc120b93c3
Pavel Marik <pmarik@perforce.com>
2024-11-15 11:28:31 +0100

    test

--- bzt/modules/services.py
+++ bzt/modules/services.py
@@ -45,7 +45,7 @@ if not is_windows():
 
 class PipInstaller(Service):
     pip_constraints = {
-        'setuptools': '75.5.0',
+        'setuptools': '65.5.0',
         'flask-cors': '4.0.2'
     }
 
