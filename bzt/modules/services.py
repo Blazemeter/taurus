981eeefef465d86c43e65e9951b758a648fad25b
dmykhaliev <dmykhaliev@perforce.com>
2025-07-22 15:58:49 +0200

    Attempt to fix locust #2

--- bzt/modules/services.py
+++ bzt/modules/services.py
@@ -46,7 +46,8 @@ if not is_windows():
 class PipInstaller(Service):
     pip_constraints = {
         'setuptools': '80.9.1',
-        'zope.event': '5.1.1'
+        'zope.event': '5.0.0',
+        'gevent': '24.10.1'
     }
 
     pip_constraints_file = None
