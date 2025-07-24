34237ee60642da24cfc32e2e76179541558dc5e0
dmykhaliev <dmykhaliev@perforce.com>
2025-07-24 14:06:08 +0200

    Revert TestNG test changes

--- examples/selenium/testng/src/main/java/example/TestNG.java
+++ examples/selenium/testng/src/main/java/example/TestNG.java
@@ -1,10 +1,7 @@
 package example;
 
-import java.io.File;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeDriverService;
-import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
@@ -15,19 +12,7 @@ public class TestNG {
 
     @BeforeClass
     public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "/root/.bzt/selenium-taurus/tools/drivers/chromedriver/138.0.7204.168/chromedriver");
-        ChromeOptions options = new ChromeOptions();
-        options.setBinary("/usr/bin/google-chrome");
-        options.addArguments("--headless=chrome");
-        options.addArguments("--no-sandbox");
-        options.addArguments("--disable-dev-shm-usage");
-
-        ChromeDriverService service = new ChromeDriverService.Builder()
-            .usingDriverExecutable(new File(System.getProperty("webdriver.chrome.driver")))
-            .withLogFile(new File("/tmp/artifacts/chromedriver.log"))
-            .build();
-
-        driver = new ChromeDriver(service, options);
+        driver = new ChromeDriver();
     }
 
     @Test
