b13e172d6e1e6812df912d17f4e804741019edbd
dmykhaliev <dmykhaliev@perforce.com>
2025-07-24 12:07:33 +0200

    Set chromedriver path directly in TestNG test

--- examples/selenium/testng/src/main/java/example/TestNG.java
+++ examples/selenium/testng/src/main/java/example/TestNG.java
@@ -1,7 +1,9 @@
 package example;
 
+import java.io.File;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeDriverService;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
@@ -13,12 +15,18 @@ public class TestNG {
 
     @BeforeClass
     public void setUp() {
+        System.setProperty("webdriver.chrome.driver", "/root/.bzt/selenium-taurus/tools/drivers/chromedriver/138.0.7204.168/chromedriver");
         ChromeOptions options = new ChromeOptions();
         options.setBinary("/usr/bin/google-chrome");
         options.addArguments("--headless=chrome");
         options.addArguments("--no-sandbox");
         options.addArguments("--disable-dev-shm-usage");
-        driver = new ChromeDriver(options);
+
+        ChromeDriverService service = new ChromeDriverService.Builder()
+            .usingDriverExecutable(new File(System.getProperty("webdriver.chrome.driver")))
+            .build();
+
+        driver = new ChromeDriver(service, options);
     }
 
     @Test
