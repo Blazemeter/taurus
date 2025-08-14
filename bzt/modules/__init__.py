efb3b5abcca123161662129c562442f01f7fa44c
dmykhaliev <dmykhaliev@perforce.com>
2025-08-14 10:03:36 +0200

    Jmeter ready

--- bzt/modules/__init__.py
+++ bzt/modules/__init__.py
@@ -17,6 +17,10 @@ limitations under the License.
 """
 import abc
 import os
+from datetime import datetime
+from urllib.parse import quote
+
+import requests
 
 from bzt import ToolError
 from bzt.engine import ScenarioExecutor
@@ -202,52 +206,108 @@ class SubprocessedExecutor(ReportableExecutor, TransactionProvider):
         return self.widget
 
 
-class RemoteProcessedExecutor(ReportableExecutor, TransactionProvider):
+class RemoteExecutor(ReportableExecutor, TransactionProvider):
     """
-    Class for subprocessed executors
+    Class for remote executors
 
-    All executors must implement the following interface.
+    All remote executors must implement the following interface.
     """
 
     def __init__(self):
-        super(RemoteProcessedExecutor, self).__init__()
+        super(RemoteExecutor, self).__init__()
         TransactionProvider.__init__(self)
         self.script = None
         self.process = None
         self.widget = None
 
     def prepare(self):
-        super(RemoteProcessedExecutor, self).prepare()
+        self.bridge_url = self.settings.get("bridge-url", os.environ.get("BRIDGE_URL", None))
+        if not self.bridge_url:
+            self.log.warning("Bridge URL is not set. Please set 'bridge-url' "
+                             "in the executor settings or BRIDGE_URL environment variable.")
+            pass
+        self.bridge_command_url = self.bridge_url.rstrip("/") + "/command"
+        self.bridge_upload_url = self.bridge_url.rstrip("/") + "/upload?path="
+        self.file_url = self.bridge_url.rstrip("/") + "/file"
+        self.bridge_os = self.settings.get("remote-os", os.environ.get("REMOTE_OS", 'windows')).lower()
+        self.remote_root_path = self.settings.get("remote-root-path", os.environ.get("REMOTE_ROOT_PATH", None))
+        if not self.remote_root_path:
+            self.remote_root_path = self.command('echo %TEMP%')['output']
+        if self.bridge_os == 'windows':
+            self.remote_root_path = self.remote_root_path.replace('\\', '/')
+        self.log.info('Remote root path: %s', self.remote_root_path)
 
-    def resource_files(self):
-        script = self.get_script_path()
-        if script:
-            return [script]
-        else:
-            return []
+        artifacts_dir = datetime.now().strftime("%Y-%m-%d-%Hh-%Mm-%Ss-") \
+                        + str(datetime.now().microsecond // 1000).zfill(3) + "ms"
+        self.remote_artifacts_path = os.path.join(self.remote_root_path, artifacts_dir)
+        if self.bridge_os == 'windows':
+            self.remote_artifacts_path = self.remote_artifacts_path.replace('\\', '/')
+        self.log.info('Creating remote artifacts path: %s', self.remote_artifacts_path)
+        self.command('mkdir -p "%s"' % self.remote_artifacts_path, wait_for_completion=True, use_shell=True)
+        self.log.info('Remote artifacts path created')
+        super(RemoteExecutor, self).prepare()
 
-    def check(self):
-        pass
-
-    def shutdown(self):
-        shutdown_process(self.process, self.log)
-
-    def _check_tools(self, tools):
-        pass
-
-    def has_results(self):
-        return bool(self.reader) and bool(self.reader.read_records)
-
-    def get_error_diagnostics(self):
-        pass
-
-    def get_widget(self):
+    def command(self, command, wait_for_completion=True, use_shell=False, workingDir=None):
         """
-        Add progress widget to console screen sidebar
-
-        :rtype: ExecutorWidget
+        Send command to bridge and return response
+        :param command: dict
+        :param wait_for_completion: bool
+        :param use_shell: bool
+        :return: dict
         """
-        if not self.widget:
-            label = ("%s" % self).split('/')
-            self.widget = ExecutorWidget(self, f"{label[0].title()}: {label[1]}")
-        return self.widget
+        data = {
+            "command": command,
+            "waitForCompletion": wait_for_completion,
+            "useShell": use_shell,
+            "workingDir": workingDir
+        }
+        response = requests.post(self.bridge_command_url, json=data)
+        if response.status_code != 200:
+            raise ToolError(f"Bridge command failed with status code {response.status_code}: {response.text}")
+        return response.json()
+
+    def upload_file(self, file_path, remote_file_path):
+        with open(file_path, "rb") as f:
+            files = {"file": ('file', f, "text/plain")}
+            encoded_test_path = quote(remote_file_path)
+            requests.post(self.bridge_upload_url + encoded_test_path, files=files)
+
+
+def resource_files(self):
+    script = self.get_script_path()
+    if script:
+        return [script]
+    else:
+        return []
+
+
+def check(self):
+    pass
+
+
+def shutdown(self):
+    shutdown_process(self.process, self.log)
+
+
+def _check_tools(self, tools):
+    pass
+
+
+def has_results(self):
+    return bool(self.reader) and bool(self.reader.read_records)
+
+
+def get_error_diagnostics(self):
+    pass
+
+
+def get_widget(self):
+    """
+    Add progress widget to console screen sidebar
+
+    :rtype: ExecutorWidget
+    """
+    if not self.widget:
+        label = ("%s" % self).split('/')
+        self.widget = ExecutorWidget(self, f"{label[0].title()}: {label[1]}")
+    return self.widget
