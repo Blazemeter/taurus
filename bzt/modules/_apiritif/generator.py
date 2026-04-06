28a2abec68296bfee8bfa4e9665c49466e079857
Shubham Sudame <shubham.sudame@perforce.com>
2026-04-06 16:06:17 +0530

    updated for firefox

--- bzt/modules/_apiritif/generator.py
+++ bzt/modules/_apiritif/generator.py
@@ -1304,12 +1304,7 @@ from selenium.webdriver.common.keys import Keys
             ast.Assign(
                 targets=[ast.Name(id="options")],
                 value=ast_call(
-                    func=ast_attr("webdriver.FirefoxOptions"))),
-            ast.Expr(
-                ast_call(
-                    func=ast_attr("options.set_capability"),
-                    args=[ast.Constant("unhandledPromptBehavior", kind=""),
-                          ast.Constant("ignore", kind="")]))]
+                    func=ast_attr("webdriver.FirefoxOptions")))]
 
         return firefox_options + self._get_headless_setup()
 
